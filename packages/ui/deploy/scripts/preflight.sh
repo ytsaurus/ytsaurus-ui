5033eb9c1c5ab4aaf8029c678847231eb7a6bd18
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-03-24 16:06:39 +0300

    fix: ytsaurus/ui docker image fixed for localmode

--- packages/ui/deploy/scripts/preflight.sh
+++ packages/ui/deploy/scripts/preflight.sh
@@ -5,14 +5,7 @@ APP_ENV=${APP_ENV:-production}
 
 sed -e "s/%APP_HTTP_PORT%/${APP_HTTP_PORT:-80}/" -i /etc/nginx/sites-enabled/*.conf
 
-if [ "$APP_ENV" = "local" ]; then
-    sed -e 's/include \/etc\/nginx\/includes\/yqlkit.conf;//' -i /etc/nginx/sites-enabled/*.conf
-    sed -e 's/include \/etc\/nginx\/includes\/docs.conf;//' -i /etc/nginx/sites-enabled/*.conf
-fi
-
-if [ -n "$DENY_YQL_PROXY" ]; then
-    sed -e 's/include \/etc\/nginx\/includes\/yqlkit.conf;//' -i /etc/nginx/sites-enabled/*.conf
-fi
+mkdir -p /opt/app/secrets /opt/app/dist/run
 
 chown -R app /opt/app/secrets
 chown app /opt/app/dist/run
