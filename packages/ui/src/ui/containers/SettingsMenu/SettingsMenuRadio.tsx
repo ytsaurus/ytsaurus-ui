de89fcc99583dc895eb00f33a16d505f68b9bacc
Konstantin Shishov <shishov.k.a@yandex.ru>
2023-04-14 15:20:57 +0300

    fix: rework font select [YTFRONT-3691]

--- packages/ui/src/ui/containers/SettingsMenu/SettingsMenuRadio.tsx
+++ packages/ui/src/ui/containers/SettingsMenu/SettingsMenuRadio.tsx
@@ -11,40 +11,46 @@ import type {FIX_MY_TYPE} from '../../types';
 
 const block = cn('elements-page');
 
-type SettingNS = FIX_MY_TYPE;
-
-interface Props extends ConnectedProps<typeof connector> {
-    settingName: string;
-    settingNS: SettingNS;
+interface BaseProps {
     items: ItemType[];
-
-    onAfterChange?(): void;
-    onChange?(settingName: string, settingNS: SettingNS, value: unknown): void;
+    name: string;
     heading?: string;
     description?: string;
-
     convertValue?(value: string): unknown;
+    onChange?(value: unknown): void;
+    onAfterChange?(): void;
+    set(value: unknown): Promise<void>;
+    get(): unknown;
 }
 
-const SettingsMenuRadio: VFC<Props> = (props) => {
-    const {getSetting, setSetting, onAfterChange, convertValue = (v) => v, ...rest} = props;
-    const {settingName, settingNS, items} = props;
-    const {heading, description, onChange} = props;
+export const SettingsMenuRadioBase = (props: BaseProps) => {
+    const {
+        name,
+        items,
+        heading,
+        description,
+        convertValue = (v) => v,
+        onChange,
+        onAfterChange,
+        set,
+        get,
+        ...rest
+    } = props;
 
     const handleChange = React.useCallback(
         (evt: React.ChangeEvent<HTMLInputElement>) => {
             const value = convertValue(evt.target.value);
             if (onChange) {
-                return onChange(settingName, settingNS, value);
+                return onChange(value);
             } else {
-                const res = setSetting(settingName, settingNS, value);
+                const res = set(value);
                 if (onAfterChange) {
                     onAfterChange();
                 }
                 return res;
             }
         },
-        [onChange, onAfterChange, setSetting, settingName, settingNS, convertValue],
+        [convertValue, onChange, set, onAfterChange],
     );
 
     return (
@@ -55,9 +61,9 @@ const SettingsMenuRadio: VFC<Props> = (props) => {
                 {...rest}
                 size="m"
                 items={items}
-                name={settingName}
+                name={name}
                 onChange={handleChange}
-                value={String(getSetting(settingName, settingNS))}
+                value={String(get())}
             />
 
             {description && (
@@ -66,6 +72,43 @@ const SettingsMenuRadio: VFC<Props> = (props) => {
                 </div>
             )}
         </div>
+    );
+};
+
+type SettingNS = FIX_MY_TYPE;
+
+interface Props
+    extends ConnectedProps<typeof connector>,
+        Pick<BaseProps, 'items' | 'heading' | 'description' | 'onAfterChange' | 'convertValue'> {
+    settingName: string;
+    settingNS: SettingNS;
+    onChange?(settingName: string, settingNS: SettingNS, value: unknown): void;
+}
+
+const SettingsMenuRadio: VFC<Props> = (props) => {
+    const {settingName, settingNS, getSetting, setSetting, onChange, ...rest} = props;
+    const onChangeHandler = React.useCallback(
+        (value: unknown) => {
+            if (onChange) onChange(settingName, settingNS, value);
+        },
+        [onChange, settingNS, settingName],
+    );
+    const set = React.useCallback(
+        (value: unknown) => setSetting(settingName, settingNS, value),
+        [setSetting, settingNS, settingName],
+    );
+    const get = React.useCallback(
+        () => getSetting(settingName, settingNS),
+        [getSetting, settingNS, settingName],
+    );
+    return (
+        <SettingsMenuRadioBase
+            {...rest}
+            name={settingName}
+            onChange={onChange ? onChangeHandler : undefined}
+            set={set}
+            get={get}
+        />
     );
 };
 
