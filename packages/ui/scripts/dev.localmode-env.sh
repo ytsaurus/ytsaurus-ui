5232ca14410fff733c25568a999a321e506c41c4
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-04-25 15:58:53 +0300

    chore(package.json): minor changes for some scripts

--- packages/ui/scripts/dev.localmode-env.sh
+++ packages/ui/scripts/dev.localmode-env.sh
@@ -1,6 +1,6 @@
-read -p "Proxy host [`hostname`]: " proxyHost
+read -p "Proxy host [$(hostname)]: " proxyHost
 if [ -z "$proxyHost" ]; then
-  proxyHost=`hostname`
+  proxyHost=$(hostname)
 fi
 
 read -p "Proxy port [8000]: " proxyPort
@@ -11,7 +11,6 @@ fi
 export APP_ENV=local
 export PROXY=$proxyHost:$proxyPort
 
-
 echo
 echo localmode environment is prepared:
 echo APP_ENV=$APP_ENV
@@ -20,6 +19,15 @@ echo
 
 curl http://${PROXY}/hosts | head -n 1 | grep '\["'
 if [ $? -ne 0 ]; then
+  srcUrl=https://raw.githubusercontent.com/ytsaurus/ytsaurus/main/yt/docker/local/run_local_cluster.sh
   echo Error: Cannot get list of hosts. Please make sure your proxy is available.
+  echo -e "\nYou can use ${srcUrl} to run you local cluster:"
+  read -p "Do you want to download the file [yN]:" getAndRun
+  if [ "$getAndRun" = "y" -o "$getAndRun" = "Y" ]; then
+    curl -o run_local_cluster.sh ${srcUrl}
+    chmod u+x run_local_cluster.sh
+    echo -e "\nrun_local_cluster.sh is downloaded, to run your cluster use command:"
+    echo -e "      ./run_local_cluster.sh --docker-hostname $(hostname)\n"
+  fi
   exit 1
 fi
