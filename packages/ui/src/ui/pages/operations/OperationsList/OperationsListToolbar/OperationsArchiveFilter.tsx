9af3f5574e53a92ab9065d2fa3ba6cce00072bb7
Konstantin Shishov <shishovka@shishovka-vm-91296122.sas.yp-c.yandex.net>
2023-05-12 18:02:41 +0300

    chore: rewrite OperationArchiveFilter with tsx

--- packages/ui/src/ui/pages/operations/OperationsList/OperationsListToolbar/OperationsArchiveFilter.tsx
+++ packages/ui/src/ui/pages/operations/OperationsList/OperationsListToolbar/OperationsArchiveFilter.tsx
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
+import React, {useState, useCallback} from 'react';
 import cn from 'bem-cn-lite';
-import {connect} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../../../../store/reducers';
+import {showArchiveOperations, showCurrentOperations} from '../../../../store/actions/operations';
+import {OPERATIONS_DATA_MODE} from '../../../../constants/operations';
+
 import _ from 'lodash';
 import moment from 'moment';
-import PropTypes from 'prop-types';
-import {Button} from '@gravity-ui/uikit';
 
+import {Button} from '@gravity-ui/uikit';
 import Icon from '../../../../components/Icon/Icon';
 import Modal from '../../../../components/Modal/Modal';
 import TimePicker from '../../../../components/TimePicker/TimePicker';
 import CustomRadioButton from '../../../../components/RadioButton/RadioButton';
-
-import {OPERATIONS_DATA_MODE} from '../../../../constants/operations';
-import {showArchiveOperations, showCurrentOperations} from '../../../../store/actions/operations';
 import {SelectButton} from '../../../../components/Button/Button';
-import {Datepicker} from '../../../../components/common/Datepicker';
+import {Datepicker, DatepickerOutputDates} from '../../../../components/common/Datepicker';
 
 import './OperationsArchiveFilter.scss';
+import {ValueOf} from '../../../../../@types/types';
 
 const block = cn('operations-list');
 const tbBlock = cn('elements-toolbar');
+const formBlock = cn('elements-form');
 
 const datePickerProps = {
     range: false,
@@ -28,172 +30,188 @@ const datePickerProps = {
     outputFormat: 'datetime',
     className: 'operations-datepicker-control',
     popupClassName: 'operations-datepicker-popup',
-};
+} as const;
 
-class OperationsArchiveFilter extends Component {
-    static propTypes = {
-        // from connect
-        dataMode: PropTypes.oneOf([OPERATIONS_DATA_MODE.ARCHIVE, OPERATIONS_DATA_MODE.CURRENT]),
-        from: PropTypes.string,
-        to: PropTypes.string,
-        showArchiveOperations: PropTypes.func.isRequired,
-        showCurrentOperations: PropTypes.func.isRequired,
-    };
+const radioButtonTypes = {
+    custom: {
+        text: 'custom',
+        value: 'custom',
+    },
+    twoHours: {
+        text: 'last 2 hours',
+        value: 'twoHours',
+        hours: 2,
+    },
+    day: {
+        text: 'last day',
+        value: 'day',
+        hours: 24,
+    },
+    week: {
+        text: 'last week',
+        value: 'week',
+        hours: 168,
+    },
+} as const;
 
-    static defaultProps = {
-        from: moment().subtract(6, 'hours').toISOString(),
-        to: moment().toISOString(),
-    };
+interface State {
+    modalVisible: boolean;
+    activeTypeValue: ValueOf<typeof radioButtonTypes>['value'];
+    from?: string;
+    to?: string;
+}
 
-    state = {
+export default function OperationsArchiveFilter() {
+    const {
+        dataMode,
+        timeRange: {from, to},
+    } = useSelector((store: RootState) => store.operations.list);
+
+    const dispatch = useDispatch();
+
+    const [state, setState] = useState<State>({
         modalVisible: false,
-        activeTypeValue: this.radioButtonTypes.custom.value,
-    };
+        activeTypeValue: radioButtonTypes.custom.value,
+        from: from || moment().subtract(6, 'hours').toISOString(),
+        to: to || moment().toISOString(),
+    });
 
-    showModal = () => {
-        const {from, to} = this.props;
-        this.setState({
+    const showModal = useCallback(() => {
+        setState({
+            ...state,
             modalVisible: true,
-            from: from,
-            to: to,
         });
-    };
+    }, [state]);
 
-    hideModal = () => {
-        this.setState({modalVisible: false});
-    };
+    const hideModal = useCallback(() => {
+        setState({...state, modalVisible: false});
+    }, [state]);
 
-    resetTimeRange = () => {
-        this.hideModal();
-        this.props.showCurrentOperations();
-    };
+    const resetTimeRange = useCallback(() => {
+        hideModal();
+        dispatch(showCurrentOperations());
+    }, [dispatch, hideModal]);
 
-    applyTimeRange = () => {
-        const {from, to, activeTypeValue} = this.state;
-        this.hideModal();
-        if (activeTypeValue === 'custom') {
-            this.props.showArchiveOperations(from, to);
+    const applyTimeRange = useCallback(() => {
+        hideModal();
+        if (state.activeTypeValue === 'custom') {
+            dispatch(showArchiveOperations(state.from, state.to));
         } else {
-            this.props.showArchiveOperations(
-                moment()
-                    .subtract(this.radioButtonTypes[activeTypeValue].hours, 'hours')
-                    .toISOString(),
-                moment().toISOString(),
+            dispatch(
+                showArchiveOperations(
+                    moment()
+                        .subtract(radioButtonTypes[state.activeTypeValue].hours, 'hours')
+                        .toISOString(),
+                    moment().toISOString(),
+                ),
             );
         }
-    };
+    }, [dispatch, hideModal, state]);
 
-    prepareText() {
-        const {from, to, dataMode} = this.props;
-
+    const prepareText = useCallback(() => {
         if (dataMode === OPERATIONS_DATA_MODE.CURRENT) {
             return 'Current operations';
         } else {
             const format = 'DD-MM-YYYY HH:mm';
             const fromPart = moment(from).format(format);
             const toPart = moment(to).format(format);
-
             return fromPart + ' — ' + toPart;
         }
-    }
+    }, [dataMode, from, to]);
 
-    get radioButtonTypes() {
-        return {
-            custom: {
-                text: 'custom',
-                value: 'custom',
-            },
-            twoHours: {
-                text: 'last 2 hours',
-                value: 'twoHours',
-                hours: 2,
-            },
-            day: {
-                text: 'last day',
-                value: 'day',
-                hours: 24,
-            },
-            week: {
-                text: 'last week',
-                value: 'week',
-                hours: 168,
-            },
-        };
-    }
+    const updateStartTime = useCallback(
+        ({from: newFrom}: DatepickerOutputDates) =>
+            setState({
+                ...state,
+                from: newFrom || state.from,
+            }),
+        [state],
+    );
+    const updateFinishTime = useCallback(
+        ({from: newTo}: DatepickerOutputDates) => setState({...state, to: newTo || state.to}),
+        [state],
+    );
 
-    updateStartTime = ({from}) => this.setState({from});
-    updateFinishTime = ({from: to}) => this.setState({to: to});
-    handleActiveTypeChange = (ev) => {
-        const activeTypeValue = ev.target.value;
-        this.setState({
-            activeTypeValue,
-            from: moment()
-                .subtract(this.radioButtonTypes[activeTypeValue].hours, 'hours')
-                .toISOString(),
-            to: moment().toISOString(),
-        });
-    };
+    const handleActiveTypeChange = useCallback(
+        (ev: React.ChangeEvent<HTMLInputElement>) => {
+            const activeTypeValue = ev.target.value as ValueOf<typeof radioButtonTypes>['value'];
+            setState({
+                ...state,
+                activeTypeValue,
+                from: moment()
+                    .subtract(
+                        activeTypeValue !== 'custom'
+                            ? radioButtonTypes[activeTypeValue].hours
+                            : undefined,
+                        'hours',
+                    )
+                    .toISOString(),
+                to: moment().toISOString(),
+            });
+        },
+        [state],
+    );
 
-    renderModal() {
-        const block = cn('elements-form');
-        const {from, to, activeTypeValue} = this.state;
-        const disabled = activeTypeValue !== this.radioButtonTypes.custom.value;
+    const text = prepareText();
+    const checked = dataMode === OPERATIONS_DATA_MODE.ARCHIVE;
+    const disabled = state.activeTypeValue !== radioButtonTypes.custom.value;
 
-        return (
+    return (
+        <div className={block('archive-range', tbBlock('component'))}>
             <Modal
                 title="Choose range"
                 size="s"
                 confirmText="Apply"
                 cancelText="Show current"
-                cl={block('time-modal')}
-                visible={this.state.modalVisible}
+                cl={formBlock('time-modal')}
+                visible={state.modalVisible}
                 content={
-                    <div className={block()}>
-                        <div className={block('button-group')}>
+                    <div className={formBlock()}>
+                        <div className={formBlock('button-group')}>
                             <CustomRadioButton
                                 name="time-range"
-                                items={_.values(this.radioButtonTypes)}
-                                value={activeTypeValue}
-                                onChange={this.handleActiveTypeChange}
+                                items={_.values(radioButtonTypes)}
+                                value={state.activeTypeValue}
+                                onChange={handleActiveTypeChange}
                             />
                         </div>
                         <div>
-                            <div className={block('label')}>From</div>
-                            <div className={block('field-group')}>
-                                <span className={block('field')}>
+                            <div className={formBlock('label')}>From</div>
+                            <div className={formBlock('field-group')}>
+                                <span className={formBlock('field')}>
                                     <Datepicker
                                         {...datePickerProps}
                                         disabled={disabled}
-                                        from={from}
-                                        onUpdate={this.updateStartTime}
+                                        from={state.from}
+                                        onUpdate={updateStartTime}
                                     />
                                 </span>
-                                <span className={block('field')}>
-                                    {from && (
+                                <span className={formBlock('field')}>
+                                    {state.from && (
                                         <TimePicker
                                             disabled={disabled}
-                                            date={from}
-                                            onChange={this.updateStartTime}
+                                            date={state.from}
+                                            onChange={updateStartTime}
                                         />
                                     )}
                                 </span>
                             </div>
-                            <div className={block('label')}>To</div>
-                            <div className={block('field-group')}>
-                                <span className={block('field')}>
+                            <div className={formBlock('label')}>To</div>
+                            <div className={formBlock('field-group')}>
+                                <span className={formBlock('field')}>
                                     <Datepicker
                                         {...datePickerProps}
                                         disabled={disabled}
-                                        from={to}
-                                        onUpdate={this.updateFinishTime}
+                                        from={state.to}
+                                        onUpdate={updateFinishTime}
                                     />
                                 </span>
-                                <span className={block('field')}>
-                                    {to && (
+                                <span className={formBlock('field')}>
+                                    {state.to && (
                                         <TimePicker
                                             disabled={disabled}
-                                            date={to}
-                                            onChange={this.updateFinishTime}
+                                            date={state.to}
+                                            onChange={updateFinishTime}
                                         />
                                     )}
                                 </span>
@@ -201,59 +219,18 @@ class OperationsArchiveFilter extends Component {
                         </div>
                     </div>
                 }
-                renderCustomCancel={(className) => (
-                    <Button
-                        title="Show current"
-                        className={className}
-                        onClick={this.resetTimeRange}
-                    >
+                renderCustomCancel={(className: string | undefined) => (
+                    <Button title="Show current" className={className} onClick={resetTimeRange}>
                         Show current
                     </Button>
                 )}
-                onCancel={this.hideModal}
-                onConfirm={this.applyTimeRange}
+                onCancel={hideModal}
+                onConfirm={applyTimeRange}
             />
-        );
-    }
-
-    render() {
-        const {dataMode} = this.props;
-        const checked = dataMode === OPERATIONS_DATA_MODE.ARCHIVE;
-        const text = this.prepareText();
-
-        return (
-            <div className={block('archive-range', tbBlock('component'))}>
-                {this.renderModal()}
-                <SelectButton
-                    type="submit"
-                    selected={checked}
-                    title={text}
-                    onClick={this.showModal}
-                >
-                    {text}
-                    <Icon awesome="calendar-alt" />
-                </SelectButton>
-            </div>
-        );
-    }
+            <SelectButton type="submit" selected={checked} title={text} onClick={showModal}>
+                {text}
+                <Icon awesome="calendar-alt" />
+            </SelectButton>
+        </div>
+    );
 }
-
-function mapStateToProps({operations}) {
-    const {
-        dataMode,
-        timeRange: {from, to},
-    } = operations.list;
-
-    return {
-        dataMode,
-        from,
-        to,
-    };
-}
-
-const mapDispatchToProps = {
-    showArchiveOperations,
-    showCurrentOperations,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(OperationsArchiveFilter);
