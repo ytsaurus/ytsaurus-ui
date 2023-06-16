a57d8e386184756787f5bbffa1925e67473949a9
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-06-16 19:14:27 +0300

    chore(utils): add formatByParams

---
+++ packages/ui/src/ui/utils/format.ts
@@ -0,0 +1,12 @@
+/**
+ * Replaces occurrences of strings like '{key1}', '{key2}'
+ * by corresponding values from params, i.e. params['key1'], params['key2']
+ * @param template
+ * @param params
+ * @example formatByParamas('hello {user}', {user: 'world'}); // returns 'hello world'
+ */
+export function formatByParams(template: string, params: Record<string, {toString(): string}>) {
+    return Object.keys(params).reduce((acc, key) => {
+        return acc.replace(new RegExp(`{${key}}`, 'g'), params[key].toString());
+    }, template);
+}
