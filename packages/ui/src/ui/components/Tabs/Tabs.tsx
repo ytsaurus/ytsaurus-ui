080bce026fce1c61a3ede66541fee0fd7f62ad18
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-07-04 13:49:52 +0300

    chore(Tabs): rewrite Tabs with typescript

--- packages/ui/src/ui/components/Tabs/Tabs.tsx
+++ packages/ui/src/ui/components/Tabs/Tabs.tsx
@@ -1,63 +1,69 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {NavLink} from 'react-router-dom';
-import PropTypes from 'prop-types';
 import block from 'bem-cn-lite';
+import partition_ from 'lodash/partition';
 
-import Hotkey from '../Hotkey/Hotkey';
+import Hotkey, {HotkeyProps} from '../Hotkey/Hotkey';
 import Link from '../Link/Link';
 import Icon from '../Icon/Icon';
 
 import action from '../action/action';
+import {makeRoutedURL} from '../../store/location';
 
 import './Tabs.scss';
-import {makeRoutedURL} from '../../store/location';
 
 const b = block('tabs');
 
-const propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            value: PropTypes.string.isRequired,
-            text: PropTypes.string.isRequired,
-            counter: PropTypes.number,
-            show: PropTypes.bool,
-            title: PropTypes.string,
-            url: PropTypes.string,
-            routed: PropTypes.bool,
-            external: PropTypes.bool,
-        }),
-    ).isRequired,
-    active: PropTypes.string,
-    getTabLink: PropTypes.func,
-    onTabChange: PropTypes.func,
-    isLink: PropTypes.bool,
-    layout: PropTypes.oneOf(['horizontal', 'vertical']),
-    size: PropTypes.oneOf(['s', 'm', 'l']),
-    underline: PropTypes.bool,
-    routed: PropTypes.bool,
-    routedPreserveLocation: PropTypes.bool,
-    exactNavLink: PropTypes.bool,
-    className: PropTypes.string,
-};
+interface Props<ValueT extends string = string> {
+    className?: string;
 
-const defaultProps = {
-    underline: false,
-    routed: false,
-    layout: 'horizontal',
-};
+    items: Array<TabItem<ValueT>>;
+    active?: ValueT;
+    getTabLink?: () => void;
+    onTabChange: () => void;
+    isLink?: boolean;
+    onTabChange: (value: ValueT) => void;
+    layout?: 'horizontal' | 'vertical';
+    size?: 's' | 'm' | 'l';
+    underline?: boolean;
+    routed?: boolean;
+    routedPreserveLocation?: boolean;
+    exactNavLink?: boolean;
+}
 
-class Tabs extends Component {
-    static propTypes = propTypes;
-    static defaultProps = defaultProps;
+interface TabItem<ValueT extends string = string> {
+    value: ValueT;
 
-    constructor(props) {
-        super(props);
+    text?: React.ReactNode;
+    counter?: number;
+    show?: boolean;
+    title?: string;
+    url?: string;
+    routed?: boolean;
+    external?: boolean;
 
-        this.storeMouseContext = this.storeMouseContext.bind(this);
-        this.onSameMouseContext = this.onSameMouseContext.bind(this);
+    hotkey?: HotkeyProps['settings'];
+}
+
+class Tabs<ValueT extends string = string> extends React.Component<Props<ValueT>> {
+    static defaultProps: Partial<Props> = {
+        underline: false,
+        routed: false,
+        layout: 'horizontal',
+    };
+
+    render() {
+        const {className} = this.props;
+
+        return (
+            <div className={b(null, className)}>
+                {this.renderList()}
+                {this.renderUnderline()}
+            </div>
+        );
     }
 
-    renderCounter(item) {
+    renderCounter(item: TabItem<ValueT>) {
         const {size} = this.props;
         const className = b('tab-counter', {size});
         return (
@@ -65,51 +71,31 @@ class Tabs extends Component {
         );
     }
 
-    renderHotkey(item) {
+    renderHotkey(item: TabItem<ValueT>) {
         return item.hotkey && <Hotkey settings={item.hotkey} />;
     }
 
-    storeMouseContext(event) {
-        this.mouseContext = {
-            target: event.currentTarget,
-            button: event.button,
-        };
-    }
-
-    onSameMouseContext(clickHandler) {
-        return (event) => {
-            const context = this.mouseContext;
-            if (
-                context &&
-                event.currentTarget === context.target &&
-                event.button === context.button
-            ) {
-                clickHandler(event);
-            }
-        };
-    }
-
-    renderNavLink(item) {
+    renderNavLink(item: TabItem<ValueT>) {
         const to = !this.props.routedPreserveLocation
             ? item.url
             : () => {
-                  return makeRoutedURL(item.url);
+                  return makeRoutedURL(item.url!);
               };
 
         return (
-            <NavLink to={to} exact={this.props.exactNavLink}>
+            <NavLink to={to!} exact={this.props.exactNavLink}>
                 {item.text}
             </NavLink>
         );
     }
 
-    renderLink(item) {
+    renderLink(item: TabItem<ValueT>) {
         const {active, getTabLink, isLink} = this.props;
 
         // NOTE: onTabChange has a priority over getTabLink in action.makeEntryClickHandler - in case tabs correspond
         // links, do not pass onTabChange at all - so it won't get called.
         const onTabChange = !isLink && this.props.onTabChange;
-        const onTabClick = (evt) => {
+        const onTabClick = (evt: React.MouseEvent<Element, MouseEvent>) => {
             const clickHandler = action.makeEntryClickHandler(evt, onTabChange, getTabLink);
             clickHandler(item.value);
         };
@@ -130,7 +116,7 @@ class Tabs extends Component {
         );
     }
 
-    renderTab(item) {
+    renderTab(item: TabItem<ValueT>) {
         const {size, active, routed} = this.props;
 
         const className = b('tab', {
@@ -156,9 +142,14 @@ class Tabs extends Component {
 
         const className = b('list', {layout});
 
+        const [regularItems, externalItems] = partition_(items, (item) => !item.external);
+
         return (
             <ul className={className}>
-                {items.map((item) => {
+                {regularItems.map((item) => {
+                    return this.renderTab(item);
+                })}
+                {externalItems.map((item) => {
                     return this.renderTab(item);
                 })}
             </ul>
@@ -172,20 +163,9 @@ class Tabs extends Component {
 
         return underline && layout === 'horizontal' && <div className={className} />;
     }
-
-    render() {
-        const {className} = this.props;
-
-        return (
-            <div className={b(null, className)}>
-                {this.renderList()}
-                {this.renderUnderline()}
-            </div>
-        );
-    }
 }
 
-export function TabsHorizontal(props) {
+export function TabsHorizontal<ValueT extends string = string>(props: Props<ValueT>) {
     return <Tabs {...props} layout={'horizontal'} />;
 }
 
