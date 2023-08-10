6eea40bf740b4dbb06d085e3a93897006108f272
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-08-10 14:40:31 +0300

    feat(System/HttpProxies): new design for details [YTFRONT-3297]

--- packages/ui/src/ui/pages/system/Proxies/Proxies.tsx
+++ packages/ui/src/ui/pages/system/Proxies/Proxies.tsx
@@ -1,25 +1,21 @@
 import React, {Component} from 'react';
 import {ConnectedProps, connect} from 'react-redux';
-import block from 'bem-cn-lite';
-import _ from 'lodash';
+import isEmpty_ from 'lodash/isEmpty';
+import map_ from 'lodash/map';
 import {compose} from 'redux';
 
 import {CollapsibleSectionStateLess} from '../../../components/CollapsibleSection/CollapsibleSection';
 import SystemStateOverview from '../SystemStateOverview/SystemStateOverview';
 import withDataLoader from '../../../hocs/pages/withDataLoader';
-import Link from '../../../components/Link/Link';
-import NodeQuad from '../NodeQuad/NodeQuad';
 
 import {cancelLoadProxies, loadProxies} from '../../../store/actions/system/proxies';
 import {isSystemResourcesLoaded} from '../../../store/selectors/system';
-import {formatCounterName} from '../../../utils/index';
 import {getUISizes} from '../../../store/selectors/global';
 import {getSettingsSystemHttpProxiesCollapsed} from '../../../store/selectors/settings-ts';
 import {setSettingsSystemHttpProxiesCollapsed} from '../../../store/actions/settings/settings';
 import type {RootState} from '../../../store/reducers';
-import type {RoleGroup} from '../../../store/reducers/system/proxies';
 
-const b = block('system');
+import {RoleGroup} from './RoleGroup';
 
 type ReduxProps = ConnectedProps<typeof connector>;
 
@@ -29,29 +25,6 @@ class Proxies extends Component<ReduxProps> {
         this.props.setSettingsSystemHttpProxiesCollapsed(!collapsed);
     };
 
-    renderRoleGroup(group: RoleGroup) {
-        const nodes = _.map(group.items, (proxy) => {
-            const state = proxy.effectiveState;
-            return <NodeQuad key={proxy.name} theme={state} />;
-        });
-        const url = `components/http_proxies?role=${group.name}`;
-
-        return (
-            <Link key={group.name} className={b('rack-wrapper')} url={url} routed={true}>
-                <span className={block('elements-heading ')({size: 's'})}>{group.name}</span>
-                <div className={b('rack', {size: 'l'})}>
-                    <div className={b('rack-nodes')}>{nodes}</div>
-                    <div className={b('rack-counters')}>
-                        <div className={b('rack-counter', {secondary: 'yes'})}>
-                            {formatCounterName('total')}
-                            <span>{group.total}</span>
-                        </div>
-                    </div>
-                </div>
-            </Link>
-        );
-    }
-
     renderOverview() {
         const {counters} = this.props;
         return <SystemStateOverview tab="http_proxies" counters={counters} />;
@@ -59,7 +32,7 @@ class Proxies extends Component<ReduxProps> {
 
     render() {
         const {roleGroups, counters, collapsibleSize, collapsed} = this.props;
-        if (_.isEmpty(roleGroups) && _.isEmpty(counters)) {
+        if (isEmpty_(roleGroups) && isEmpty_(counters)) {
             return null;
         }
 
@@ -73,7 +46,17 @@ class Proxies extends Component<ReduxProps> {
                 collapsed={collapsed}
                 size={collapsibleSize}
             >
-                <div className={b('racks')}>{_.map(roleGroups, this.renderRoleGroup)}</div>
+                <div>
+                    {map_(roleGroups, (data) => {
+                        return (
+                            <RoleGroup
+                                key={data.name}
+                                data={data}
+                                url={`components/http_proxies?role=${data.name}`}
+                            />
+                        );
+                    })}
+                </div>
             </CollapsibleSectionStateLess>
         );
     }
---
+++ packages/ui/src/ui/pages/system/Proxies/RoleGroup.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import cn from 'bem-cn-lite';
+import {Link, Text} from '@gravity-ui/uikit';
+
+import './RoleGroup.scss';
+
+import {ProgressCircle} from '../../../components/ProgressCircle/ProgressCircle';
+
+import type {RoleGroupInfo} from '../../../store/reducers/system/proxies';
+import format from '../../../common/hammer/format';
+
+const block = cn('yt-role-group');
+
+export function RoleGroup({
+    data: {name, total, effectiveStates},
+    url,
+}: {
+    data: RoleGroupInfo;
+    url: string;
+}) {
+    const {online, offline, banned} = effectiveStates;
+    return (
+        <Link href={url} className={block()}>
+            <Text className={block('name')} color="primary" variant="body-3" ellipsis>
+                {name}
+            </Text>
+            <div className={block('progress')}>
+                <div className={block('progress-text')}>
+                    <div>
+                        <Text color="secondary">TOTAL SERVERS IN RACK</Text>
+                    </div>
+                    <Text className={block('progress-number')} color="primary" variant="display-1">
+                        {format.Number(total)}
+                    </Text>
+                </div>
+                <ProgressCircle
+                    size={52}
+                    gap={1.5}
+                    stack={[
+                        {value: online, color: 'var(--success-color)'},
+                        {value: offline, color: 'var(--danger-color)'},
+                        {value: banned, color: 'var(--warning-color)'},
+                    ]}
+                />
+            </div>
+            <States states={effectiveStates} />
+        </Link>
+    );
+}
+
+function States({states}: {states: RoleGroupInfo['effectiveStates']}) {
+    const {online, offline, banned, alert, dec} = states;
+    return (
+        <div className={block('counters')}>
+            <Status status="online" count={online} />
+            <Status status="offline" count={offline} />
+            <Status status="banned" count={banned} />
+            <Status status="alert" count={alert} />
+            <Status status="dec" count={dec} />
+        </div>
+    );
+}
+
+function Status({status, count}: {status: keyof RoleGroupInfo['effectiveStates']; count: number}) {
+    return (
+        <div className={block('status')}>
+            <Text className={block('status-label')} color="secondary" variant="caption-2">
+                {status}
+            </Text>
+            <div className={block('status-count')}>
+                <div className={block('status-color', {theme: status})} />
+                <Text className={block('status-count-number')} color="primary" variant="caption-2">
+                    {format.Number(count)}
+                </Text>
+            </div>
+        </div>
+    );
+}
