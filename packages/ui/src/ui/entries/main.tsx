9bfe4a489ec67d1e7556d1c815217823234a45f8
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-09-27 16:34:04 +0300

    fix: uncaught error from browser's console

--- packages/ui/src/ui/entries/main.tsx
+++ packages/ui/src/ui/entries/main.tsx
@@ -5,15 +5,19 @@ import axios from 'axios';
 import {handleAuthError} from '../store/actions/global';
 import './main.scss';
 
-yt.subscribe('error', onAxiosError);
+yt.subscribe('error', onError);
 axios.interceptors.response.use(undefined, onAxiosError);
 
 renderApp({});
 
 function onAxiosError(e: any) {
+    onError(e);
+    return Promise.reject(e);
+}
+
+function onError(e: any) {
     const isAuthError = axios.isAxiosError(e) && e.response?.status === 401;
     if (isAuthError) {
         handleAuthError();
     }
-    return Promise.reject(e);
 }
