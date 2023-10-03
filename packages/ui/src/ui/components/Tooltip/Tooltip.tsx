5a102dcff1d7aec789554cc08001f9bb0571eb0e
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-10-03 20:15:07 +0300

    chore: fixes for Tooltip

--- packages/ui/src/ui/components/Tooltip/Tooltip.tsx
+++ packages/ui/src/ui/components/Tooltip/Tooltip.tsx
@@ -1,45 +1,88 @@
 import React from 'react';
 import cn from 'bem-cn-lite';
 
-import {Popover, PopoverProps} from '@gravity-ui/uikit';
+import {Popover, PopoverInstanceProps, PopoverProps} from '@gravity-ui/uikit';
 import './Tooltip.scss';
 import {FIX_MY_TYPE} from '../../types';
 
 const block = cn('yt-tooltip');
 
-export type TooltipProps = Omit<PopoverProps, 'theme' | 'children'> & {
+export type TooltipProps = Omit<
+    PopoverProps,
+    'theme' | 'children' | 'delayOpening' | 'delayClosing' | 'behavior'
+> & {
     theme?: PopoverProps['theme'] | 'fix-link-colors';
     children?: React.ReactNode;
 };
 
+const SHOW_HIDE_DELAY = 400;
+
 export function Tooltip(props: TooltipProps) {
-    const {
-        behavior,
-        content,
-        className,
-        delayOpening = 400,
-        delayClosing = 400,
-        children,
-        theme,
-        ...rest
-    } = props;
+    const {content, className, children, theme, ...rest} = props;
 
-    const delayProps = Object.assign(
-        {},
-        'behavior' in props ? {behavior} : {delayOpening, delayClosing},
-    );
+    const anchorRef = React.useRef<HTMLDivElement>(null);
+    const popoverRef = React.useRef<PopoverInstanceProps>(null);
 
-    return content ? (
-        <Popover
-            {...rest}
-            {...delayProps}
-            theme={theme as FIX_MY_TYPE}
-            className={className}
-            content={content}
+    const [isOpen, setOpen] = React.useState(rest.initialOpen);
+    const [nextOpen, setNextOpen] = React.useState<boolean | undefined>(undefined);
+
+    const toggleOpen = React.useCallback(() => {
+        if (isOpen) {
+            popoverRef.current?.closeTooltip();
+        } else {
+            popoverRef.current?.openTooltip();
+        }
+    }, [isOpen]);
+
+    React.useEffect(() => {
+        if (nextOpen === undefined) {
+            return () => {};
+        }
+
+        const timerId = setTimeout(() => {
+            if (nextOpen !== isOpen) {
+                if (nextOpen) {
+                    popoverRef.current?.openTooltip();
+                } else {
+                    popoverRef.current?.closeTooltip();
+                }
+            }
+            setNextOpen(undefined);
+        }, SHOW_HIDE_DELAY);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [isOpen, nextOpen]);
+
+    const handleClose = React.useCallback(() => {
+        setNextOpen(false);
+    }, []);
+
+    const handleOpen = React.useCallback(() => {
+        setNextOpen(true);
+    }, []);
+
+    return (
+        <div
+            className={block({'has-tooltip': Boolean(content)}, className)}
+            ref={anchorRef}
+            onMouseOver={handleOpen}
+            onMouseOut={handleClose}
+            onClick={toggleOpen}
         >
             {children}
-        </Popover>
-    ) : (
-        <div className={block(null, className)}>{children}</div>
+            {content ? (
+                <Popover
+                    {...rest}
+                    theme={theme as FIX_MY_TYPE}
+                    className={className}
+                    content={content}
+                    anchorRef={anchorRef}
+                    ref={popoverRef}
+                    onOpenChange={setOpen}
+                />
+            ) : null}
+        </div>
     );
 }
