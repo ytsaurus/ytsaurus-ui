82b52a3641ef081e92d312439c0d8acb24a9508d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-10-26 16:34:34 +0300

    fix(GroupsPage): get rid of updater from GroupsPage [YTFRONT-3835]

--- packages/ui/src/ui/constants/groups.js
+++ packages/ui/src/ui/constants/groups.js
@@ -5,8 +5,6 @@ const PREFIX = createPrefix('GROUPS');
 export const GROUPS_TABLE = createActionTypes(PREFIX + 'GROUPS_TABLE');
 export const GROUPS_TABLE_DATA_FIELDS = PREFIX + 'GROUPS_TABLE_DATA_FIELDS';
 
-export const GROUPS_TABLE_UPDATER_ID = 'GROUPS_TABLE_UPDATER_ID';
-
 export const ROOT_GROUP_NAME = '<Root>';
 
 export const GROUP_EDITOR_ACTION = createActionTypes(PREFIX + 'GROUP_EDITOR_ACTION');
