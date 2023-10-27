15d950d6104632f7dab553ee33f4e77f3d5c70d2
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-10-27 17:07:43 +0300

    chore: rewrite StatusLabels with typescript

---
+++ packages/ui/src/ui/components/StatusLabel/StatusLabel.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import block from 'bem-cn-lite';
+
+import hammer from '../../common/hammer';
+import Icon, {IconName} from '../Icon/Icon';
+
+import './StatusLabel.scss';
+
+function getViewState(label?: StatusLabelState): ViewState {
+    const states: Record<StatusLabelState, ViewState> = {
+        // preparing states
+        materializing: 'preparing',
+        initializing: 'preparing',
+        preparing: 'preparing',
+        pending: 'preparing',
+        starting: 'preparing',
+
+        // running states
+        running: 'running',
+        completing: 'running',
+        failing: 'running',
+        aborting: 'running',
+        reviving: 'running',
+        suspended: 'suspended',
+
+        // final states
+        failed: 'failed',
+        completed: 'completed',
+        aborted: 'aborted',
+    };
+
+    return states[label!] ?? 'unknown';
+}
+
+export type StatusLabelState =
+    | 'aborted'
+    | 'aborting'
+    | 'completed'
+    | 'completing'
+    | 'failed'
+    | 'failing'
+    | 'initializing'
+    | 'materializing'
+    | 'pending'
+    | 'preparing'
+    | 'reviving'
+    | 'running'
+    | 'starting'
+    | 'suspended'
+    | 'running';
+
+type ViewState =
+    | 'preparing'
+    | 'running'
+    | 'failed'
+    | 'aborted'
+    | 'completed'
+    | 'suspended'
+    | 'unknown';
+
+function getIcon(state: ViewState) {
+    const iconsByViewState: Record<ViewState, IconName> = {
+        preparing: 'clock',
+        running: 'play-circle',
+        suspended: 'pause-circle',
+        failed: 'times-circle',
+        completed: 'check-circle',
+        aborted: 'times-circle',
+        unknown: 'question-circle',
+    };
+    return iconsByViewState[state];
+}
+
+const b = block('status-label');
+
+export type StatusLabelProps = {
+    className?: string;
+    label?: StatusLabelState;
+    renderPlaque?: boolean;
+};
+
+export default function StatusLabel({className, label, renderPlaque}: StatusLabelProps) {
+    const mappedState = getViewState(label);
+    const icon = getIcon(mappedState);
+    const mods = {state: mappedState};
+
+    return !label ? (
+        <span />
+    ) : (
+        <span className={b(null, renderPlaque ? b('plaque', mods, className) : b(mods, className))}>
+            <Icon awesome={icon} />
+            <span>{hammer.format['ReadableField'](label)}</span>
+        </span>
+    );
+}
