e435a259a040fbf09490873210cca5ad37ff3e9d
Konstantin Shishov <shishovka@shishovka-vm-91296122.sas.yp-c.yandex.net>
2023-11-09 20:31:31 +0300

    fix: babel config app-builder conflict

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-typescript'],
+    env: {
+        'test:unit': {
+            presets: [
+                ['@babel/preset-env', {targets: {node: 'current'}}],
+                '@babel/preset-typescript',
+            ],
+        },
+    },
 };
