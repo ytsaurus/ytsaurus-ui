0611638cba7656dcc8cdde277b118eba0604e912
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-11-13 17:55:23 +0300

    chore(Accounts): use 'useUpdater' hook [YTFRONT-3835]

--- packages/ui/src/ui/hooks/use-updater.ts
+++ packages/ui/src/ui/hooks/use-updater.ts
@@ -5,12 +5,14 @@ export const DEFAULT_UPDATER_TIMEOUT = 30 * 1000;
 
 const UPDATER_ID = 'UPDATER_ID';
 
-export function useUpdater(fn: () => unknown, timeout = DEFAULT_UPDATER_TIMEOUT) {
-    const [updater] = React.useState(new Updater());
+export function useUpdater(fn?: () => unknown, timeout = DEFAULT_UPDATER_TIMEOUT) {
+    const [updater] = React.useState(fn ? new Updater() : undefined);
     React.useEffect(() => {
-        updater.add(UPDATER_ID, fn, timeout);
+        if (fn) {
+            updater?.add(UPDATER_ID, fn, timeout);
+        }
         return () => {
-            updater.remove(UPDATER_ID);
+            updater?.remove(UPDATER_ID);
         };
     }, [updater, fn, timeout]);
 }
