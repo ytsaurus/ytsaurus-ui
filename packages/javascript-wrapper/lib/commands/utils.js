0374ada3f42ee24281ec8365e2de2f7cc938d969
Marat Validov <mytwinkg@gmail.com>
2023-12-12 18:05:22 +0300

    prepareData serializer

--- packages/javascript-wrapper/lib/commands/utils.js
+++ packages/javascript-wrapper/lib/commands/utils.js
@@ -1,20 +1,25 @@
 var error = require('../utils/error.js');
+var setup = require('../utils/setup.js');
 
-function prepareInputRows(data) {
+function prepareInputRows(localSetup, data) {
+    var serializer = setup.getOption(localSetup, 'JSONSerializer');
+
     if (Array.isArray(data)) {
         return data.map(function (row) {
-            return JSON.stringify(row);
+            return serializer.stringify(row);
         }).join('\n');
     } else {
         throw new Error(error.prepare('bad command arguments - "data" must be an array.'));
     }
 }
 
-function prepareWriteTableData(data) {
+function prepareWriteTableData(localSetup, data) {
+    var serializer = setup.getOption(localSetup, 'JSONSerializer');
+
     const type = Object.prototype.toString.call(data);
     if (Array.isArray(data)) {
         return data.map(function (row) {
-            return JSON.stringify(row);
+            return serializer.stringify(row);
         }).join('\n');
     } else if (type === '[object File]') {
         return data;
