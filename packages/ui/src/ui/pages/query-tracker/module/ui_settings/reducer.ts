d2e5efa5e1e192cf945f57071145cb911c683b51
vitshev <vitali.shevchenko@gmail.com>
2023-12-12 12:15:45 +0100

    feature(query-tracker): hide query history sidebar

--- packages/ui/src/ui/pages/query-tracker/module/ui_settings/reducer.ts
+++ packages/ui/src/ui/pages/query-tracker/module/ui_settings/reducer.ts
@@ -3,20 +3,26 @@ import {
     TOGGLE_QUERIES_LIST_SIDEBAR
 } from './actions';
 
+const LOCAL_STORAGE_KEY = 'query-tracker/ui-settings/showQueriesList';
+
 export interface QueryTrackerUiSettingsState {
     showQueriesList: boolean;
 }
 
 const initState: QueryTrackerUiSettingsState = {
-    showQueriesList: true
+    showQueriesList: (window.localStorage.getItem(LOCAL_STORAGE_KEY) || 'true') === 'true'
 };
 
 export function reducer(state = initState, action: Actions): QueryTrackerUiSettingsState {
     switch (action.type) {
         case TOGGLE_QUERIES_LIST_SIDEBAR: {
+            const showQueriesList = ! state.showQueriesList;
+
+            window.localStorage.setItem(LOCAL_STORAGE_KEY, String(showQueriesList))
+
             return {
                 ...state,
-                showQueriesList: ! state.showQueriesList
+                showQueriesList
             };
         }
     }
