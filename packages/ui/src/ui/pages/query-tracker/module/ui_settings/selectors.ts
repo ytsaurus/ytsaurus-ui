717098bb131b0a7f12e9a84f7058dbce8b2e272d
vitshev <vitali.shevchenko@gmail.com>
2023-12-12 11:48:42 +0100

    feature(query-tracker): hide query history sidebar

--- packages/ui/src/ui/pages/query-tracker/module/ui_settings/selectors.ts
+++ packages/ui/src/ui/pages/query-tracker/module/ui_settings/selectors.ts
@@ -1,6 +1,8 @@
 import {createSelector} from 'reselect';
 import {RootState} from '../../../../store/reducers';
+
 const getState = (state: RootState) => state.queryTracker;
+
 export const getVisibilityQueriesListUiSetting = createSelector(
     [getState],
     (state) => state.uiSettings.showQueriesList
