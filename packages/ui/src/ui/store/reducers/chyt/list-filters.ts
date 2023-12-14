ac0e9da828feaaf81dd882118776ec579e6e9209
vitshev <vitali.shevchenko@gmail.com>
2023-12-14 16:48:54 +0100

    chore(settings): add getSettingsData and setSettingByKey

---
+++ packages/ui/src/ui/store/reducers/chyt/list-filters.ts
@@ -0,0 +1,31 @@
+import {CHYT_LIST_FILTERS} from '../../../constants/chyt-page';
+import {ActionD, SortState} from '../../../types';
+import {replaceEmpty} from '../../../utils/empty';
+import {ChytInfo} from './list';
+
+export type ChytListFilters = {
+    name: string;
+    creator: string | undefined;
+    state: string | undefined;
+
+    sortState: Array<SortState<keyof ChytInfo>>;
+};
+
+export const initialState: ChytListFilters = {
+    name: '',
+    creator: undefined,
+    state: undefined,
+
+    sortState: [{column: 'alias', order: 'asc'}],
+};
+
+export default function reducer(state = initialState, action: ChytListFiltersAction) {
+    switch (action.type) {
+        case CHYT_LIST_FILTERS:
+            return {...state, ...replaceEmpty(action.data)};
+        default:
+            return state;
+    }
+}
+
+export type ChytListFiltersAction = ActionD<typeof CHYT_LIST_FILTERS, Partial<ChytListFilters>>;
---
+++ packages/ui/src/ui/store/selectors/settings-base.ts
@@ -0,0 +1,4 @@
+import {Settings} from '../../../shared/constants/settings-types';
+import {RootState} from '../../store/reducers';
+
+export const getSettingsData = (state: RootState) => state.settings.data as Settings;
