921d1e1e092fc1af9194b262450d272957781454
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-12-14 19:38:48 +0300

    chore: rewrite withSplit with typescript

--- packages/ui/src/ui/hocs/withSplit.tsx
+++ packages/ui/src/ui/hocs/withSplit.tsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import {getDisplayName} from '../utils';
 import {SPLIT_PANE_ID} from '../constants/index';
 
-export default function withSplit(Component) {
-    return class WithSplit extends React.Component {
+export default function withSplit<P>(Component: React.ComponentType<P>) {
+    return class WithSplit extends React.Component<P> {
         static displayName = `WithSplit(${getDisplayName(Component)})`;
 
         render() {
             return ReactDOM.createPortal(
                 <Component {...this.props} />,
-                document.getElementById(SPLIT_PANE_ID),
+                document.getElementById(SPLIT_PANE_ID)!,
             );
         }
     };
