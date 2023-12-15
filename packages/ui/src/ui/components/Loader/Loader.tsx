4e06ecc3254d7d567a520eeae3252f1bad1c8207
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-12-15 12:32:54 +0300

    chore(Loader): add Loader.props.centered

--- packages/ui/src/ui/components/Loader/Loader.tsx
+++ packages/ui/src/ui/components/Loader/Loader.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import cn from 'bem-cn-lite';
-import {Loader} from '@gravity-ui/uikit';
+
+import {Loader, LoaderProps} from '@gravity-ui/uikit';
+
 import './Loader.scss';
 
 const block = cn('yt-loader');
 
-function YTLoader({visible, className}: {visible?: boolean; className?: string}) {
-    return <div className={block(null, className)}>{visible && <Loader size={'s'} />}</div>;
+export type YTLoaderProps = LoaderProps & {visible?: boolean; centered?: boolean};
+
+function YTLoader({visible, centered, className, size = 's'}: YTLoaderProps) {
+    return <div className={block({centered}, className)}>{visible && <Loader size={size} />}</div>;
 }
 
 export default React.memo(YTLoader);
