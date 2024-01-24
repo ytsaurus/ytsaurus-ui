a6e1dab15f790b65215176c30022d0753b0319e6
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-01-24 22:08:18 +0300

    chore(ACL): minor fixes for ACL [YTFRONT-3660]

--- packages/ui/src/ui/pages/chyt/ChytPageClique/ChytPageCliqueAcl.tsx
+++ packages/ui/src/ui/pages/chyt/ChytPageClique/ChytPageCliqueAcl.tsx
@@ -6,5 +6,9 @@ import {AccessContentAcl} from '../../../components/ACL/ACL-connect-helpers';
 
 export function ChytPageCliqueAcl() {
     const alias = useSelector(getChytCurrentAlias);
-    return <AccessContentAcl path={`//sys/access_control_object_namespaces/chyt/${alias}`} />;
+    return (
+        Boolean(alias) && (
+            <AccessContentAcl path={`//sys/access_control_object_namespaces/chyt/${alias}`} />
+        )
+    );
 }
