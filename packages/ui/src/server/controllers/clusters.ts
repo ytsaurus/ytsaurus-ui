d582bfcd490b8199e5713f24f7970697ba0513dd
vitshev <vitali.shevchenko@gmail.com>
2024-02-06 16:32:09 +0100

    feat: show authorized clusters [#349]

--- packages/ui/src/server/controllers/clusters.ts
+++ packages/ui/src/server/controllers/clusters.ts
@@ -1,6 +1,6 @@
 import type {Request, Response} from 'express';
+import {YT_CYPRESS_COOKIE_NAME} from '../../shared/constants';
 import {sendError, sendResponse} from '../utils';
-
 import {getVersions} from '../components/cluster-queries';
 import {getClustersFromConfig} from '../components/utils';
 
@@ -10,4 +10,34 @@ export function clusterVersions(req: Request, res: Response) {
     getVersions(req, clusters)
         .then((data: {}) => sendResponse(res, data))
         .catch((error: any) => sendError(res, error));
+}
+
+type ClusterAuthStatus = Record<string, {authorized: boolean}>;
+
+export function clusterAuthStatus(req: Request, res: Response) {
+    const allowPasswordAuth = req.ctx.config.allowPasswordAuth;
+
+    let data = {};
+
+    if (allowPasswordAuth) {
+        const clusters = getClustersFromConfig();
+
+        data = Object.keys(clusters).reduce((ret, cluster) => {
+            ret[cluster] = {
+                authorized: Boolean(req.cookies[`${cluster}:${YT_CYPRESS_COOKIE_NAME}`]),
+            };
+
+            return ret;
+        }, {} as ClusterAuthStatus);
+    } else {
+        const clusters = getClustersFromConfig();
+
+        data = Object.keys(clusters).reduce((ret, clusterName) => {
+            ret[clusterName] = {authorized: true};
+
+            return ret;
+        }, {} as ClusterAuthStatus);
+    }
+
+    sendResponse(res, data);
 }
