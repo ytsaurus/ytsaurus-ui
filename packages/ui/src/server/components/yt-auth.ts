352a2715ba5e1267ed2604cea59c5312be31763d
vitshev <vitali.shevchenko@gmail.com>
2024-02-15 13:05:37 +0100

    chore: mass logout for multicluster [#349]

--- packages/ui/src/server/components/yt-auth.ts
+++ packages/ui/src/server/components/yt-auth.ts
@@ -1,14 +1,21 @@
 import {AppConfig} from '@gravity-ui/nodekit';
 import type {Response} from 'express';
 import {YT_CYPRESS_COOKIE_NAME} from '../../shared/constants';
+import {getClustersFromConfig} from './utils';
 
 export function isYtAuthEnabled(config: AppConfig) {
     return Boolean(config.allowPasswordAuth);
 }
 
-export function YTAuthLogout(res: Response, ytAuthCluster: string) {
-    res.setHeader('set-cookie', [
-        `${YT_CYPRESS_COOKIE_NAME}=deleted; Path=/; Max-Age=0;`,
-        `${ytAuthCluster}:${YT_CYPRESS_COOKIE_NAME}=deleted; Path=/; Max-Age=0;`,
-    ]);
+export function YTAuthLogout(res: Response) {
+    const clusters = getClustersFromConfig();
+
+    res.setHeader(
+        'set-cookie',
+        [`${YT_CYPRESS_COOKIE_NAME}=deleted; Path=/; Max-Age=0;`].concat(
+            Object.keys(clusters).map(
+                (cluster) => `${cluster}:${YT_CYPRESS_COOKIE_NAME}=deleted; Path=/; Max-Age=0;`,
+            ),
+        ),
+    );
 }
