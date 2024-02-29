ef72155ff599dfbb5f419099a982be629aee6779
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-02-29 15:15:12 +0300

    chore: add hooks/use-hotkeys-js-scope.ts

--- packages/ui/src/ui/hocs/components/Modal/withScope.tsx
+++ packages/ui/src/ui/hocs/components/Modal/withScope.tsx
@@ -1,50 +1,17 @@
 import React from 'react';
-import key from 'hotkeys-js';
-import PropTypes from 'prop-types';
 
 import {getDisplayName} from '../../../utils';
+import {useHotkeysScope} from '../../../hooks/use-hotkeysjs-scope';
 
 export default function withScope(scope: string) {
     return function wrap<P>(Component: React.ComponentType<P>) {
         type Props = P & {visible: true};
-        return class WithScope extends React.Component<Props> {
-            static propTypes = {
-                visible: PropTypes.bool.isRequired,
-            };
-
-            static displayName = `WithScope(${getDisplayName(Component)})`;
-
-            prevScope = '';
-
-            componentDidMount() {
-                this.prevScope = key.getScope();
-
-                if (this.props.visible) {
-                    key.setScope(scope);
-                }
-            }
-
-            componentDidUpdate(prevProps: Props) {
-                const {visible: prevVisible} = prevProps;
-                const {visible} = this.props;
-
-                if (prevVisible !== visible) {
-                    if (visible) {
-                        this.prevScope = key.getScope();
-                        key.setScope(scope);
-                    } else {
-                        key.setScope(this.prevScope);
-                    }
-                }
-            }
-
-            componentWillUnmount() {
-                key.setScope(this.prevScope);
-            }
-
-            render() {
-                return <Component {...this.props} />;
-            }
+        const ResComponent: React.FC<Props> = function WithScope(props) {
+            useHotkeysScope(scope, props.visible);
+            return <Component {...props} />;
         };
+
+        ResComponent.displayName = `WithScope(${getDisplayName(Component)})`;
+        return ResComponent;
     };
 }
