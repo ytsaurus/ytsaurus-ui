e750edb38aac578a9d48b92a2e769641cf13534a
simbiozizv <simbioziz@gmail.com>
2024-04-01 16:45:06 +0300

    feat: add redux toolkit [YTFRONT-4094]

--- packages/ui/src/ui/store/index.ts
+++ packages/ui/src/ui/store/index.ts
@@ -1,34 +1,26 @@
-import {Store, applyMiddleware, compose, createStore} from 'redux';
+import {Store, configureStore} from '@reduxjs/toolkit';
 // @ts-ignore
 import {listenForHistoryChange} from 'redux-location-state';
 import {createBrowserHistory} from 'history';
-import thunkMiddleware from 'redux-thunk';
-
 import getLocationMiddleware from '../state-url-mapping';
 import {RootState, makeRootReducer} from './reducers';
 
-const enhancedCompose =
-    ((window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose) || compose;
-
-function configureStore(history: ReturnType<typeof createBrowserHistory>) {
+export const createAppStore = () => {
+    const history = createBrowserHistory();
     const rootReducer = makeRootReducer();
-
     const {locationMiddleware, reducersWithLocation} = getLocationMiddleware(history, rootReducer);
 
-    const middlewares = applyMiddleware(thunkMiddleware, locationMiddleware);
+    const store: Store<RootState, any> = configureStore({
+        reducer: reducersWithLocation as ReturnType<typeof makeRootReducer>,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(locationMiddleware),
+        devTools: process.env.NODE_ENV !== 'production',
+    });
 
-    return enhancedCompose(middlewares)(createStore)(reducersWithLocation);
-}
-
-export function createAppStore() {
-    const history = createBrowserHistory();
-    const store: Store<RootState, any> = configureStore(history);
     listenForHistoryChange(store, history);
-
     (window as any).appBrowserHistory = history;
     (window as any).store = store;
     return {store, history};
-}
+};
 
 export type AppBrowserHistory = ReturnType<typeof createAppStore>['history'];
 export type StoreType = ReturnType<typeof createAppStore>['store'];
