5fd9c03822735c03c516464c370e65da97a7764f
simbiozizv <simbioziz@gmail.com>
2024-04-02 16:34:05 +0300

    feat(Query): add tooltip to search field qt [YTFRONT-4096]

--- packages/ui/src/ui/pages/query-tracker/QueriesList/QueriesListFilter/index.tsx
+++ packages/ui/src/ui/pages/query-tracker/QueriesList/QueriesListFilter/index.tsx
@@ -1,7 +1,8 @@
 import React, {useCallback} from 'react';
 import block from 'bem-cn-lite';
-import {ControlGroupOption, RadioButton} from '@gravity-ui/uikit';
+import {ControlGroupOption, Icon, RadioButton, Tooltip} from '@gravity-ui/uikit';
 import {QueriesListAuthorFilter, QueriesListMode} from '../../module/queries_list/types';
+import CircleQuestionIcon from '@gravity-ui/icons/svgs/circle-question.svg';
 
 import './index.scss';
 import {QueryEngineFilter} from './QueryEngineFilter';
@@ -51,25 +52,34 @@ export function QueriesHistoryListFilter({className}: QueriesHistoryListFilterPr
             <div className={b('row')}>
                 {filterViewMode === QueriesListMode.History && (
                     <RadioButton
-                        className={b('row-item')}
                         options={AuthorFilter}
                         value={filter?.user || QueriesListAuthorFilter.My}
                         onUpdate={onChangeAuthorFilter}
                     />
                 )}
-                <QueryEngineFilter
-                    className={b('row-item')}
-                    value={filter?.engine}
-                    onChange={onChangeEngineFilter}
-                />
+                <QueryEngineFilter value={filter?.engine} onChange={onChangeEngineFilter} />
             </div>
             {filterViewMode === QueriesListMode.History && (
                 <div className={b('row')}>
                     <QueryTextFilter
-                        placeholder="Search in query name and body"
+                        placeholder="Search in query name, body and access control"
                         value={filter?.filter}
                         onChange={onChangeTextFilter}
                     />
+                    <Tooltip
+                        content={
+                            <>
+                                Search in query text, annotations and access control
+                                <br />
+                                Use `&quot;title&quot;=&quot;test_name&quot;` to search in query
+                                name
+                                <br />
+                                Use `aco:nobody` to search in query access control
+                            </>
+                        }
+                    >
+                        <Icon data={CircleQuestionIcon} size={16} />
+                    </Tooltip>
                 </div>
             )}
         </div>
