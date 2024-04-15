24204621a4202846617303b74ad92a76ef0e3d28
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-04-15 13:38:56 +0300

    chore(ACL): get rid of autofocues from filters [YTFRONT-3836]
    When list of items above such filter is long enough autofocus
    causes unnecessary scrolling to the focused element.

--- packages/ui/src/ui/containers/ACL/ObjectPermissionsFilters/ObjectPermissionsFilters.tsx
+++ packages/ui/src/ui/containers/ACL/ObjectPermissionsFilters/ObjectPermissionsFilters.tsx
@@ -58,6 +58,7 @@ export default function ObjectPermissionsFilters({
                             }}
                             value={subjectFilter}
                             size="m"
+                            autofocus={false}
                         />
                     ),
                 },
