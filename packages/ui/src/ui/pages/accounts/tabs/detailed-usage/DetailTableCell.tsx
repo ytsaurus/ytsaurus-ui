e0e6c8a6b48806a0ca3f44fecfb15c1ad440095d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-05-08 01:48:11 +0300

    fix(AccountsUsage): fix for 'view'-parameter [YTFRONT-3737]

--- packages/ui/src/ui/pages/accounts/tabs/detailed-usage/DetailTableCell.tsx
+++ packages/ui/src/ui/pages/accounts/tabs/detailed-usage/DetailTableCell.tsx
@@ -17,20 +17,20 @@ const getDiffClass = (showDiff: boolean, sign: number) => {
 type Props = {
     value: number | null;
     additionalValue: number | null;
-    viewType: AccountUsageViewType;
+    viewType?: AccountUsageViewType;
     formatType: 'bytes' | 'number';
 };
 
 export const DetailTableCell: FC<Props> = ({value, additionalValue, viewType, formatType}) => {
     const totalValue = additionalValue ? Number(value) + additionalValue : Number(value);
-    const showDiff = viewType.endsWith('-diff');
+    const showDiff = viewType?.endsWith('-diff');
     const sign = Math.sign(totalValue);
 
     const formatFn = formatType === 'bytes' ? format.Bytes : format.Number;
 
     return (
         <Flex justifyContent="center" alignItems="center" gap={1} className={block()}>
-            <span className={block('value', {diff: getDiffClass(showDiff, sign)})}>
+            <span className={block('value', {diff: getDiffClass(Boolean(showDiff), sign)})}>
                 {showDiff && getRenderSign(sign)}
                 {formatFn(totalValue)}
             </span>
