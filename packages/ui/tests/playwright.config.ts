8b7ff3008818a820f18ccfd6791ceba8a47571f0
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-06-05 18:59:28 +0300

    chore(.github/workflows/ci-e2e.yaml): use 'npm run e2e:localmode:screnshots'

--- packages/ui/tests/playwright.config.ts
+++ packages/ui/tests/playwright.config.ts
@@ -34,10 +34,11 @@ export default defineConfig({
         /* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://127.0.0.1:3000',
 
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
         ignoreHTTPSErrors: true,
         testIdAttribute: 'data-qa',
+
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
     },
     timeout: 10000,
 
