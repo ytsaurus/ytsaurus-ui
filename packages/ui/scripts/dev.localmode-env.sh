12c4532e0b627e28e655ea0004612c5a05acc0b3
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-06-18 17:48:10 +0300

    chore(tests/screenshots): add tests for Scheduling

--- packages/ui/scripts/dev.localmode-env.sh
+++ packages/ui/scripts/dev.localmode-env.sh
@@ -25,6 +25,10 @@ if [ $? -ne 0 ]; then
   echo -e "\n\nrun_local_cluster.sh is downloaded, to run your cluster use command:"
 
   command="./run_local_cluster.sh --yt-version dev --docker-hostname $(hostname) --fqdn localhost --node-count 2 --ui-app-installation ${APP_INSTALLATION:-''}"
+  if [ "$SKIP_PULL" != "" ]; then
+    command="$command --ui-skip-pull true --yt-skip-pull true"
+  fi
+
   if [ "$UI_VERSION_LOCAL" != "" ]; then
     docker tag $(npm run -s show:docker-image-name):local $(npm run -s show:docker-image-name:stable):local
     command="$command --ui-version $UI_VERSION_LOCAL --ui-skip-pull true"
--- packages/ui/tests/utils/BasePage.ts
+++ packages/ui/tests/utils/BasePage.ts
@@ -2,11 +2,30 @@ import {Page} from '@playwright/test';
 import {replaceInnerHtml} from './dom';
 import {E2E_DIR_NAME} from '.';
 
+class DFDialogComponent {
+    readonly page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async showTab(name: string) {
+        await this.page.click(`.df-dialog-tab__name :text("${name}")`);
+        await this.page.mouse.move(0, 0);
+    }
+
+    async waitForField(title: string) {
+        await this.page.waitForSelector(`.df-dialog__label :text("${title}")`);
+    }
+}
+
 export class BasePage {
     readonly page: Page;
+    readonly dfDialog: DFDialogComponent;
 
     constructor({page}: {page: Page}) {
         this.page = page;
+        this.dfDialog = new DFDialogComponent(page);
     }
 
     async waitForTable(selector: string, rowCount: number) {
@@ -21,5 +40,9 @@ export class BasePage {
 
     async replaceBreadcrumbsTestDir() {
         await this.replaceBreadcrumbsByTitle(E2E_DIR_NAME, 'e2e.1970-01-01.00:00:00.xxxxxxxxxxx');
+    }
+
+    async waitForACL() {
+        await this.page.waitForSelector('.navigation-acl__row .yt-subject-link');
     }
 }
