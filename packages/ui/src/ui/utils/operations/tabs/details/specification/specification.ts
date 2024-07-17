bfb59cfebbb3dfd137094e98ebd06c39db7f44b0
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-07-17 12:14:30 +0300

    fix(Operation/Specification/Input): show cluster in links [YTFRONT-4265]
    (cherry picked from commit 8bcde04cf55facc4a5aa8185a1fb360d9b00d4f5)

--- packages/ui/src/ui/utils/operations/tabs/details/specification/specification.ts
+++ packages/ui/src/ui/utils/operations/tabs/details/specification/specification.ts
@@ -184,23 +184,33 @@ function prepareTable(
     };
 }
 
-function groupTables<T extends {path: string; transaction?: string}>(tables: Array<T>) {
-    const group: Array<{path: string; name?: string; isFolder?: boolean; transaction?: string}> =
-        [];
-    let currentFolder: string;
+function groupTables<T extends {path: string; transaction?: string; cluster?: string}>(
+    tables: Array<T>,
+) {
+    const group: Array<{
+        path: string;
+        name?: string;
+        isFolder?: boolean;
+        transaction?: string;
+        cluster?: string;
+    }> = [];
+    let currentGroup: string;
 
     _.each(tables, (table) => {
         try {
+            const {cluster} = table;
             const path = ypath.YPath.create(table.path, 'absolute');
             const name = path.getKey();
-            const folder: string = path.toSubpath(-2).stringify();
+            const folder = path.toSubpath(-2).stringify();
+            const groupKey: string = cluster ? `${cluster}:${folder}` : folder;
 
-            if (currentFolder !== folder) {
-                currentFolder = folder;
+            if (currentGroup !== groupKey) {
+                currentGroup = groupKey;
                 group.push({
                     path: folder,
                     transaction: table.transaction,
                     isFolder: true,
+                    cluster,
                 });
             }
 
