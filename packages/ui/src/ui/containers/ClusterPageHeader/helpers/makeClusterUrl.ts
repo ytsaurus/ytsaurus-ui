37f9b6aadc44cbced0d4d4db2c4b0b4cbf864cb3
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-08-16 13:33:04 +0300

    chore(Maintenance): allow to select another cluster from location.pathname like '/:cluster'

--- packages/ui/src/ui/containers/ClusterPageHeader/helpers/makeClusterUrl.ts
+++ packages/ui/src/ui/containers/ClusterPageHeader/helpers/makeClusterUrl.ts
@@ -1,14 +1,6 @@
 import {makeRoutedURL} from '../../../store/location';
-import {matchPath} from 'react-router';
 
 export const makeClusterUrl = (cluster: string): string | null => {
-    const match = matchPath(window.location.pathname, {
-        path: '/:cluster/:page',
-    });
-    if (!match) {
-        return null;
-    }
-
     const oldUrl = makeRoutedURL(window.location.pathname);
     const [empty, oldCluster, ...rest] = oldUrl.split('/');
     if (oldCluster === cluster) {
