9897cb01fb95dc21ac71295157c0122c1afade03
simbiozizv <simbiozizv@yandex-team.ru>
2024-09-11 16:39:54 +0300

    feat(Navigation): open access logs in qt [YTFRONT-4345]

---
+++ packages/ui/src/ui/pages/navigation/tabs/AccessLog/AccessLogOpenQtButton.tsx
@@ -0,0 +1,26 @@
+import React, {FC, useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {fetchAccessLogQtId} from '../../../../store/actions/navigation/tabs/access-log/access-log';
+import {Button, Icon} from '@gravity-ui/uikit';
+import AbbrSqlIcon from '@gravity-ui/icons/svgs/abbr-sql.svg';
+
+export const AccessLogOpenQtButton: FC = () => {
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+
+    const handleClick = async () => {
+        try {
+            setLoading(true);
+            await dispatch(fetchAccessLogQtId());
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <Button onClick={handleClick} loading={loading}>
+            <Icon data={AbbrSqlIcon} size={16} />
+            Open in QT
+        </Button>
+    );
+};
