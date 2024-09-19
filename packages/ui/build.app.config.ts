f41ea0f7e1bcb3391ea9a4bc478795c0cf3c36f2
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-09-19 14:55:32 +0300

    ci(.github/workflows): add ci-statoscope.yaml

--- packages/ui/build.app.config.ts
+++ packages/ui/build.app.config.ts
@@ -1,5 +1,17 @@
 import {ServiceConfig} from '@gravity-ui/app-builder';
 
+const analyzeBundle: Required<ServiceConfig>['client']['analyzeBundle'] = process.env
+    .ANALYZE_BUNDLE as any;
+
+if (analyzeBundle) {
+    console.log({analyzeBundle}, '\n');
+}
+
+const debugPort = process.env.DEBUG_PORT ? Number(process.env.DEBUG_PORT) : undefined;
+if (debugPort) {
+    console.log({debugPort}, '\n');
+}
+
 const client: ServiceConfig['client'] = {
     watchOptions: {
         aggregateTimeout: 1000,
@@ -13,11 +25,13 @@ const client: ServiceConfig['client'] = {
     },
     hiddenSourceMap: false,
     disableReactRefresh: true,
+    analyzeBundle,
 };
 
 const server: ServiceConfig['server'] = {
     watch: ['dist/shared'],
     watchThrottle: 1000,
+    inspectBrk: debugPort,
 };
 
 export default {client, server};
---
+++ packages/ui/statoscope.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    validate: {
+        // add webpack plugin with rules
+        plugins: ['@statoscope/webpack'],
+        reporters: [
+            // console-reporter to output results into console (enabled by default)
+            '@statoscope/console',
+            // reporter that generates UI-report with validation-results
+            ['@statoscope/stats-report', {saveStatsTo: 'validated.json'}],
+        ],
+        // rules to validate your stats (use all of them or only specific rules)
+        rules: {
+            // ensures that bundle hasn't package duplicates
+            // TODO: akaretnikov@ вернуть no-packages-dups после дедупликации пакетов
+            '@statoscope/webpack/no-packages-dups': [
+                'warn',
+                {
+                    exclude: [],
+                },
+            ],
+            // ensure that the download time of entrypoints is not over the limit (10 sec)
+            // '@statoscope/webpack/entry-download-time-limits': [
+            //     'error',
+            //     {global: {maxDownloadTime: 10000}},
+            // ],
+            // ensure that the download size of entrypoints is not over the limit (20 mb)
+            // it includes async chunks so is not that important, therefore warn
+            '@statoscope/webpack/entry-download-size-limits': [
+                'warn',
+                {global: {maxSize: 20 * 1024 * 1024}},
+            ],
+            // ensures that bundle doesn't have modules with deoptimization
+            // '@statoscope/webpack/no-modules-deopts': ['warn'],
+            // diff download size of entrypoints between input and reference stats. Fails if size diff is over the limit (10 kb)
+            '@statoscope/webpack/diff-entry-download-size-limits': [
+                'error',
+                {byName: [{name: 'main', limits: {maxInitialSizeDiff: 10 * 1024}}]},
+            ],
+            // diff download time of entrypoints between input and reference stats. Fails if download time is over the limit (500 ms)
+            '@statoscope/webpack/diff-entry-download-time-limits': [
+                'error',
+                {byName: [{name: 'main', limits: {maxInitialDownloadTimeDiff: 500}}]},
+            ],
+            // compares usage of specified modules between input and reference stats
+            // '@statoscope/webpack/diff-deprecated-modules': ['error', [/\/path\/to\/module\.js/]],
+            // compares usage of specified packages usage between input and reference stats. Fails if rxjs usage has increased
+            // '@statoscope/webpack/diff-deprecated-packages': ['error', ['lodash/chain']],
+        },
+    },
+};
