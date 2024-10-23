dabd1175ee29fcb91d23a8d77e55621c7a201008
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-10-24 02:31:02 +0300

    chore(makeNavigationLink): add 'navmode' parameter [YTFRONT-4119]

--- packages/ui/src/ui/utils/app-url.ts
+++ packages/ui/src/ui/utils/app-url.ts
@@ -1,6 +1,8 @@
 import {Page} from '../../shared/constants/settings';
 import {Tab as ComponentsTab} from '../constants/components/main';
 import YT from '../config/yt-config';
+import type {Tab as NavigationTab} from '../constants/navigation';
+import {ValueOf} from '../types';
 
 export function makeComponentsNodesUrl({host, cluster}: {host?: string; cluster?: string} = {}) {
     return host
@@ -12,8 +14,17 @@ export function makeProxyUrl(cluster: string, address: string) {
     return `/${cluster}/${Page.COMPONENTS}/${ComponentsTab.RPC_PROXIES}?host=${address}`;
 }
 
-export function makeNavigationLink(path: string, cluster?: string) {
-    return `/${cluster || YT.cluster}/${Page.NAVIGATION}?path=${path}`;
+export function makeNavigationLink({
+    path,
+    cluster,
+    navmode,
+}: {
+    path: string;
+    cluster?: string;
+    navmode?: ValueOf<typeof NavigationTab>;
+}) {
+    const res = `/${cluster || YT.cluster}/${Page.NAVIGATION}?path=${path}`;
+    return navmode ? `${res}&navmode=${navmode}` : res;
 }
 
 export function makeSchedulingUrl({
