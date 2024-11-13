ef28721e6a07ba6e4fb780e2b5be0396e7963a06
simbiozizv <simbiozizv@yandex-team.ru>
2024-11-13 15:20:09 +0300

    chore(System): master instance rewrite js to tsx [YTFRONT-4452]

--- packages/ui/src/ui/pages/system/Masters/Instance.tsx
+++ packages/ui/src/ui/pages/system/Masters/Instance.tsx
@@ -1,7 +1,6 @@
-import React, {Component, Fragment} from 'react';
-import PropTypes from 'prop-types';
+import React, {FC} from 'react';
 import hammer from '../../../common/hammer';
-import NodeQuad from '../NodeQuad/NodeQuad';
+import NodeQuad, {NodeQuadTheme} from '../NodeQuad/NodeQuad';
 import {Tooltip} from '../../../components/Tooltip/Tooltip';
 import Icon from '../../../components/Icon/Icon';
 import ReadOnlyIcon from '../../../assets/img/svg/read-only-icon.svg';
@@ -12,110 +11,96 @@ import ClipboardButton from '../../../components/ClipboardButton/ClipboardButton
 import {ChangeMaintenanceButton} from './ChangeMaintenanceButton';
 import block from 'bem-cn-lite';
 import {MasterInstance} from '../../../store/selectors/system/masters';
-import ypath from '../../../common/thor/ypath';
 import './MasterGroup.scss';
 
 const b = block('master-group');
 
-export class Instance extends Component {
-    static instanceStateToTheme = {
-        active: 'online',
-        connected: 'online',
-        leading: 'online',
-        disconnected: 'banned',
-        following: 'following',
-        follower_recovery: 'banned',
-        leader_recovery: 'banned',
-        standby: 'banned',
-        elections: 'offline',
-        stopped: 'offline',
-        unknown: 'offline',
-        online: 'online',
-        offline: 'offline',
-    };
+const instanceStateToTheme: Record<string, NodeQuadTheme> = {
+    active: 'online',
+    connected: 'online',
+    leading: 'online',
+    disconnected: 'banned',
+    following: 'following',
+    follower_recovery: 'banned',
+    leader_recovery: 'banned',
+    standby: 'banned',
+    elections: 'offline',
+    stopped: 'offline',
+    unknown: 'offline',
+    online: 'online',
+    offline: 'offline',
+};
 
-    static propTypes = {
-        instance: MasterInstance,
-        hostType: PropTypes.string.isRequired,
-        allowVoting: PropTypes.bool,
-        allowService: PropTypes.bool,
-    };
+type Props = {
+    instance: MasterInstance;
+    hostType: string;
+    allowVoting?: boolean;
+    allowService?: boolean;
+};
 
-    render() {
-        const {instance, hostType, allowVoting, allowService} = this.props;
-        const {state, $address, $physicalAddress, $attributes, $rowAddress} = instance;
-        const address = hostType === 'host' ? $address : $physicalAddress;
-        const maintenance = ypath.getValue($rowAddress, '/attributes/maintenance');
-        const maintenanceMessage = maintenance
-            ? ypath.getValue($rowAddress, '/attributes/maintenance_message') || 'Maintenance'
-            : '';
-        const {voting} = $attributes ?? {};
-        // do not use `!voting` cause `voting === undefined` is the same as `voting === true`
-        const denyVoting = allowVoting && voting === false;
-        const theme =
-            denyVoting && state === 'following'
-                ? 'nonvoting'
-                : Instance.instanceStateToTheme[state];
-        const addressWithoutPort = hammer.format['Address'](address);
+export const Instance: FC<Props> = ({instance, hostType, allowVoting, allowService}) => {
+    const {state, $address, $physicalAddress} = instance;
+    const attributes = instance.$attributes as Record<string, any>;
+    const address = hostType === 'host' ? $address : $physicalAddress;
+    const maintenance = instance.getMaintenance();
+    const maintenanceMessage = maintenance ? instance.getMaintenanceMessage() || 'Maintenance' : '';
+    const {voting} = attributes ?? {};
+    // do not use `!voting` cause `voting === undefined` is the same as `voting === true`
+    const denyVoting = allowVoting && voting === false;
+    const theme = denyVoting && state === 'following' ? 'nonvoting' : instanceStateToTheme[state!];
+    const addressWithoutPort = hammer.format['Address'](address);
 
-        /* eslint-disable camelcase */
-        return (
-            <Fragment>
-                <div className={b('availability')}>
-                    <NodeQuad theme={theme} />
-                </div>
-                <div className={b('role')}>
-                    <span>{hammer.format['ReadableField'](state ? state : 'unknown')}</span>
-                </div>
-                <div className={b('icon')}>
-                    {maintenanceMessage && (
-                        <Tooltip content={<span>{maintenanceMessage}</span>}>
-                            <Icon
-                                className={b('icon-maintenance')}
-                                awesome="digging"
-                                face="solid"
-                            />
-                        </Tooltip>
-                    )}
-                    {$attributes?.read_only && (
-                        <span className={b('icon-glyph')} title="Read only">
-                            <ReadOnlyIcon width={14} height={14} />
-                        </span>
-                    )}
-                    {$attributes?.warming_up && (
-                        <span className={b('icon-glyph')} title="Warming up">
-                            <WarmUpIcon width={14} height={14} />
-                        </span>
-                    )}
-                </div>
-                <div className={b('host')}>
-                    <Tooltip content={addressWithoutPort}>
-                        <div className={b('host-name')}>
-                            {makeShortSystemAddress(addressWithoutPort) || addressWithoutPort}
-                        </div>
+    return (
+        <>
+            <div className={b('availability')}>
+                <NodeQuad theme={theme} />
+            </div>
+            <div className={b('role')}>
+                <span>{hammer.format['ReadableField'](state ? state : 'unknown')}</span>
+            </div>
+            <div className={b('icon')}>
+                {maintenanceMessage && (
+                    <Tooltip content={<span>{maintenanceMessage}</span>}>
+                        <Icon className={b('icon-maintenance')} awesome="digging" face="solid" />
                     </Tooltip>
-                    <Flex gap={1}>
-                        <span className={b('host-btn')}>
-                            <ClipboardButton view="flat-secondary" text={address} />
-                        </span>
-                        {allowService && (
-                            <ChangeMaintenanceButton
-                                className={b('host-btn')}
-                                address={address}
-                                maintenance={maintenance}
-                                maintenanceMessage={maintenanceMessage}
-                                type="master"
-                            />
-                        )}
-                        {
-                            <Text className={b('nonvoting', {show: denyVoting})} color="secondary">
-                                [nonvoting]
-                            </Text>
-                        }
-                    </Flex>
-                </div>
-            </Fragment>
-        );
-        /* eslint-enable camelcase */
-    }
-}
+                )}
+                {attributes?.read_only && (
+                    <span className={b('icon-glyph')} title="Read only">
+                        <ReadOnlyIcon width={14} height={14} />
+                    </span>
+                )}
+                {attributes?.warming_up && (
+                    <span className={b('icon-glyph')} title="Warming up">
+                        <WarmUpIcon width={14} height={14} />
+                    </span>
+                )}
+            </div>
+            <div className={b('host')}>
+                <Tooltip content={addressWithoutPort}>
+                    <div className={b('host-name')}>
+                        {makeShortSystemAddress(addressWithoutPort) || addressWithoutPort}
+                    </div>
+                </Tooltip>
+                <Flex gap={1}>
+                    <span className={b('host-btn')}>
+                        <ClipboardButton view="flat-secondary" text={address} />
+                    </span>
+                    {allowService && (
+                        <ChangeMaintenanceButton
+                            className={b('host-btn')}
+                            address={address}
+                            maintenance={maintenance}
+                            maintenanceMessage={maintenanceMessage}
+                            type="master"
+                        />
+                    )}
+                    {
+                        <Text className={b('nonvoting', {show: denyVoting})} color="secondary">
+                            [nonvoting]
+                        </Text>
+                    }
+                </Flex>
+            </div>
+        </>
+    );
+};
