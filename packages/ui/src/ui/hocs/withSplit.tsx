4f03479f042577f71d2098cc61983a7166412b22
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-11-16 17:56:09 +0300

    chore: extact useSidePanel hook [YTFRONT-4463]

--- packages/ui/src/ui/hocs/withSplit.tsx
+++ packages/ui/src/ui/hocs/withSplit.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {getDisplayName} from '../utils';
 import {SPLIT_PANE_ID} from '../constants/index';
 
-export default function withSplit<P>(Component: React.ComponentType<P>) {
+export default function withSplit<P>(Component: React.ComponentType<P>): React.ComponentType<P> {
     return class WithSplit extends React.Component<P> {
         static displayName = `WithSplit(${getDisplayName(Component)})`;
 
