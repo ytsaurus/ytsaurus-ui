ebc80778172570e056728a72cf2a450eb9037359
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-11-19 18:00:26 +0300

    chore(UIFactory/AclApi): add AclApi.isAllowedToEditColumnGroups [YTFRONT-3836]

--- packages/ui/src/ui/utils/acl/external-acl-api.ts
+++ packages/ui/src/ui/utils/acl/external-acl-api.ts
@@ -75,6 +75,8 @@ export interface AclApi {
         editInheritance?: string;
         editColumnsAcl?: string;
     };
+
+    isAllowedToEditColumnGroups(params: {nodeType?: string}): boolean;
 }
 
 export interface GetAclParams {
@@ -151,6 +153,10 @@ export const defaultAclApi: AclApi = {
     createColumnGroup: () => methodNotSupported('createColumnGroup'),
     editColumnGroup: () => methodNotSupported('editColumnGroup'),
     deleteColumnGroup: () => methodNotSupported('deleteColumnGroup'),
+
+    isAllowedToEditColumnGroups({nodeType}) {
+        return nodeType === 'map_node' || nodeType === 'table';
+    },
 };
 
 function methodNotSupported(method: string) {
