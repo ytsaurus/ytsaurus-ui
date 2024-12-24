be970fc2cf65fe6596d32028e10b3292047e363f
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-12-24 19:07:43 +0300

    chore: minor fix for styles

--- packages/ui/src/ui/pages/job/tabs/Statistics/Statistics.tsx
+++ packages/ui/src/ui/pages/job/tabs/Statistics/Statistics.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import {useSelector} from 'react-redux';
+import cn from 'bem-cn-lite';
+
 import {getRawStatistic} from '../../../../store/selectors/job/statistics';
 import {StatisticTable, StatisticTree} from '../../../../components/StatisticTable';
 import {isDocsAllowed} from '../../../../config';
 import UIFactory from '../../../../UIFactory';
 
+import './Statistics.scss';
+
+const block = cn('yt-job-statistics');
+
 export default function Statistics() {
     const statistic = useSelector(getRawStatistic);
     return (
         <StatisticTable
+            className={block()}
             helpUrl={isDocsAllowed() ? UIFactory.docsUrls['problems:jobstatistics'] : undefined}
             statistic={statistic as unknown as StatisticTree}
             visibleColumns={['min', 'max', 'last', 'sum', 'count']}
