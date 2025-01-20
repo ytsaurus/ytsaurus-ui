df5efe5b0a9d74af8e52d935a17480794ad70437
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-01-21 01:52:20 +0300

    test(Settings): fix for flacky test

--- packages/ui/tests/screenshots/pages/settings/settings.base.screen.ts
+++ packages/ui/tests/screenshots/pages/settings/settings.base.screen.ts
@@ -1,19 +1,21 @@
 import {expect, test} from '@playwright/test';
 import {makeClusterUrl} from '../../../utils';
+import {basePage} from '../../../utils/BasePage';
 
 test('Settings: checking the functionality of the menu', async ({page}) => {
     await page.goto(makeClusterUrl('path-viewer'));
     await page.waitForSelector('.gn-aside-header__footer');
 
-    page.click('.gn-footer-item');
-    await page.waitForSelector('.settings-panel');
-    let header = await page.textContent('.gn-settings__section-heading');
-    expect(header).toEqual('General');
-    await expect(page).toHaveScreenshot();
+    await basePage(page).settingsToggleVisibility();
 
-    page.click('.gn-settings-menu__item[data-id="/Table"]');
-    await page.waitForSelector('.gn-settings-menu__item_selected[data-id="/Table"]');
-    header = await page.textContent('.gn-settings__section-heading');
-    expect(header).toEqual('Table');
-    await expect(page).toHaveScreenshot();
+    await test.step('General', async () => {
+        await page.waitForSelector('.gn-settings__section-heading:text("General")');
+        await expect(page).toHaveScreenshot();
+    });
+
+    await test.step('Table', async () => {
+        await basePage(page).settingsShowSection('Table');
+        await page.waitForSelector('.gn-settings__section-heading:text("Table")');
+        await expect(page).toHaveScreenshot();
+    });
 });
