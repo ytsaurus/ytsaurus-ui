9f0b20b02e4237499220cad51ace014776a0d779
Viktor Rozaev <vrozaev@tracto.ai>
2025-01-28 16:19:54 +0100

    refactor: rewrite src/ui/constants/navigation/modals/delete-object.ts to typescript

--- packages/ui/src/ui/constants/navigation/modals/delete-object.ts
+++ packages/ui/src/ui/constants/navigation/modals/delete-object.ts
@@ -3,10 +3,10 @@ import createActionTypes, {createPrefix} from '../../../constants/utils';
 
 const PREFIX = createPrefix(Page.NAVIGATION);
 
-export const DELETE_OBJECT = createActionTypes(PREFIX + 'DELETE_OBJECT');
-export const LOAD_REAL_PATH = createActionTypes(PREFIX + 'LOAD_REAL_PATH');
-export const OPEN_DELETE_OBJECT_POPUP = PREFIX + 'OPEN_DELETE_OBJECT_POPUP';
-export const CLOSE_DELETE_OBJECT_POPUP = PREFIX + 'CLOSE_DELETE_OBJECT_POPUP';
-export const TOGGLE_PERMANENTLY_DELETE = PREFIX + 'TOGGLE_PERMANENTLY_DELETE';
+export const DELETE_OBJECT = createActionTypes(`${PREFIX}DELETE_OBJECT`);
+export const LOAD_REAL_PATH = createActionTypes(`${PREFIX}LOAD_REAL_PATH`);
+export const OPEN_DELETE_OBJECT_POPUP = `${PREFIX}OPEN_DELETE_OBJECT_POPUP` as const;
+export const CLOSE_DELETE_OBJECT_POPUP = `${PREFIX}CLOSE_DELETE_OBJECT_POPUP` as const;
+export const TOGGLE_PERMANENTLY_DELETE = `${PREFIX}TOGGLE_PERMANENTLY_DELETE` as const;
 
 export const SUPPRESS_REDIRECT = '&';
