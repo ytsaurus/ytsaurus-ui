b87d696b18f121c4e7080c1ee2b8f527e101a540
simbiozizv <simbiozizv@yandex-team.ru>
2025-02-25 09:19:54 +0300

    feat(Operations): jobs timeline [YTFRONT-4695]

---
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/JobsTimeline/JobsTimeline.tsx
@@ -0,0 +1,60 @@
+import React, {FC, useEffect} from 'react';
+import {TimelineRuler} from '../../../../../components/common/Timeline/TimelineRuler/TimelineRuler';
+import './JobsTimeline.scss';
+import cn from 'bem-cn-lite';
+import {Loader} from '@gravity-ui/uikit';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+    selectEventsInInterval,
+    selectEventsInterval,
+    selectInterval,
+    selectLoading,
+} from '../../../../../store/selectors/operations/jobs-timeline';
+import {getJobsWithEvents} from '../../../../../store/actions/operations/jobs-timeline';
+import {
+    Interval,
+    setInterval,
+} from '../../../../../store/reducers/operations/jobs/jobs-timeline-slice';
+import {EventsTimeline} from './EventsTimeline';
+import {TimeLineHeader} from './TimeLineHeader';
+import {JobsTable} from './JobsTable';
+
+const block = cn('yt-jobs-timeline');
+
+export const JobsTimeline: FC = () => {
+    const dispatch = useDispatch();
+    const events = useSelector(selectEventsInInterval);
+    const eventsRange = useSelector(selectEventsInterval);
+    const isLoading = useSelector(selectLoading);
+    const interval = useSelector(selectInterval);
+
+    useEffect(() => {
+        dispatch(getJobsWithEvents(true));
+    }, [dispatch]);
+
+    if (isLoading) return <Loader />;
+    if (!interval || !eventsRange) return null;
+
+    const handleRulerChange = (data: Interval) => {
+        dispatch(setInterval(data));
+    };
+
+    return (
+        <div className={block()}>
+            <TimeLineHeader />
+            <TimelineRuler
+                {...interval}
+                hasScaleButtons
+                minRange={eventsRange.from}
+                maxRange={eventsRange.to}
+                hasNowButton={false}
+                onUpdate={handleRulerChange}
+                className={block('timeline-ruler')}
+            />
+            <div className={block('content')}>
+                <JobsTable events={events} />
+                <EventsTimeline events={events} interval={interval} />
+            </div>
+        </div>
+    );
+};
---
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/JobsTimeline/helpers/prepareJobTimeline.ts
@@ -0,0 +1,47 @@
+import {JobsTimelineState} from '../../../../../../store/reducers/operations/jobs/jobs-timeline-slice';
+import {TimelineAxis} from '../../../../../../packages/ya-timeline';
+import {JobLineEvent} from '../../../../../../packages/ya-timeline/components/Events/JobLineRenderer';
+import {getColorByState} from './getColorByState';
+import {ROW_HEIGHT} from '../constants';
+
+export const prepareJobTimeline = (events: JobsTimelineState['events']) => {
+    return Object.keys(events).reduce<{axes: TimelineAxis[]; timelines: JobLineEvent[]}>(
+        (acc, jobId, i) => {
+            acc.axes.push({
+                id: jobId,
+                top: ROW_HEIGHT * i,
+                height: ROW_HEIGHT,
+                tracksCount: 1,
+            });
+
+            const jobEvents = events[jobId];
+            acc.timelines.push({
+                renderType: 'jobLine',
+                axisId: jobId,
+                eventsCount: jobEvents.length,
+                trackIndex: 0,
+                from: jobEvents[0].startTime,
+                to: jobEvents.at(-1)?.endTime,
+                parts: jobEvents.reduce<JobLineEvent['parts']>((a, event) => {
+                    a.push({
+                        percentage: event.percent,
+                        color: getColorByState(event.state),
+                        state: event.state,
+                        interval: {
+                            from: event.startTime,
+                            to: event.endTime,
+                        },
+                        phases: event.phases,
+                    });
+                    return a;
+                }, []),
+            });
+
+            return acc;
+        },
+        {
+            axes: [],
+            timelines: [],
+        },
+    );
+};
