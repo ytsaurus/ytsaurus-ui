adeebaf1e2b3dcc17cc164fb29e81c4fad7e9751
simbiozizv <simbiozizv@yandex-team.ru>
2025-02-25 09:19:54 +0300

    feat(Operations): jobs timeline [YTFRONT-4695]

---
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/JobsTimeline/EventsTimeline/EventsTimeline.tsx
@@ -0,0 +1,105 @@
+import React, {FC, useCallback, useMemo, useState} from 'react';
+import {Timeline} from './EventCanvas';
+import {
+    Interval,
+    TimelineJob,
+} from '../../../../../../store/reducers/operations/jobs/jobs-timeline-slice';
+import {
+    EventsSelectedEvent,
+    HoverEvent,
+} from '../../../../../../packages/ya-timeline/components/Events';
+import './EventsTimeline.scss';
+import cn from 'bem-cn-lite';
+import {prepareJobTimeline} from '../helpers/prepareJobTimeline';
+import {JobLineEvent} from './renderer/JobLineRenderer';
+import {EventTimelineTooltip} from './EventTimelineTooltip';
+import {ROW_HEIGHT} from '../constants';
+import {useThemeValue} from '@gravity-ui/uikit';
+import {BoundsChangedEvent} from '../../../../../../packages/ya-timeline';
+
+const block = cn('yt-events-timeline');
+
+type Props = {
+    jobs: TimelineJob[];
+    selectedJob?: string[];
+    interval: Interval;
+    filter?: string;
+    onTimelineClick: (jobId: {id: string; timelineId: string}) => void;
+    onBoundsChanged: (interval: Interval) => void;
+};
+
+export const EventsTimeline: FC<Props> = ({
+    jobs,
+    selectedJob,
+    interval,
+    filter,
+    onTimelineClick,
+    onBoundsChanged,
+}) => {
+    const theme = useThemeValue();
+    const [hoverEvent, setHoverEvent] = useState<HoverEvent<JobLineEvent>>();
+
+    const {axes, timelines} = useMemo(() => {
+        return prepareJobTimeline({jobs, selectedJob, filter});
+    }, [filter, jobs, selectedJob]);
+
+    const handleHoverEvent = useCallback((e: HoverEvent<JobLineEvent>) => {
+        setHoverEvent(e);
+    }, []);
+
+    const handleLeftEvent = useCallback(() => {
+        setHoverEvent(undefined);
+    }, []);
+
+    const handleEventClick = useCallback(
+        (e: EventsSelectedEvent<JobLineEvent>) => {
+            const events = e.detail.events;
+            if (!events.length) {
+                onTimelineClick({id: '', timelineId: ''});
+                return;
+            }
+
+            const job = events.find((event) => event.renderType === 'jobLine');
+            if (!job) return;
+
+            onTimelineClick({
+                id: job.jobId,
+                timelineId: `jobLine:${job.axisId}:${job.from}-${job.to}`,
+            });
+        },
+        [onTimelineClick],
+    );
+
+    const scrollTopChanged = useCallback(
+        ({detail}: BoundsChangedEvent) => {
+            onBoundsChanged({from: detail.start, to: detail.end});
+        },
+        [onBoundsChanged],
+    );
+
+    return (
+        <div
+            className={block()}
+            onMouseLeave={handleLeftEvent}
+            style={{
+                height: `${axes.length * ROW_HEIGHT}px`,
+            }}
+        >
+            {hoverEvent && <EventTimelineTooltip events={hoverEvent.detail} />}
+
+            <Timeline
+                theme={theme}
+                start={interval.from}
+                end={interval.to}
+                axes={axes}
+                selectedEvents={selectedJob}
+                events={timelines}
+                eventsSelected={handleEventClick}
+                hoverEvent={handleHoverEvent}
+                leftEvent={handleLeftEvent}
+                isZoomAllowed
+                boundsChanged={scrollTopChanged}
+            />
+        </div>
+    );
+};
---
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/JobsTimeline/JobsGroups/JobsGroups.tsx
@@ -0,0 +1,125 @@
+import React, {FC, useCallback, useEffect, useMemo} from 'react';
+import {JobsGroupCollapse} from './JobsGroupCollapse';
+import cn from 'bem-cn-lite';
+import './JobsGroups.scss';
+import {JobsTable} from '../JobsTable';
+import {EventsTimeline} from '../EventsTimeline';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+    selectActiveJob,
+    selectFilter,
+    selectInterval,
+    selectJobGroupsCount,
+    selectJobsInIntervalByGroup,
+} from '../../../../../../store/selectors/operations/jobs-timeline';
+import {
+    Interval,
+    setInterval,
+    setSelectedJob,
+} from '../../../../../../store/reducers/operations/jobs/jobs-timeline-slice';
+import hammer from '../../../../../../common/hammer';
+import {useSidePanel} from '../../../../../../hooks/use-side-panel';
+import {EventsSidePanel} from '../EventsSidePanel';
+
+const block = cn('yt-timeline-event-group');
+
+export const JobsGroups: FC = () => {
+    const dispatch = useDispatch();
+    const jobGroups = useSelector(selectJobsInIntervalByGroup);
+    const groupsCount = useSelector(selectJobGroupsCount);
+    const interval = useSelector(selectInterval);
+    const {timelineId} = useSelector(selectActiveJob);
+    const filter = useSelector(selectFilter);
+
+    const handleTimeLineClick = useCallback(
+        (value: {id: string; timelineId: string}) => {
+            dispatch(setSelectedJob(value));
+        },
+        [dispatch],
+    );
+
+    const handleBoundsChanged = useCallback(
+        (newInterval: Interval) => {
+            dispatch(setInterval(newInterval));
+        },
+        [dispatch],
+    );
+
+    const handleSidePanelClose = useCallback(
+        (onClose: () => void) => () => {
+            handleTimeLineClick({id: '', timelineId: ''});
+            onClose();
+        },
+        [handleTimeLineClick],
+    );
+
+    const handleSidePanelOutsideClick = useCallback(
+        (onClose: () => void) => (e: MouseEvent) => {
+            if (e.target instanceof Element && e.target.localName !== 'events-timeline-canvas') {
+                handleTimeLineClick({id: '', timelineId: ''});
+                onClose();
+            }
+        },
+        [handleTimeLineClick],
+    );
+
+    const {openWidget, closeWidget, widgetContent} = useSidePanel('JobsTimeline', {
+        renderContent({onClose}) {
+            return (
+                <EventsSidePanel
+                    onClose={handleSidePanelClose(onClose)}
+                    onOutsideClick={handleSidePanelOutsideClick(onClose)}
+                />
+            );
+        },
+    });
+
+    useEffect(() => {
+        const operation = timelineId ? openWidget : closeWidget;
+        operation();
+    }, [closeWidget, openWidget, timelineId]);
+
+    const groups = useMemo(() => {
+        if (!interval) return [];
+
+        const groupNames = Object.keys(jobGroups).sort();
+        return groupNames.map((groupName) => {
+            const group = jobGroups[groupName].sort(
+                (a, b) => Number(a.cookieId) - Number(b.cookieId),
+            );
+            const readableGroupName = hammer.format['ReadableField'](groupName);
+            const title = `${readableGroupName} (${group.length} / ${groupsCount[groupName]})`;
+
+            return (
+                <JobsGroupCollapse key={groupName} title={title}>
+                    <div className={block()}>
+                        <JobsTable name={readableGroupName} jobs={group} />
+                        <EventsTimeline
+                            jobs={group}
+                            selectedJob={[timelineId]}
+                            interval={interval}
+                            filter={filter}
+                            onTimelineClick={handleTimeLineClick}
+                            onBoundsChanged={handleBoundsChanged}
+                        />
+                    </div>
+                </JobsGroupCollapse>
+            );
+        });
+    }, [
+        filter,
+        groupsCount,
+        handleBoundsChanged,
+        handleTimeLineClick,
+        interval,
+        jobGroups,
+        timelineId,
+    ]);
+
+    return (
+        <>
+            {groups}
+            <>{widgetContent}</>
+        </>
+    );
+};
