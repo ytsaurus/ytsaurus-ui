c2b7d71cc9111e439ed724f861b3d7e8c278b228
simbiozizv <simbiozizv@yandex-team.ru>
2025-02-25 09:19:54 +0300

    feat(Operations): jobs timeline [YTFRONT-4695]

---
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/JobsTimeline/JobsTable.tsx
@@ -0,0 +1,28 @@
+import React, {FC} from 'react';
+import {Flex, Icon, Link, Text} from '@gravity-ui/uikit';
+import FileIcon from '@gravity-ui/icons/svgs/file.svg';
+import cn from 'bem-cn-lite';
+import './JobsTable.scss';
+
+const block = cn('yt-jobs-table');
+
+type Props = {
+    jobIds: string[];
+};
+
+export const JobsTable: FC<Props> = ({jobIds}) => {
+    return (
+        <Flex direction="column" className={block()}>
+            {jobIds.map((id) => (
+                <Flex alignItems="center" gap={1} key={id} className={block('item')}>
+                    <Icon data={FileIcon} size={16} />
+                    <Link href={`${id}/details`} target="_blank">
+                        <Text variant="body-1" ellipsis>
+                            {id}
+                        </Text>
+                    </Link>
+                </Flex>
+            ))}
+        </Flex>
+    );
+};
