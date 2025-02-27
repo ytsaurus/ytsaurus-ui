c758f1d9aac782816e42c980d89824ff5d5b6c8b
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-02-27 13:54:40 +0300

    feat(UIFactory): rework `UIFactory.renderAppFooter` method [YTFRONT-4149]

--- packages/ui/src/ui/containers/RootPage/RootPage.tsx
+++ packages/ui/src/ui/containers/RootPage/RootPage.tsx
@@ -8,7 +8,6 @@ import {HeaderWithLinks} from '../../containers/ClustersMenu/HeaderLinks';
 import {ALL_LINKS_ITEMS} from '../../containers/ClustersMenu/header-links-items';
 
 import './RootPage.scss';
-import UIFactory from '../../UIFactory';
 
 const block = cn('yt-root-page');
 
@@ -35,7 +34,6 @@ function RootPage({title, children, currentPathname}: Props) {
             <div className={block('content')}>
                 <div className={'elements-main-section'}>{children}</div>
             </div>
-            {UIFactory.renderAppFooter()}
         </React.Fragment>
     );
 }
