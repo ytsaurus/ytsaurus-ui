6cbacbb84f936be0cd665e794c38daf50ae6ef26
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-03-08 16:03:47 +0300

    chore(components/Block): better classNameo [YTFRONT-3310]

--- packages/ui/src/ui/components/Error/Error.js
+++ packages/ui/src/ui/components/Error/Error.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 
 import Block from '../../components/Block/Block';
 
-import './Error.scss';
-
 const propTypes = Block.propTypes;
 
 export default class Error extends Component {
