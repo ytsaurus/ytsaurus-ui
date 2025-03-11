00762a3668dd2c27101ddd0bedc30f1150855f74
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-03-11 20:28:08 +0300

    chore(store/reducers/navigation/navigation): rewrite with typescript [YTFRONT-4729]

--- packages/ui/src/ui/constants/navigation/index.ts
+++ packages/ui/src/ui/constants/navigation/index.ts
@@ -28,20 +28,20 @@ export const ContentMode = {
 
 export const DEFAULT_PATH = '/';
 
-export const SET_MODE = PREFIX + 'SET_MODE';
-export const SET_SELECTED_ITEM = PREFIX + 'SET_SELECTED_ITEM';
-export const SELECT_ALL = PREFIX + 'SELECT_ALL';
-export const SET_CONTENT_MODE = PREFIX + 'SET_CONTENT_MODE';
-export const SET_TEXT_FILTER = PREFIX + 'SET_TEXT_FILTER';
-export const SET_TRANSACTION = PREFIX + 'SET_TRANSACTION';
-export const CLEAR_TRANSACTION = PREFIX + 'CLEAR_TRANSACTION';
-export const UPDATE_PATH = PREFIX + 'UPDATE_PATH';
+export const SET_MODE = `${PREFIX}SET_MODE` as const;
+export const SET_SELECTED_ITEM = `${PREFIX}SET_SELECTED_ITEM` as const;
+export const SELECT_ALL = `${PREFIX}SELECT_ALL` as const;
+export const SET_CONTENT_MODE = `${PREFIX}SET_CONTENT_MODE` as const;
+export const SET_TEXT_FILTER = `${PREFIX}SET_TEXT_FILTER` as const;
+export const SET_TRANSACTION = `${PREFIX}SET_TRANSACTION` as const;
+export const CLEAR_TRANSACTION = `${PREFIX}CLEAR_TRANSACTION` as const;
+export const UPDATE_PATH = `${PREFIX}UPDATE_PATH` as const;
 export const UPDATE_VIEW = createActionTypes(`${PREFIX}UPDATE_VIEW`);
-export const NAVIGATION_PARTIAL = `${PREFIX}NAVIGATION_PARTIAL`;
+export const NAVIGATION_PARTIAL = `${PREFIX}NAVIGATION_PARTIAL` as const;
 
 export const FETCH_NODES = createActionTypes(`${PREFIX}FETCH_NODES`);
 export const UPDATE_RESOURCE_USAGE = createActionTypes(`${PREFIX}UPDATE_RESOURCE_USAGE`);
 
-export const NAVIGATION_MAP_NODE_TABLE_ID = 'navigation/map-nodes';
+export const NAVIGATION_MAP_NODE_TABLE_ID = 'navigation/map-nodes' as const;
 export const SET_MEDIUM_TYPE = PREFIX + 'SET_MEDIUM_TYPE';
 export const APPLY_CUSTOM_SORT = PREFIX + 'APPLY_CUSTOM_SORT';
