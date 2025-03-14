2a1685acb28f6d3f7e3e37d863d6850023d46762
simbiozizv <simbiozizv@yandex-team.ru>
2025-03-14 13:18:51 +0300

    fix(Queries): schema fix [YTFRONT-4771]

--- packages/ui/src/ui/pages/query-tracker/Navigation/helpers/createTableSelect.ts
+++ packages/ui/src/ui/pages/query-tracker/Navigation/helpers/createTableSelect.ts
@@ -4,6 +4,7 @@ import ypath from '../../../../common/thor/ypath';
 import {QueryEngine} from '../../module/engines';
 import {makePathByQueryEngine} from './makePathByQueryEngine';
 import {ClusterConfig} from '../../../../../shared/yt-types';
+import unipika from '../../../../common/thor/unipika';
 
 type Props = (data: {
     clusterConfig: ClusterConfig;
@@ -17,7 +18,7 @@ export const createTableSelect: Props = async ({clusterConfig, path, engine, lim
     const schema: NavigationTableSchema[] = ypath.getValue(attributes.schema);
 
     return `SELECT
-${schema.map((i) => '    `' + i.name + '`').join(',\r\n')}
+${schema.map((i) => '    ' + unipika.prettyprint(i.name, {asHTML: false}).replaceAll('"', '`')).join(',\r\n')}
 FROM ${makePathByQueryEngine({path, cluster: clusterConfig.id, engine})}
 LIMIT ${limit}`;
 };
