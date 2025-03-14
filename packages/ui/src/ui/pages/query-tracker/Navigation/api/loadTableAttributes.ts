5fe2fd95f981e51488f10dba23139391dfa950d8
simbiozizv <simbiozizv@yandex-team.ru>
2025-03-14 13:18:51 +0300

    fix(Queries): schema fix [YTFRONT-4771]

--- packages/ui/src/ui/pages/query-tracker/Navigation/api/loadTableAttributes.ts
+++ packages/ui/src/ui/pages/query-tracker/Navigation/api/loadTableAttributes.ts
@@ -1,24 +1,56 @@
-import {wrapApiPromiseByToaster} from '../../../../utils/utils';
+import {getBatchError} from '../../../../utils/utils';
 import {YTApiId, ytApiV3Id} from '../../../../rum/rum-wrap-api';
 import {JSONParser} from '../../module/api';
 import {getClusterProxy} from '../../../../store/selectors/global';
-import {ClusterConfig} from '../../../../../shared/yt-types';
+import {BatchSubRequest, ClusterConfig} from '../../../../../shared/yt-types';
+import {Toaster} from '@gravity-ui/uikit';
 
-export const loadTableAttributes = (path: string, clusterConfig: ClusterConfig) => {
-    return wrapApiPromiseByToaster<Record<string, any>>(
-        ytApiV3Id.get(YTApiId.navigationAttributes, {
-            setup: {
-                proxy: getClusterProxy(clusterConfig),
-                ...JSONParser,
-            },
+const toaster = new Toaster();
+
+export const loadTableAttributes = async (path: string, clusterConfig: ClusterConfig) => {
+    const setup = {
+        proxy: getClusterProxy(clusterConfig),
+        ...JSONParser,
+    };
+
+    const requests: BatchSubRequest[] = [
+        {
+            command: 'get' as const,
             parameters: {
                 path: `${path}/@`,
             },
-        }),
-        {
-            skipSuccessToast: true,
-            toasterName: 'query_navigation_node_attributes',
-            errorTitle: 'Navigation node get attributes failure',
+            setup,
         },
-    );
+        {
+            command: 'get' as const,
+            parameters: {
+                path: `${path}/@schema`,
+            },
+            setup,
+        },
+    ];
+
+    try {
+        const results = await ytApiV3Id.executeBatch(YTApiId.navigationGetPath, {
+            parameters: {requests},
+        });
+
+        const error = getBatchError(results, 'Failed to get navigation attributes');
+        if (error) throw error;
+
+        return {
+            ...results[0].output,
+            schema: {
+                ...results[1].output,
+            },
+        };
+    } catch (e) {
+        toaster.add({
+            name: 'load_table_attributes',
+            theme: 'danger',
+            title: 'Failed to load table attributes',
+            content: (e as Error).message,
+        });
+        return {};
+    }
 };
