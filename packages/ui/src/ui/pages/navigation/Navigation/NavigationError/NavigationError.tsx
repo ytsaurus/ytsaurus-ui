d581b5d15cdd57a18640ef32c11a7a9c8c192858
simbiozizv <simbiozizv@yandex-team.ru>
2025-03-21 17:16:45 +0300

    chore(Navigation): navigation error vertical variant [YTFRONT-4802]

--- packages/ui/src/ui/pages/navigation/Navigation/NavigationError/NavigationError.tsx
+++ packages/ui/src/ui/pages/navigation/Navigation/NavigationError/NavigationError.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cn from 'bem-cn-lite';
-import {Flex, Text} from '@gravity-ui/uikit';
+import {Flex, FlexProps, Text} from '@gravity-ui/uikit';
 
 import {YTErrorBlock} from '../../../../components/Error/Error';
 import ErrorDetails from '../../../../components/ErrorDetails/ErrorDetails';
@@ -20,23 +20,31 @@ type Props = {
     details: YTError;
     cluster: string;
     message: string;
+    vertical?: boolean;
 };
 
 function PrettyError(props: Props & {code: ErrorCode}) {
-    const {details, path, cluster, code} = props;
+    const {details, path, cluster, code, vertical} = props;
 
     const error = code == 901 ? getPermissionDeniedError(details)! : details;
     const title = getErrorTitle({...error, code}, path);
+    const direction: FlexProps['direction'] = vertical ? 'column' : undefined;
 
     return (
-        <Flex className={block()} justifyContent="center" alignItems="center" gap={7}>
+        <Flex
+            className={block()}
+            justifyContent="center"
+            alignItems="center"
+            direction={direction}
+            gap={7}
+        >
             <Flex>
                 <NavigationErrorImage type={code} />
             </Flex>
             <Flex direction="column" className={block('info')} gap={4}>
                 <Text className={block('title')}>{title}</Text>
                 <ErrorDetails error={details} />
-                <Flex direction="row" gap={3}>
+                <Flex gap={3} direction={direction}>
                     {code === 901 ? (
                         <RequestPermission cluster={cluster} path={path} error={error} />
                     ) : (
