035815cb2b3dcd20e9a2ee065b644acef456f664
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-03-21 13:29:32 +0300

    fix(Operation/Statistics): add copy button for values [YTFRONT-4805]

--- packages/ui/src/ui/pages/operations/OperationDetail/tabs/statistics/OperationStatisticName.tsx
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/statistics/OperationStatisticName.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {Flex} from '@gravity-ui/uikit';
 
 import {getOperationStatisticsDescription} from '../../../../../store/selectors/global/supported-features';
 import {Tooltip} from '../../../../../components/Tooltip/Tooltip';
 import format from '../../../../../common/hammer/format';
 import {StatisticName, formatByUnit} from '../../../../../components/StatisticTable';
+import ClipboardButton from '../../../../../components/ClipboardButton/ClipboardButton';
 
 function useStatisticInfo(name: string) {
     const {getStatisticInfo} = useSelector(getOperationStatisticsDescription);
@@ -30,7 +32,23 @@ function OperationStatisticValueImpl({
     const asStr = formatByUnit(value, info?.unit, settings);
     const asNumber = format.Number(value, settings);
 
-    return asStr !== undefined ? <Tooltip content={asNumber}>{asStr}</Tooltip> : asNumber;
+    return asStr !== undefined ? (
+        <Tooltip
+            placement={['right', 'left']}
+            content={
+                <Flex gap={1}>
+                    <span> {asNumber}</span>
+                    {!isNaN(value!) && (
+                        <ClipboardButton text={`${value}`} inlineMargins view="flat-outlined" />
+                    )}
+                </Flex>
+            }
+        >
+            {asStr}
+        </Tooltip>
+    ) : (
+        asNumber
+    );
 }
 
 export const OperationStatisticValue = React.memo(OperationStatisticValueImpl);
