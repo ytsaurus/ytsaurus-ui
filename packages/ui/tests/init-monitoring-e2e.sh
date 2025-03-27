c3ee82f2aace19f59762c4be1eca4f5ffedf6ebd
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-03-28 02:09:37 +0300

    chore(Monitring): add files with dashboards description [YTFRONT-4388]

---
+++ packages/ui/tests/init-monitoring-e2e.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+if ! which yt >/dev/null; then
+    echo You have to install YT CLI manually, please 1>&2
+    exit 1
+fi
+
+if [ -z "${YT_PROXY}" ]; then
+    echo You have to provide YT_PROXY environment variable 1>&2
+    exit 2
+fi
+
+# Import known dashboards
+pushd $(dirname $0)/data/monitoring/json
+ls | xargs -I {} bash -c "
+    echo -n Creating document {}...;
+    yt create -r -i document //sys/interface-monitoring/{} >/dev/null;
+    yt set --format json //sys/interface-monitoring/{} < {};
+    echo OK;
+"
