8738c0217df23cc219d90a72fc1b1d069aa1ac9b
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-03-29 20:33:06 +0300

    chore(Monitoring): use `@gravity-ui/dashkit` to render layout [YTFRONT-4388]

--- packages/ui/src/shared/constants/yt-api-id.ts
+++ packages/ui/src/shared/constants/yt-api-id.ts
@@ -198,4 +198,6 @@ export const enum YTApiId {
 
     //pipeline/flow
     flowExecuteDescribePipeline = 'flowExecuteDescribePipeline',
+
+    prometheusMonitoringLayout = 'prometheusMonitoringLayout',
 }
