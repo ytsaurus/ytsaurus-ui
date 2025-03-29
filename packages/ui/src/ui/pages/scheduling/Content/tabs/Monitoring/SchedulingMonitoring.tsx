0232af7c09371b3d0d68e643d28e0f2e1fcde049
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-03-29 20:33:06 +0300

    chore(Monitoring): use `@gravity-ui/dashkit` to render layout [YTFRONT-4388]

---
+++ packages/ui/src/ui/pages/scheduling/Content/tabs/Monitoring/SchedulingMonitoring.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+import {PrometheusDashboardLazy} from '../../../../../containers/PrometheusDashboard/lazy';
+
+import {getCluster} from '../../../../../store/selectors/global';
+import {getPool, getTree} from '../../../../../store/selectors/scheduling/scheduling';
+
+export function SchedulingMonitoring() {
+    const cluster = useSelector(getCluster);
+    const pool = useSelector(getPool);
+    const tree = useSelector(getTree);
+
+    const params = React.useMemo(() => {
+        return {cluster, tree, pool};
+    }, [cluster, tree, pool]);
+
+    return <PrometheusDashboardLazy type="scheduler-pool" params={params} />;
+}
