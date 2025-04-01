c805ca07c98a9ebed9a651aa2f86cefbb8641ec0
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-02 00:38:24 +0300

    chore(Monitoring): add raw implementation of 'prometheus.timeseries' without view, data loading only [YTFRONT-4388]

--- packages/ui/src/ui/utils/format.ts
+++ packages/ui/src/ui/utils/format.ts
@@ -10,3 +10,20 @@ export function formatByParams(template: string, params: Record<string, {toStrin
         return acc.replace(new RegExp(`{${key}}`, 'g'), params[key].toString());
     }, template);
 }
+
+/**
+ * Replaces occurrences of strings like `"$key1"`, `"$key2"`
+ * by corresponding values from params, i.e. `"params['key1']"`, `"params['key2']"`
+ * @param template
+ * @param params
+ * @example formatByParamas('hello {user}', {user: 'world'}); // returns 'hello world'
+ */
+export function formatByPramsQuotedEnv(
+    template: string,
+    params: Record<string, {toString(): string}>,
+) {
+    return Object.keys(params).reduce((acc, key) => {
+        const res = acc.replace(new RegExp(`"\\$${key}"`, 'g'), `"${params[key]}"`);
+        return res;
+    }, template);
+}
