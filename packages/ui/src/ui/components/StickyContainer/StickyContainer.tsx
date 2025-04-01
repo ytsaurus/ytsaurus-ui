34040cac948079157cd1a35132e9fb32388259f1
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-01 17:56:23 +0300

    chore: extract `useIntersection` hook, add `IntersectionObserverContainer` [YTFRONT-4388]

--- packages/ui/src/ui/components/StickyContainer/StickyContainer.tsx
+++ packages/ui/src/ui/components/StickyContainer/StickyContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import cn from 'bem-cn-lite';
 import {HEADER_HEIGHT} from '../../constants';
+import {useIntersectionRatio} from '../../hooks/use-intersection';
 
 import './StickyContainer.scss';
 
@@ -13,40 +14,21 @@ export function StickyContainer({
     topOffset?: number;
     children: (params: {sticky: boolean; topStickyClassName?: string}) => React.ReactNode;
 }) {
-    const [sticky, setSticky] = React.useState(false);
     const [element, setElement] = React.useState<HTMLDivElement | null>(null);
 
-    const observer = React.useMemo(() => {
-        return new IntersectionObserver(
-            (entries) => {
-                if (entries[0].intersectionRatio === 0) {
-                    setSticky(true);
-                } else if (entries[0].intersectionRatio === 1) {
-                    setSticky(false);
-                }
-            },
-            {threshold: [0, 1], rootMargin: `${-topOffset ?? 0}px 0px 0px 0px`},
-        );
-    }, [topOffset]);
+    const intersectionRatio = useIntersectionRatio({
+        element,
+        options: {
+            threshold: [0, 1],
+            rootMargin: `${-topOffset}px 0px 0px 0px`,
+        },
+    });
 
-    React.useEffect(() => {
-        if (element) {
-            observer.observe(element);
-        }
-        return () => {
-            if (element) {
-                observer.unobserve(element);
-            }
-        };
-    }, [element]);
-
-    const onRef = React.useCallback((div: HTMLDivElement | null) => {
-        setElement(div);
-    }, []);
+    const sticky = intersectionRatio !== 1;
 
     return (
         <div className={block()}>
-            <div className={block('top')} ref={onRef} />
+            <div className={block('top')} ref={setElement} />
             {children({
                 sticky,
                 topStickyClassName: sticky ? block('sticky', {top: true}) : undefined,
