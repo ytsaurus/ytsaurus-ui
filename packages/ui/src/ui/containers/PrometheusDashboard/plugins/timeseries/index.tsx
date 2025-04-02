990b659059429fa30e17c39b2d9d4b8d32b3943d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-02 20:52:54 +0300

    chore(Monitoring): use YagrChartKit to rendear  [YTFRONT-4388]

--- packages/ui/src/ui/containers/PrometheusDashboard/plugins/timeseries/index.tsx
+++ packages/ui/src/ui/containers/PrometheusDashboard/plugins/timeseries/index.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import axios from 'axios';
+import cn from 'bem-cn-lite';
 
 import {Flex} from '@gravity-ui/uikit';
 
 import {YTError} from '../../../../../@types/types';
 
+import format from '../../../../common/hammer/format';
 import {formatByPramsQuotedEnv} from '../../../../utils/format';
 import {IntersectionObserverContainer} from '../../../../components/IntersectionObserverContainer/IntersectionObserverContainer';
 import {TimeseriesTarget} from '../../../../containers/PrometheusDashboard/PrometheusDashboard';
-import YagrChartKit, {YagrWidgetData} from '../../../../components/YagrChartKit/YagrChartKit';
+import {YTChartKitLazy, getSerieColor} from '../../../../components/YTChartKit';
+import {YagrWidgetData} from '@gravity-ui/chartkit/yagr';
 import {InlineError} from '../../../../components/InlineError/InlineError';
 import CancelHelper, {isCancelled} from '../../../../utils/cancel-helper';
 import Loader from '../../../../components/Loader/Loader';
+import {humanizeInterval} from '../../../../components/common/Timeline/util';
 
 import {PrometheusPlugins} from '../../PrometheusDashKit';
+
+import './timeseries.scss';
+
+const block = cn('yt-prometheus-timeseries');
 
 export const renderPluginTimeseries: PrometheusPlugins['timeseries']['renderer'] = (
     props,
     elementRef,
 ) => {
     return (
-        <IntersectionObserverContainer ref={elementRef}>
+        <IntersectionObserverContainer key={props.id} className={block()} ref={elementRef}>
             <PrometheusChart {...props} />
         </IntersectionObserverContainer>
     );
@@ -31,24 +39,26 @@ type Props = TimeseriesParameters[0];
 type PrometheusChartProps = Props;
 
 function PrometheusChart({data}: PrometheusChartProps) {
-    const {title, targets, params} = data;
+    const {title} = data;
 
-    const {error, data: chartData, loading} = useLoadQueriesData(targets, params);
+    const {error, data: chartData, loading} = useLoadQueriesData(data);
 
     return (
-        <Flex direction="column">
-            {title}
-            {error && <InlineError error={error} />}
-            {!chartData && loading && <Loader visible centered />}
-            {chartData && <YagrChartKit type="yagr" data={chartData} />}
+        <Flex className={block('widget')} direction="column">
+            {chartData ? (
+                <YTChartKitLazy type="yagr" data={chartData} />
+            ) : (
+                <>
+                    <div className={block('widget-title')}>{title}</div>
+                    {error && <InlineError error={error} />}
+                    {!chartData && loading && <Loader visible centered />}
+                </>
+            )}
         </Flex>
     );
 }
 
-function useLoadQueriesData(
-    targets: Array<TimeseriesTarget>,
-    params: Record<string, {toString(): string}>,
-) {
+function useLoadQueriesData({title, targets, params}: PrometheusChartProps['data']) {
     const [cancelHelper] = React.useState(new CancelHelper());
     const [chartData, setChartData] = React.useState<{
         data?: YagrWidgetData;
@@ -67,7 +77,7 @@ function useLoadQueriesData(
             const end = Date.now() / 1000;
             const start = end - 3600;
             const step = Math.max(1, Math.floor((end - start) / 250));
-            const query = formatByPramsQuotedEnv(item.expr, params);
+            const query = replaceExprParams(item.expr, params, step);
             return axios
                 .get<QueryRangeResponse>('/api/prometheus/query_range', {
                     params: {query, start, end, step},
@@ -78,7 +88,7 @@ function useLoadQueriesData(
 
         Promise.all(promises)
             .then((results) => {
-                setChartData({data: makeYagrWidgetData(targets, results), loading: false});
+                setChartData({data: makeYagrWidgetData(title, targets, results), loading: false});
             })
             .catch((error) => {
                 setChartData({error: isCancelled(error) ? undefined : error, loading: false});
@@ -88,14 +98,68 @@ function useLoadQueriesData(
     return chartData;
 }
 
+const SPECIAL_EXPR_ENV = {
+    $__rate_interval: calculateRateInterval,
+};
+
+/**
+ * Poor implementation of
+ * https://github.com/grafana/grafana/blob/192d3783d5bede8362c1eed0c27422f431478b5a/pkg/promlib/models/query.go#L345-L366
+ */
+function calculateRateInterval(stepSec: number) {
+    const minStep = 15000;
+    return humanizeInterval(0, Math.max(stepSec * 1000 + minStep, minStep * 4));
+}
+
+function replaceExprParams(
+    expr: string,
+    params: Record<string, {toString(): string}>,
+    stepSec: number,
+) {
+    let res = formatByPramsQuotedEnv(expr, params);
+    for (const k of Object.keys(SPECIAL_EXPR_ENV)) {
+        const key = k as keyof typeof SPECIAL_EXPR_ENV;
+        if (res.indexOf(key)) {
+            res = res.replace(key, SPECIAL_EXPR_ENV[key](stepSec));
+        }
+    }
+    return res;
+}
+
 function makeYagrWidgetData(
+    title: string,
     targets: Array<TimeseriesTarget>,
     results: Array<QueryRangeResponse>,
 ): YagrWidgetData {
-    //const res: YagrWidgetData = {};
-    console.log({targets, results});
-    // @ts-expect-error
-    return {targets, results};
+    const res: YagrWidgetData = {
+        data: {graphs: [], timeline: []},
+        libraryConfig: {title: {text: title}, legend: {show: true}},
+    };
+    for (let serie = 0; serie < results?.length; ++serie) {
+        const graph: (typeof res)['data']['graphs'][number] = {
+            name: format.ReadableField(targets[serie].legendFormat),
+            data: [],
+            formatter: format.Number,
+            color: getSerieColor(serie),
+        };
+        res.data.graphs.push(graph);
+
+        const emtyTimline = res.data.timeline.length === 0;
+
+        const metricData = results[serie]?.data?.result?.[0];
+        for (let i = 0; i < metricData?.values?.length; ++i) {
+            const [t, valueStr] = metricData.values[i];
+            if (emtyTimline) {
+                res.data.timeline.push(t * 1000);
+            }
+            graph.data.push(Number(valueStr));
+        }
+
+        if (graph.data.length === 0) {
+            res.data.graphs.pop();
+        }
+    }
+    return res;
 }
 
 type QueryRangeResponse = {
