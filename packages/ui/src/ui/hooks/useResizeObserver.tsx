7cf9cb2602a139552c2e705ae001aa3ad124747f
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-03 15:38:40 +0300

    chore(Monitoring): use chart width to calculate pointCount [YTFRONT-4388]

--- packages/ui/src/ui/hooks/useResizeObserver.tsx
+++ packages/ui/src/ui/hooks/useResizeObserver.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
+import debounce_ from 'lodash/debounce';
 
 interface Props {
     element?: Element;
     onResize: ResizeObserverCallback;
+    debounceMs?: number;
 }
 
-export default function useResizeObserver({element, onResize}: Props) {
+export default function useResizeObserver({element, onResize, debounceMs = 300}: Props) {
+    const handleResize = React.useMemo(() => {
+        const onResizeDebounced = debounce_(onResize, debounceMs);
+        return (...args: Parameters<typeof onResizeDebounced>) => {
+            onResizeDebounced(...args);
+        };
+    }, [onResize, debounceMs]);
+
     React.useEffect(() => {
         if (element) {
-            const observer = new ResizeObserver(onResize);
+            const observer = new ResizeObserver(handleResize);
             observer.observe(element);
             return () => {
                 if (element) {
@@ -19,5 +28,16 @@ export default function useResizeObserver({element, onResize}: Props) {
         } else {
             return undefined;
         }
-    }, [element, onResize]);
+    }, [element, handleResize]);
+}
+
+export function useElementSize({
+    element,
+    debounceMs,
+}: Pick<Props, 'element'> & {debounceMs?: number}) {
+    const [entries, setEntries] = React.useState<Array<ResizeObserverEntry>>();
+
+    useResizeObserver({element, onResize: setEntries, debounceMs});
+
+    return entries?.[0];
 }
