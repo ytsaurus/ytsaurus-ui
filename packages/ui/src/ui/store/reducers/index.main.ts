cc6c970c80fbbcd23e2c151ac1b362e5f8b9c2a5
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-04 00:31:19 +0300

    chore(Monitoring): add url-mapping for /scheduling/monitoring [YTFRONT-4388]

--- packages/ui/src/ui/store/reducers/index.main.ts
+++ packages/ui/src/ui/store/reducers/index.main.ts
@@ -43,6 +43,7 @@ import {chyt} from './chyt';
 import {getMainLocations} from '../../store/location.main';
 import {flow} from '../../store/reducers/flow';
 import {rootApi} from '../../store/api';
+import {prometheusDashboardSlice} from './prometheusDashboard/prometheusDahsboard';
 
 const appReducers = {
     acl,
@@ -84,6 +85,7 @@ const appReducers = {
     manageTokens,
     flow,
     [rootApi.reducerPath]: rootApi.reducer,
+    prometheusDashboard: prometheusDashboardSlice.reducer,
 };
 
 export type RootState = ReturnType<ReturnType<typeof makeRootReducer>>;
