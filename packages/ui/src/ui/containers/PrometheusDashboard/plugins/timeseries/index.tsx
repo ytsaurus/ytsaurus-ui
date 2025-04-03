7021beb157ffde9cb9280f0e7fdde19b815d5093
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-04 00:31:19 +0300

    chore(Monitoring): add url-mapping for /scheduling/monitoring [YTFRONT-4388]

--- packages/ui/src/ui/containers/PrometheusDashboard/plugins/timeseries/index.tsx
+++ packages/ui/src/ui/containers/PrometheusDashboard/plugins/timeseries/index.tsx
@@ -9,7 +9,7 @@ import {YTError} from '../../../../../@types/types';
 import format from '../../../../common/hammer/format';
 import {formatByPramsQuotedEnv} from '../../../../utils/format';
 import {IntersectionObserverContainer} from '../../../../components/IntersectionObserverContainer/IntersectionObserverContainer';
-import {TimeseriesTarget} from '../../../../containers/PrometheusDashboard/PrometheusDashboard';
+
 import {YTChartKitLazy, getSerieColor} from '../../../../components/YTChartKit';
 import {YagrWidgetData} from '@gravity-ui/chartkit/yagr';
 import {InlineError} from '../../../../components/InlineError/InlineError';
@@ -21,6 +21,7 @@ import {useElementSize} from '../../../../hooks/useResizeObserver';
 import {PrometheusPlugins} from '../../PrometheusDashKit';
 import {PrometheusWidgetToolbar} from '../../PrometheusWidgetToolbar/PrometheusWidgetToolbar';
 import {usePrometheusDashboardContext} from '../../PrometheusDashboardContext/PrometheusDashboardContext';
+import {TimeseriesTarget} from '../../types';
 
 import './timeseries.scss';
 
@@ -37,7 +38,10 @@ function ExpandablePrometheusChart({
     elementRef,
     ...props
 }: PrometheusChartProps & {elementRef: React.Ref<any>}) {
-    const {expandedId} = usePrometheusDashboardContext();
+    const {
+        expandedId,
+        timeRangeFilter: {from, to},
+    } = usePrometheusDashboardContext();
 
     return (
         <IntersectionObserverContainer
@@ -45,7 +49,7 @@ function ExpandablePrometheusChart({
             className={block({expanded: props.id === expandedId})}
             ref={elementRef}
         >
-            <PrometheusChart {...props} />
+            <PrometheusChart {...props} from={from} to={to} />
         </IntersectionObserverContainer>
     );
 }
@@ -54,13 +58,18 @@ type TimeseriesParameters = Parameters<PrometheusPlugins['timeseries']['renderer
 type Props = TimeseriesParameters[0];
 type PrometheusChartProps = Props;
 
-function PrometheusChart({data, id}: PrometheusChartProps) {
+function PrometheusChart({
+    data,
+    id,
+    from,
+    to,
+}: PrometheusChartProps & {from?: number; to?: number}) {
     const {title} = data;
     const [element, setElement] = React.useState<HTMLDivElement | null>(null);
 
     const pointCount = useElementSize({element: element as Element})?.contentRect.width;
 
-    const {error, data: chartData, loading} = useLoadQueriesData(data, pointCount);
+    const {error, data: chartData, loading} = useLoadQueriesData({data, pointCount, from, to});
 
     return (
         <React.Fragment>
@@ -80,10 +89,12 @@ function PrometheusChart({data, id}: PrometheusChartProps) {
     );
 }
 
-function useLoadQueriesData(
-    {title, targets, params}: PrometheusChartProps['data'],
-    pointCount?: number,
-) {
+function useLoadQueriesData({
+    data: {title, targets, params},
+    pointCount,
+    from,
+    to,
+}: Pick<PrometheusChartProps, 'data'> & {pointCount?: number; from?: number; to?: number}) {
     const [cancelHelper] = React.useState(new CancelHelper());
     const [chartData, setChartData] = React.useState<{
         data?: YagrWidgetData;
@@ -92,20 +103,22 @@ function useLoadQueriesData(
     }>({});
 
     React.useEffect(() => {
-        if (!pointCount) {
+        if (!pointCount || from === undefined || to === undefined) {
             return;
         }
 
         cancelHelper.removeAllRequests();
         setChartData({loading: true});
+
+        const end = to / 1000;
+        const start = from / 1000;
+        const step = Math.max(1, Math.floor((end - start) / Math.max(10, pointCount)));
+
         /**
          * Temporary solution without storing results in store
          * TODO: use rtk-query later
          */
         const promises = targets.map((item) => {
-            const end = Date.now() / 1000;
-            const start = end - 3600;
-            const step = Math.max(1, Math.floor((end - start) / Math.max(10, pointCount)));
             const query = replaceExprParams(item.expr, params, step);
             return axios
                 .get<QueryRangeResponse>('/api/prometheus/query_range', {
@@ -117,12 +130,15 @@ function useLoadQueriesData(
 
         Promise.all(promises)
             .then((results) => {
-                setChartData({data: makeYagrWidgetData(title, targets, results), loading: false});
+                setChartData({
+                    data: makeYagrWidgetData(title, targets, results, {end, start, step}),
+                    loading: false,
+                });
             })
             .catch((error) => {
                 setChartData({error: isCancelled(error) ? undefined : error, loading: false});
             });
-    }, [targets, params, cancelHelper, title, pointCount]);
+    }, [targets, params, cancelHelper, title, pointCount, from, to]);
 
     return chartData;
 }
@@ -159,32 +175,45 @@ function makeYagrWidgetData(
     title: string,
     targets: Array<TimeseriesTarget>,
     results: Array<QueryRangeResponse>,
+    {end, start, step}: {end: number; start: number; step: number},
 ): YagrWidgetData {
     const res: YagrWidgetData = {
         data: {graphs: [], timeline: []},
-        libraryConfig: {title: {text: title}, legend: {show: true}},
+        libraryConfig: {
+            title: {text: title},
+            legend: {show: true},
+        },
     };
+
+    for (let t = start; t - step * 0.5 < end; t += step) {
+        res.data.timeline.push(t * 1000);
+    }
+
+    const {timeline} = res.data;
+
     for (let serie = 0; serie < results?.length; ++serie) {
         const graph: (typeof res)['data']['graphs'][number] = {
             name: format.ReadableField(targets[serie].legendFormat),
-            data: [],
+            data: new Array(timeline.length),
             formatter: format.Number,
             color: getSerieColor(serie),
         };
         res.data.graphs.push(graph);
 
-        const emtyTimline = res.data.timeline.length === 0;
+        const {values = []} = results[serie]?.data?.result?.[0] ?? {};
 
-        const metricData = results[serie]?.data?.result?.[0];
-        for (let i = 0; i < metricData?.values?.length; ++i) {
-            const [t, valueStr] = metricData.values[i];
-            if (emtyTimline) {
-                res.data.timeline.push(t * 1000);
-            }
-            graph.data.push(Number(valueStr));
+        if (!values.length) {
+            continue;
         }
 
-        if (graph.data.length === 0) {
+        let i = 0;
+        for (; i < values.length; ++i) {
+            const [t, valueStr] = values[i];
+            const pos = Math.round((t - start) / step);
+            graph.data[pos] = Number(valueStr);
+        }
+
+        if (i === 0) {
             res.data.graphs.pop();
         }
     }
---
+++ packages/ui/src/ui/store/reducers/prometheusDashboard/prometheusDahsboard.tsx
@@ -0,0 +1,42 @@
+import {PayloadAction, createSlice} from '@reduxjs/toolkit';
+import {EMPTY_OBJECT} from '../../../constants/empty';
+import type {PrometheusWidgetId} from '../../../containers/PrometheusDashboard/types';
+
+export type PrometheusDashboardType = 'scheduler-pool';
+
+export type PrometheusDashboardState = {
+    expandedPanels: Partial<Record<PrometheusDashboardType, PrometheusWidgetId>>;
+
+    timeRangeFilter:
+        | {shortcutValue: string; from?: number; to?: number}
+        | {from: number; to: number; shortcutValue?: undefined};
+};
+
+export const initialState: PrometheusDashboardState = {
+    expandedPanels: EMPTY_OBJECT,
+    timeRangeFilter: {shortcutValue: '30m'},
+};
+
+export const prometheusDashboardSlice = createSlice({
+    initialState,
+    name: 'prometheusDashboard',
+    reducers: {
+        setExpandedId(
+            state,
+            {
+                payload: {type, id},
+            }: PayloadAction<{
+                type: PrometheusDashboardType;
+                id: PrometheusWidgetId | undefined;
+            }>,
+        ) {
+            const {[type]: _prevId, ...rest} = state.expandedPanels;
+            const res = id === undefined ? rest : {...state, [type]: id};
+
+            return {...state, expandedPanels: Object.keys(res).length === 0 ? EMPTY_OBJECT : res};
+        },
+        setTimeRangeFilter(state, {payload}: PayloadAction<Pick<typeof state, 'timeRangeFilter'>>) {
+            return {...state, ...payload};
+        },
+    },
+});
