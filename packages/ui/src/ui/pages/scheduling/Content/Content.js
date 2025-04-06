d4b6551244a567ab19b91971ae18f5b4c4eec83d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-07 00:35:52 +0300

    chore(Scheduling): rename Tab to SchedulingTab [YTFRONT-4388]

--- packages/ui/src/ui/pages/scheduling/Content/Content.js
+++ packages/ui/src/ui/pages/scheduling/Content/Content.js
@@ -12,7 +12,11 @@ import {OverviewWithRum} from '../../../pages/scheduling/Content/tabs/Overview/O
 import Details from '../../../pages/scheduling/Content/tabs/Details/Details';
 import ErrorBoundary from '../../../components/ErrorBoundary/ErrorBoundary';
 
-import {DEFAULT_TAB, SCHEDULING_ALLOWED_ROOT_TABS, Tab} from '../../../constants/scheduling';
+import {
+    DEFAULT_TAB,
+    SCHEDULING_ALLOWED_ROOT_TABS,
+    SchedulingTab,
+} from '../../../constants/scheduling';
 import PoolAcl from '../../../pages/scheduling/Content/tabs/PoolAcl/PoolAcl';
 import {
     getIsRoot,
@@ -54,10 +58,10 @@ function Content({className, match, location}) {
     const isRoot = useSelector(getIsRoot);
     const allowAcl = useSelector(isPoolAclAllowed);
 
-    const localTab = {...Tab};
+    const localTab = {...SchedulingTab};
 
     const showSettings = reduce_(
-        Tab,
+        SchedulingTab,
         (acc, tab) => {
             acc[tab] = {show: SCHEDULING_ALLOWED_ROOT_TABS[tab] || !isRoot};
             return acc;
@@ -67,7 +71,7 @@ function Content({className, match, location}) {
 
     const titleDict = {};
 
-    const aclTab = showSettings[Tab.ACL];
+    const aclTab = showSettings[SchedulingTab.ACL];
     aclTab.show = aclTab.show && allowAcl;
 
     const extraTabs = [
@@ -110,7 +114,7 @@ function Content({className, match, location}) {
     });
 
     delete localTab.ACL;
-    localTab[Tab.ACL] = Tab.ACL;
+    localTab[SchedulingTab.ACL] = SchedulingTab.ACL;
 
     const props = makeTabProps(match.url, localTab, showSettings, {pool, tree}, titleDict);
 
@@ -127,10 +131,15 @@ function Content({className, match, location}) {
                 />
 
                 <Switch>
-                    <Route path={`${match.path}/${Tab.OVERVIEW}`} component={OverviewWithRum} />
-                    <Route path={`${match.path}/${Tab.DETAILS}`} component={Details} />
+                    <Route
+                        path={`${match.path}/${SchedulingTab.OVERVIEW}`}
+                        component={OverviewWithRum}
+                    />
+                    <Route path={`${match.path}/${SchedulingTab.DETAILS}`} component={Details} />
                     {extraRoutes}
-                    {aclTab.show && <Route path={`${match.path}/${Tab.ACL}`} component={PoolAcl} />}
+                    {aclTab.show && (
+                        <Route path={`${match.path}/${SchedulingTab.ACL}`} component={PoolAcl} />
+                    )}
                     <Route path={`${match.path}/:tab`} component={Placeholder} />
                     <Redirect
                         from={match.url}
