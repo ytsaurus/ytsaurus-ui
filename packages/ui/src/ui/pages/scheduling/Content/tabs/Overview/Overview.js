e972c93bd9a36136463e56f97a6f7691f770af1b
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-07 00:38:51 +0300

    feat(Monitoring): add Prometheus dashboards integration [YTFRONT-4388]

--- packages/ui/src/ui/pages/scheduling/Content/tabs/Overview/Overview.js
+++ packages/ui/src/ui/pages/scheduling/Content/tabs/Overview/Overview.js
@@ -358,7 +358,13 @@ class Overview extends Component {
             >
                 {Overview.renderType(item, ...rest, allowExpand)}
                 <span className={block('name-content-name')}>
-                    {Overview.renderName(item, SchedulingTab.OVERVIEW, cluster, currentPool, changePool)}
+                    {Overview.renderName(
+                        item,
+                        SchedulingTab.OVERVIEW,
+                        cluster,
+                        currentPool,
+                        changePool,
+                    )}
                 </span>
                 {Overview.renderEphemeralIcon(item)}
                 {Overview.renderEffectiveLightweightIcon(item)}
