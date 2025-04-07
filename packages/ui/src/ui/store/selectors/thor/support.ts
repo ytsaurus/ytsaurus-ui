c9019088ea9446bc46a6a9361b1db45cb3b1f7aa
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-07 19:26:57 +0300

    chore(selectors/thor/support): get rid of 'schedulingChildrenByPool' feature

--- packages/ui/src/ui/store/selectors/thor/support.ts
+++ packages/ui/src/ui/store/selectors/thor/support.ts
@@ -124,9 +124,6 @@ const FEATURES = {
     nodeMaintenanceApi: {
         proxy: '23.1.11106567',
     } as FeatureVersions,
-    schedulingChildrenByPool: {
-        scheduler: '23.1.11146742',
-    } as FeatureVersions,
 };
 
 export function _isFeatureSupported<T extends Record<string, FeatureVersions>>(
@@ -194,12 +191,5 @@ export const isSupportedNodeMaintenanceApi = createSelector(
     [isSupportedSelector],
     (isSupported) => {
         return isSupported('nodeMaintenanceApi');
-    },
-);
-
-export const isSupportedSchedulingChildrenByPool = createSelector(
-    [isSupportedSelector],
-    (isSupported) => {
-        return isSupported('schedulingChildrenByPool');
     },
 );
