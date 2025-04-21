a3c85e024be4f4726f64377bf4e0c6ef65d2856d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-21 17:26:15 +0300

    feat(Accounts): add Prometheus dashboard inegration [YTFRONT-4388]

--- packages/ui/src/ui/UIFactory/default-ui-factory.tsx
+++ packages/ui/src/ui/UIFactory/default-ui-factory.tsx
@@ -22,6 +22,7 @@ import {QUERY_RESULT_CHART_TAB} from '../pages/query-tracker/QueryResultsVisuali
 import {IncarnationsLazy} from '../pages/operations/OperationDetail/tabs/incarnations/IncarnationsLazy';
 import {AppNavigationPageLayoutLazy} from '../containers/AppNavigation/AppNavigationPageLayout.lazy';
 
+import {AccountsMonitorPrometheus} from '../pages/accounts/tabs/monitor/AccountsMonitorPromehteus/AccountsMonitorPrometheus';
 import {QueueMetricsPrometheus} from '../pages/navigation/tabs/Queue/views/QueueMetrics/QueueMetricsPrometheus/QueueMetricsPrometheus';
 import {ConsumerMetricsPrometheus} from '../pages/navigation/tabs/Consumer/views/ConsumerMetrics/ConsumerMetricsPrometheus/ConsumerMetricsPrometheus';
 
@@ -96,6 +97,10 @@ export const defaultUIFactory: UIFactory = {
         return uiSettings.componentVersionsMonitoring;
     },
     getMonitoringForAccounts() {
+        if (getConfigData().allowPrometheusDashboards) {
+            return {component: AccountsMonitorPrometheus};
+        }
+
         if (!uiSettings?.accountsMonitoring?.urlTemplate) {
             return undefined;
         }
