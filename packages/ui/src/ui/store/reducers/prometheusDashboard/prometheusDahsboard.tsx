5138eb3e0df1a9e0a2c17ad18e909b49e1ba06e9
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-23 16:57:35 +0300

    feat(Bundles): add Prometheus dashboard integration [YTFRONT-4388]

--- packages/ui/src/ui/store/reducers/prometheusDashboard/prometheusDahsboard.tsx
+++ packages/ui/src/ui/store/reducers/prometheusDashboard/prometheusDahsboard.tsx
@@ -4,6 +4,16 @@ import type {PrometheusWidgetId} from '../../../containers/PrometheusDashboard/t
 
 export type PrometheusDashboardType =
     | 'account'
+    | 'bundle-ui-user-load'
+    | 'bundle-ui-resource'
+    | 'bundle-ui-cpu'
+    | 'bundle-ui-memory'
+    | 'bundle-ui-disk'
+    | 'bundle-ui-lsm'
+    | 'bundle-ui-network'
+    | 'bundle-ui-efficiency'
+    | 'bundle-ui-rpc-proxy-overview'
+    | 'bundle-ui-rpc-proxy'
     | 'chyt-monitoring'
     | 'cluster-resources'
     | 'master-global'
