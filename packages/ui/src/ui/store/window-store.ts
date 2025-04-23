6c51810174da67801ed69b334459bd1b48cd03d0
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-04-23 04:10:42 +0300

    chore: prepare for delayed loading of reducers [YTFRONT-4388]

--- packages/ui/src/ui/store/window-store.ts
+++ packages/ui/src/ui/store/window-store.ts
@@ -1,5 +1,7 @@
 import type {AppBrowserHistory, StoreType} from './store.main';
 import {MakeRotedUrlFnType} from './location';
+import {CombinedSliceReducer, Slice, SliceCaseReducers, SliceSelectors} from '@reduxjs/toolkit';
+import {RootState} from './reducers';
 
 export function getWindowStore(): StoreType {
     return (window as any).store;
@@ -10,4 +12,39 @@ export const makeRoutedURL: MakeRotedUrlFnType = (...args) =>
 
 export function getAppBrowserHistory(): AppBrowserHistory {
     return (window as any).appBrowserHistory;
+}
+
+export function injectReducers<
+    K extends string,
+    SS,
+    SCR extends SliceCaseReducers<SS>,
+    N extends string,
+    Selectors extends SliceSelectors<SS>,
+>(reducerPath: K, slice: Slice<SS, SCR, N, K, Selectors>) {
+    if ((getWindowStore().getState() as any)[reducerPath] !== undefined) {
+        throw new Error(`Unexpected behavior: state['${reducerPath}'] is already defined`);
+    }
+
+    const {rootReducer} = getWindowStore() as any;
+    const injectedSlice = slice.injectInto(rootReducer as CombinedSliceReducer<RootState>, {
+        reducerPath,
+    });
+    getWindowStore().replaceReducer(rootReducer);
+    getAppBrowserHistory().replace(
+        makeRoutedURL(window.location.pathname, {}, (getWindowStore() as any).initialState),
+    );
+    return injectedSlice;
+}
+
+export function nameAndInitialState<K extends string, SliceState>(
+    reducerPath: K,
+    initialState: SliceState,
+) {
+    return {
+        name: reducerPath,
+        initialState: {
+            ...initialState,
+            ...(getWindowStore() as any).initialState[reducerPath],
+        } as SliceState,
+    };
 }
