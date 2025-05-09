d3efece7a2b57bd61f0ff7737a297385533980b8
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-09 14:16:44 +0300

    chore: migrate to rspack(dev env only)

--- packages/ui/build.app.config.ts
+++ packages/ui/build.app.config.ts
@@ -12,12 +12,14 @@ if (debugPort) {
     console.log({debugPort}, '\n');
 }
 
+const isDev = process.env.APP_ENV === 'development';
+
 const port = Number(process.env.LOCAL_DEV_PORT);
 
 const client: ServiceConfig['client'] = {
-    watchOptions: {
-        aggregateTimeout: 1000,
-    },
+    bundler: isDev ? 'rspack' : 'webpack',
+    javaScriptLoader: isDev ? 'swc' : 'babel',
+    cache: true,
     includes: ['src/shared'],
     images: ['src/ui/assets/img'],
     icons: ['src/ui/assets/img/svg'],
