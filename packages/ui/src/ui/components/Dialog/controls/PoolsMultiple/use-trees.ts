17b66ab0fe19b9fa21162c9a2c2a9c450e3a87a9
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-12 13:55:38 +0300

    feat(Dialog): add pools multiple select control

---
+++ packages/ui/src/ui/components/Dialog/controls/PoolsMultiple/use-trees.ts
@@ -0,0 +1,47 @@
+import {useMemo} from 'react';
+
+import map_ from 'lodash/map';
+
+import {useFetchBatchQuery} from '../../../../store/api/yt';
+
+import {YTApiId} from '../../../../rum/rum-wrap-api';
+
+import {PoolPair} from './PoolsMultiple';
+
+export function useTrees(value: PoolPair[]) {
+    const {data: trees, isLoading: isTreesLoading} = useFetchBatchQuery<string>({
+        id: YTApiId.listPoolsTrees,
+        parameters: {
+            requests: [
+                {
+                    command: 'list' as const,
+                    parameters: {
+                        path: '//sys/scheduler/orchid/scheduler/pool_trees',
+                    },
+                },
+            ],
+        },
+        errorTitle: 'Failed to fetch list of pools trees',
+    });
+
+    const treesLoadedWithData = trees && trees?.length && trees?.[0].output;
+
+    const treesOptions = useMemo(() => {
+        return treesLoadedWithData
+            ? map_(trees[0].output, (item) => ({
+                  value: item,
+                  content: item,
+              }))
+            : [];
+    }, [treesLoadedWithData]);
+
+    const uniqueTrees = useMemo(() => {
+        return value.map((pair) => pair.tree).filter((tree) => Boolean(tree));
+    }, [value]);
+
+    return {
+        trees: uniqueTrees,
+        isTreesLoading,
+        treesOptions,
+    };
+}
