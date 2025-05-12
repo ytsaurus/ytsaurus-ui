4eb5a99fde7997bb78c46de0993e1086f97815f3
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-12 14:01:33 +0300

    feat(Dashboard2): setup pools widget [YTFRONT-3400]

---
+++ packages/ui/src/ui/store/api/dashboard2/pools/pools.ts
@@ -0,0 +1,82 @@
+import map_ from 'lodash/map';
+
+import ypath from '../../../../common/thor/ypath';
+
+import {ytApiV3} from '../../../../rum/rum-wrap-api';
+
+export type PoolQueryParams = {
+    tree: string;
+    pool: string;
+};
+
+export type PoolsQueryArgs = {
+    queries: PoolQueryParams[];
+};
+
+export async function pools(args: PoolsQueryArgs) {
+    try {
+        const {queries} = args;
+        const response = await ytApiV3.executeBatch({
+            parameters: {
+                requests: map_(queries, ({tree, pool}) => ({
+                    command: 'get' as const,
+                    parameters: {
+                        path: `//sys/scheduler/orchid/scheduler/pool_trees/${tree}/pools/${pool}`,
+                    },
+                })),
+            },
+        });
+
+        const pools = map_(response, (item, index) => {
+            const {output} = item;
+
+            const operationsGarantee = ypath.getValue(output, '/max_operation_count');
+            const operationsUsage = ypath.getValue(output, '/running_operation_count');
+
+            const garantee = ypath.getValue(output, '/estimated_guarantee_resources');
+            const usage = ypath.getValue(output, '/resource_usage');
+
+            const cpu = {
+                value: (ypath.getValue(usage, '/cpu') / ypath.getValue(garantee, '/cpu')) * 50,
+                garantee: ypath.getValue(garantee, '/cpu'),
+                usage: ypath.getValue(usage, '/cpu'),
+            };
+
+            const memory = {
+                value:
+                    (ypath.getValue(usage, '/user_memory') /
+                        ypath.getValue(garantee, '/user_memory')) *
+                    50,
+                garantee: ypath.getValue(garantee, '/user_memory'),
+                usage: ypath.getValue(usage, '/user_memory'),
+            };
+
+            const gpu = {
+                value: (ypath.getValue(usage, '/gpu') / ypath.getValue(garantee, '/gpu')) * 50,
+                garantee: ypath.getValue(garantee, '/gpu'),
+                usage: ypath.getValue(usage, '/gpu'),
+            };
+
+            const operations = {
+                value: (operationsUsage / operationsGarantee) * 50,
+                garantee: operationsGarantee,
+                usage: operationsUsage,
+            };
+
+            return {
+                general: {
+                    pool: queries?.[index]?.pool || 'unkwown',
+                    tree: queries?.[index]?.tree || 'unknown',
+                },
+                cpu,
+                gpu,
+                memory,
+                operations,
+            };
+        });
+
+        return {data: pools};
+    } catch (error) {
+        return {error};
+    }
+}
