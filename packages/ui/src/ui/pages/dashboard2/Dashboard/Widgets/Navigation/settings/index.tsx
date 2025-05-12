f8843ad043f3d3eefb753306e14d5752c49cf8ce
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-12 13:21:49 +0300

    feat(Dashboard2): setup navigation widget [YTFRONT-3400]

---
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Navigation/settings/index.tsx
@@ -0,0 +1,16 @@
+export type NavigationSettingsValues = {
+    name: string;
+};
+
+export function useNavigationSettings() {
+    return [
+        {
+            type: 'text' as const,
+            name: 'name',
+            caption: 'Name',
+            extras: {
+                placeholder: 'Navigation',
+            },
+        },
+    ];
+}
