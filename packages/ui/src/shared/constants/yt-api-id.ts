c131a261684549c9b67d754b6dc722e06ecd8393
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-12 13:50:49 +0300

    feat(Dashboard2): setup accounts widget [YTFRONT-3400]

--- packages/ui/src/shared/constants/yt-api-id.ts
+++ packages/ui/src/shared/constants/yt-api-id.ts
@@ -3,6 +3,8 @@ export enum YTApiId {
     ui_clusterInfo,
     ui_loadColumnPreset,
 
+    favoritesAccountsAttributes,
+
     // Some ids without specific page
     clusterParams,
     clusterParamsIsDeveloper,
