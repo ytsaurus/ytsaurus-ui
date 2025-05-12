f024042b2419ff6e23dd3ef6ce1acb6e09168586
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-12 14:25:25 +0300

    feat(Dashboard2): setup services widget [YTFRONT-3400]

---
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Services/ServicesWidgetContent/ServicesWidgetContent.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Label, Text} from '@gravity-ui/uikit';
+import {PluginWidgetProps} from '@gravity-ui/dashkit';
+import {createColumnHelper} from '@gravity-ui/table/tanstack';
+
+import {ServiceInfo} from '../../../../../../store/api/dashboard2/services/services';
+
+import {WidgetTable} from '../../../../../../pages/dashboard2/Dashboard/components/WidgetTable/WidgetTable';
+import {GeneralCell} from '../../../../../../pages/dashboard2/Dashboard/components/GeneralCell/GeneralCell';
+
+import {Health} from '../../../../../../components/Health/Health';
+
+import {useServicesWidget} from './use-services-widget';
+
+const columnHelper = createColumnHelper<ServiceInfo>();
+
+const columns = [
+    columnHelper.accessor('general', {
+        cell: (general) => <GeneralCell {...general.getValue()} />,
+        header: 'Name',
+    }),
+    columnHelper.accessor('type', {
+        cell: (type) => <Label>{String(...type.getValue())}</Label>,
+        header: 'Type',
+    }),
+    columnHelper.accessor('status', {
+        cell: (status) => <Health value={status.getValue()} />,
+        header: 'Health',
+    }),
+    columnHelper.accessor('config', {
+        cell: (config) => <Text whiteSpace={'nowrap'}>{config.getValue()}</Text>,
+        header: 'Config',
+    }),
+];
+
+export function ServicesWidgetContent(props: PluginWidgetProps) {
+    const {data, error, isLoading} = useServicesWidget(props);
+
+    return (
+        <WidgetTable
+            columns={columns}
+            data={data || []}
+            itemHeight={40}
+            isLoading={isLoading}
+            fallback={{itemsName: 'services'}}
+            error={error}
+        />
+    );
+}
---
+++ packages/ui/src/ui/store/api/dashboard2/services/services.ts
@@ -0,0 +1,131 @@
+import {BaseQueryApi} from '@reduxjs/toolkit/query';
+
+import map_ from 'lodash/map';
+import filter_ from 'lodash/filter';
+import find_ from 'lodash/find';
+
+import hammer from '../../../../common/hammer';
+
+import {RootState} from '../../../../store/reducers';
+import {isDeveloper} from '../../../../store/selectors/global/is-developer';
+
+import {YTApiId, ytApiV3Id} from '../../../../rum/rum-wrap-api';
+import {getBatchError} from '../../../../utils/utils';
+import {StrawberryCliqueHealthType, chytApiAction} from '../../../../utils/strawberryControllerApi';
+import {YTHealth} from '../../../../types';
+import {Page} from '../../../../../shared/constants/settings';
+
+export type ServiceInfo = {
+    general: {name: string; url: string};
+    type: 'bundle' | 'CHYT';
+    status?: YTHealth | StrawberryCliqueHealthType;
+    config: string;
+};
+
+export type ServicesItem = {
+    service: string;
+    item: string;
+};
+
+const makeItemLink = (type: 'chyt' | 'bundle', name: string, cluster: string) => {
+    const page = type === 'chyt' ? Page.CHYT : Page.TABLET_CELL_BUNDLES;
+    const itemParam = type === 'chyt' ? name : `instances?activeBundle=${name}`;
+    return `/${cluster}/${page}/${itemParam}`;
+};
+
+const bundlesRequests = (items: ServicesItem[]) =>
+    map_(items, ({item}) => ({
+        command: 'get' as const,
+        parameters: {
+            path: `//sys/tablet_cell_bundles/${item}`,
+            attributes: ['health', 'bundle_controller_target_config'],
+        },
+    }));
+
+async function fetchBundles(items: ServicesItem[], cluster: string) {
+    const response = await ytApiV3Id.executeBatch(YTApiId.tabletCellBundles, {
+        parameters: {requests: bundlesRequests(items)},
+    });
+
+    const error = getBatchError(response, 'Tablet cell bundles cannot be loaded');
+    if (error) {
+        throw error;
+    }
+
+    if (!response.length) return [];
+
+    const bundlesInfo = map_(response, ({output}, idx) => {
+        if (!output) {
+            return undefined;
+        }
+
+        const item = output?.$attributes;
+
+        const memory = hammer.format['Bytes'](
+            item?.bundle_controller_target_config?.tablet_node_resource_guarantee?.memory || '',
+        );
+        const cpu = hammer.format['vCores'](
+            item?.bundle_controller_target_config?.tablet_node_resource_guarantee?.vcpu || '',
+        );
+        const instances = item?.bundle_controller_target_config?.tablet_node_count;
+
+        return {
+            type: 'bundle' as const,
+            general: {
+                name: items?.[idx]?.item || 'unknown',
+                url: makeItemLink('bundle', items?.[idx]?.item || 'unknown', cluster),
+            },
+            status: item?.health as YTHealth,
+            config: `${instances} x (${memory}, ${cpu})`,
+        };
+    }).filter(Boolean) as ServiceInfo[];
+
+    return bundlesInfo;
+}
+
+async function fetchChyt(items: ServicesItem[], cluster: string, isAdmin: boolean) {
+    const cliquesResponses = await Promise.all(
+        map_(items, ({item}) => chytApiAction('get_brief_info', cluster, {alias: item}, {isAdmin})),
+    );
+
+    const cliques = map_(cliquesResponses, ({result: item}, idx) => {
+        const cpu = `${item.ctl_attributes?.total_cpu} ${item.ctl_attributes?.total_cpu && item.ctl_attributes?.total_cpu > 1 ? 'cores' : 'core'}`;
+        const memory = hammer.format['Bytes'](item.ctl_attributes?.total_memory || '');
+        const instances = item.ctl_attributes?.instance_count || 0;
+        const alias = items?.[idx]?.item || 'unknown';
+        return {
+            type: 'CHYT' as const,
+            general: {
+                name: alias,
+                url: makeItemLink('chyt', alias, cluster),
+            },
+            status: item?.health,
+            config: `${instances} x (${memory}, ${cpu})`,
+        };
+    });
+
+    return cliques;
+}
+
+export async function services(args: {cluster: string; items?: ServicesItem[]}, api: BaseQueryApi) {
+    try {
+        const {cluster, items} = args;
+
+        const state = api.getState() as RootState;
+        const isAdmin = isDeveloper(state);
+
+        const requestedCliques = filter_(items, ({service}) => service === 'chyt');
+        const requestedBundles = filter_(items, ({service}) => service === 'bundle');
+
+        const cliques = await fetchChyt(requestedCliques, cluster, isAdmin);
+        const bundles = await fetchBundles(requestedBundles, cluster);
+
+        return {
+            data: map_(items, (item) =>
+                find_([...bundles, ...cliques], (i) => item.item === i.general.name),
+            ).filter(Boolean) as ServiceInfo[],
+        };
+    } catch (error) {
+        return {error};
+    }
+}
