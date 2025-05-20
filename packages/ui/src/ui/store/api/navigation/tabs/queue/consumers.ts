9d40327c2266e4a886c4cfedcc0876ecca782dcc
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-05-20 23:32:37 +0300

    chore: refactor queue store api

---
+++ packages/ui/src/ui/store/api/navigation/tabs/queue/consumers.ts
@@ -0,0 +1,116 @@
+import {BaseQueryApi} from '@reduxjs/toolkit/query';
+
+import {RootState} from '../../../../../store/reducers';
+import {getPath} from '../../../../../store/selectors/navigation';
+import {getCluster} from '../../../../../store/selectors/global';
+
+import {ytApiV3, ytApiV4} from '../../../../../rum/rum-wrap-api';
+import {wrapApiPromiseByToaster} from '../../../../../utils/utils';
+import {BatchResultsItem} from '../../../../../../shared/yt-types';
+
+type CreateConsumersMutationArgs = {
+    consumerPath: string;
+} & (
+    | {
+          register?: false | undefined;
+          vital?: boolean;
+      }
+    | {
+          register: true;
+          vital: boolean;
+      }
+);
+
+export async function createConsumer(args: CreateConsumersMutationArgs, api: BaseQueryApi) {
+    try {
+        const {vital, register, consumerPath} = args;
+
+        const state = api.getState() as RootState;
+        const queuePath = getPath(state);
+
+        await ytApiV3.create({
+            parameters: {
+                type: 'queue_consumer',
+                path: consumerPath,
+            },
+        });
+
+        if (register) {
+            const response = await wrapApiPromiseByToaster<
+                {results: BatchResultsItem<unknown>[]},
+                'v4'
+            >(
+                ytApiV4.executeBatch({
+                    parameters: {
+                        requests: [
+                            {
+                                command: 'register_queue_consumer' as const,
+                                parameters: {
+                                    vital,
+                                    queue_path: queuePath,
+                                    consumer_path: consumerPath,
+                                },
+                            },
+                        ],
+                    },
+                }),
+                {
+                    toasterName: 'register queue consumer',
+                    successContent:
+                        'Registration of the consumer has started, this may take some time',
+                    skipErrorToast: true,
+                    batchType: 'v4',
+                    errorTitle: '',
+                },
+            );
+            if (response.results[0]?.error) {
+                throw response.results[0]?.error;
+            }
+        }
+
+        return {data: []};
+    } catch (error) {
+        return {error};
+    }
+}
+
+export async function unregisterConsumer(args: {consumerPath: string}, api: BaseQueryApi) {
+    try {
+        const {consumerPath} = args;
+
+        const state = api.getState() as RootState;
+        const queue_path = getPath(state);
+
+        const response = await wrapApiPromiseByToaster(
+            ytApiV4.executeBatch({
+                parameters: {
+                    requests: [
+                        {
+                            command: 'unregister_queue_consumer' as const,
+                            parameters: {
+                                queue_path,
+                                consumer_path: consumerPath,
+                            },
+                        },
+                    ],
+                },
+            }),
+            {
+                toasterName: 'unregister queue consumer',
+                successContent:
+                    'Unregistration of the consumer has started, this may take some time',
+                skipErrorToast: true,
+                batchType: 'v4',
+                errorTitle: '',
+            },
+        );
+
+        if (response.results[0]?.error) {
+            throw response.results[0]?.error;
+        }
+
+        return {data: []};
+    } catch (error) {
+        return {error};
+    }
+}
