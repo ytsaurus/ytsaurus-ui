32617edc46bdc765d740f784727ac3a301c101dd
simbiozizv <simbiozizv@yandex-team.ru>
2025-05-23 20:45:28 +0300

    chore(Navigation): rename ordered table to queue table [YTFRONT-4822]

--- packages/ui/src/ui/components/Icon/auto-imported-icons.ts
+++ packages/ui/src/ui/components/Icon/auto-imported-icons.ts
@@ -98,7 +98,7 @@ import Star from '../../assets/img/svg/icons/star.svg';
 import StopCircle from '../../assets/img/svg/icons/stop-circle.svg';
 import SyncAlt from '../../assets/img/svg/icons/sync-alt.svg';
 import Table from '../../assets/img/svg/icons/table.svg';
-import OrderedTable from '../../assets/img/svg/icons/ordered-table.svg';
+import QueueTable from '../../assets/img/svg/icons/queue-table.svg';
 import Tag from '../../assets/img/svg/icons/tag.svg';
 import Tasks from '../../assets/img/svg/icons/tasks.svg';
 import Th from '../../assets/img/svg/icons/th.svg';
@@ -209,7 +209,7 @@ export const autoIcons = {
     ['stop-circle']: StopCircle,
     ['sync-alt']: SyncAlt,
     ['table']: Table,
-    ['ordered-table']: OrderedTable,
+    ['queue-table']: QueueTable,
     ['tag']: Tag,
     ['tasks']: Tasks,
     ['th']: Th,
