09050a89fc2009a1be8b2430faccf93a7b29f572
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-05-28 01:12:31 +0300

    chore(Flow/Graph): load graph data [YTFRONT-3978]

--- packages/ui/src/ui/store/reducers/flow/index.ts
+++ packages/ui/src/ui/store/reducers/flow/index.ts
@@ -4,5 +4,13 @@ import {filters} from './filters';
 import {dynamicSpec, staticSpec} from './specs';
 import {status} from './status';
 import {layout} from './layout';
+import {flowGraphSlice} from './graph';
 
-export const flow = combineReducers({filters, dynamicSpec, staticSpec, status, layout});
+export const flow = combineReducers({
+    filters,
+    dynamicSpec,
+    staticSpec,
+    status,
+    layout,
+    graph: flowGraphSlice.reducer,
+});
