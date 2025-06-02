ad392e69c7ccfe447ce6749a7011aad3ee6e91a0
simbiozizv <simbiozizv@yandex-team.ru>
2025-06-02 22:29:11 +0300

    fix(Chart): yql datetime in chart [YTFRONT-4937]

--- packages/ui/src/ui/pages/query-tracker/QueryResultsVisualization/preparers/getPointData.ts
+++ packages/ui/src/ui/pages/query-tracker/QueryResultsVisualization/preparers/getPointData.ts
@@ -1,4 +1,5 @@
 import {Result} from '../../module/query_result/types';
+import {get64Timestamp} from '../helpers/get64Timestamp';
 
 export const NumberTypes = [
     'yql.interval',
@@ -12,8 +13,16 @@ export const NumberTypes = [
     'yql.decimal',
 ];
 
+export const DateTimeTypes = ['yql.datetime', 'yql.date', 'yql.timestamp'];
+export const DateTime64Types = ['yql.datetime64', 'yql.date64', 'yql.timestamp64'];
+
 export const getPointValue = (value: Result) => {
     if (NumberTypes.includes(value.$type)) return Number(value.$value);
+    if (DateTimeTypes.includes(value.$type)) return Number(value.$value) * 1000;
+
+    if (DateTime64Types.includes(value.$type)) {
+        return get64Timestamp(value);
+    }
 
     return value.$rawValue;
 };
