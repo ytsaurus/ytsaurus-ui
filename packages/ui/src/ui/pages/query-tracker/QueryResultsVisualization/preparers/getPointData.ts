17d6c9caeace7648652b1ffcada1aed4bce252fe
simbiozizv <simbiozizv@yandex-team.ru>
2025-06-02 22:29:11 +0300

    fix(Chart): yql datetime in chart [YTFRONT-4937]

--- packages/ui/src/ui/pages/query-tracker/QueryResultsVisualization/preparers/getPointData.ts
+++ packages/ui/src/ui/pages/query-tracker/QueryResultsVisualization/preparers/getPointData.ts
@@ -1,4 +1,6 @@
 import {Result} from '../../module/query_result/types';
+// @ts-ignore
+import {timestamp} from '@gravity-ui/unipika/lib/plugins/yql-timestamp';
 
 export const NumberTypes = [
     'yql.interval',
@@ -12,8 +14,16 @@ export const NumberTypes = [
     'yql.decimal',
 ];
 
+export const DateTimeTypes = ['yql.datetime', 'yql.date', 'yql.timestamp'];
+export const DateTime64Types = ['yql.datetime64', 'yql.date64', 'yql.timestamp64'];
+
 export const getPointValue = (value: Result) => {
     if (NumberTypes.includes(value.$type)) return Number(value.$value);
+    if (DateTimeTypes.includes(value.$type)) return Number(value.$value) * 1000;
+
+    if (DateTime64Types.includes(value.$type)) {
+        return timestamp(value);
+    }
 
     return value.$rawValue;
 };
