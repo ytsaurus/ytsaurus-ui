60dcaae18b9a5306a76d8c9adb0ea6f6d3e8f60a
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-04 02:42:19 +0300

    chore(YTGraph): extract useElkLayout (+YTGraph.props.renderBlock) [YTFRONT-3978]

--- packages/ui/src/ui/pages/query-tracker/Plan/GraphEditor/QueriesNodeBlock.ts
+++ packages/ui/src/ui/pages/query-tracker/Plan/GraphEditor/QueriesNodeBlock.ts
@@ -1,20 +1,12 @@
 import {ECameraScaleLevel} from '@gravity-ui/graph';
 
-import {NodeTBlock} from '../../../../components/YTGraph/canvas/NodeBlock';
+import {BaseMeta, NodeTBlock} from '../../../../components/YTGraph/canvas/NodeBlock';
 
 import {type NodeDetails, NodeProgress} from '../models/plan';
 import {OperationSchemas} from '../utils';
 
-export type QueriesBlockMeta = {
+export type QueriesBlockMeta = BaseMeta & {
     level: ECameraScaleLevel;
-    icon: {
-        src: string;
-        height: number;
-        width: number;
-    };
-    bottomText?: string;
-    textSize: number;
-    padding?: number;
     nodeProgress?: NodeProgress;
     schemas?: OperationSchemas;
     details?: NodeDetails;
