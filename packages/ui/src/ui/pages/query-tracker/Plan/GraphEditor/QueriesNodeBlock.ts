ac4ae1675c7d813e091e1f5b2ab7e8135119568f
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-05 02:06:55 +0300

    chore(YTGraph): extract YTGraphCanvasBlock [YTFRONT-3978]

--- packages/ui/src/ui/pages/query-tracker/Plan/GraphEditor/QueriesNodeBlock.ts
+++ packages/ui/src/ui/pages/query-tracker/Plan/GraphEditor/QueriesNodeBlock.ts
@@ -1,15 +1,121 @@
+import {TBlock} from '@gravity-ui/graph';
 import {ECameraScaleLevel} from '@gravity-ui/graph';
 
-import {BaseMeta, NodeTBlock} from '../../../../components/YTGraph/canvas/NodeBlock';
+import {YTGrapCanvasBlock} from '../../../../components/YTGraph';
 
 import {type NodeDetails, NodeProgress} from '../models/plan';
 import {OperationSchemas} from '../utils';
 
-export type QueriesBlockMeta = BaseMeta & {
+export type QueriesBlockMeta = {
     level: ECameraScaleLevel;
     nodeProgress?: NodeProgress;
     schemas?: OperationSchemas;
     details?: NodeDetails;
+
+    icon: {
+        src: string;
+        height?: number;
+        width?: number;
+    };
+    bottomText?: string;
+    textSize?: number;
+    padding?: number;
 };
 
 export type QueriesNodeBlock = NodeTBlock<QueriesBlockMeta>;
+
+const DEFAULT_CONTENT_OFFSET = 10;
+const DEFAULT_ICON_OFFSET = 20;
+
+const ICON_SIZE = 24;
+const TEXT_SIZE = 14;
+
+export type NodeTBlock<T extends QueriesBlockMeta> = Omit<TBlock, 'meta'> & {meta: T};
+
+export class QueriesCanvasBlock extends YTGrapCanvasBlock<QueriesNodeBlock> {
+    icon: null | HTMLImageElement = null;
+
+    override renderBlock(mode: 'minimalistic' | 'schematic') {
+        this.drawBorder({
+            progressPercent: this.getProgressPercent(),
+            inProgress: this.checkNodeProgress(),
+        });
+        this.renderContent(mode);
+        this.drawJobCounter();
+    }
+
+    private getProgressPercent() {
+        const nodeProgress = this.state.meta.nodeProgress;
+        if (this.state.meta.nodeProgress?.state === 'Finished') return 1;
+        if (!nodeProgress || !nodeProgress.total) return 0;
+
+        return (nodeProgress.completed || 0) / nodeProgress.total;
+    }
+
+    private checkNodeProgress() {
+        const {nodeProgress} = this.state.meta;
+        return (
+            Boolean(nodeProgress?.state) && ['Started', 'InProgress'].includes(nodeProgress?.state!)
+        );
+    }
+
+    private renderContent(mode: 'minimalistic' | 'schematic') {
+        const {height, width, x, y} = this.state;
+        const padding = this.state.meta.padding || 0;
+        const textSize = this.state.meta.textSize ?? TEXT_SIZE;
+        const {iconWidth, iconHeight} = iconSizes(this.state.meta);
+        const isSchematic = mode === 'schematic';
+
+        let textHeight = 0;
+        if (isSchematic) {
+            const contentWidth = width - padding * 2;
+            const textWidth = this.context.ctx.measureText(this.state.name).width;
+            const numLines = Math.ceil(textWidth / contentWidth);
+            textHeight = numLines * textSize;
+        }
+
+        const contentHeight = textHeight + DEFAULT_CONTENT_OFFSET + iconHeight;
+        const startYPosition = (height - contentHeight) / 2;
+
+        if (isSchematic) {
+            this.drawInnerText({
+                text: this.state.name,
+                yPosition: startYPosition + DEFAULT_CONTENT_OFFSET + iconHeight,
+                hasIcon: Boolean(this.state.meta.icon?.src),
+            });
+            this.drawBottomText(this.state.meta.bottomText);
+
+            this.drawIcon(
+                this.state.meta.icon?.src,
+                x + (width - iconWidth) / 2,
+                y + startYPosition,
+                iconWidth,
+                iconHeight,
+            );
+        } else {
+            this.drawIcon(
+                this.state.meta.icon?.src,
+                x + DEFAULT_ICON_OFFSET,
+                y + DEFAULT_ICON_OFFSET,
+                width - DEFAULT_ICON_OFFSET * 2,
+                height - DEFAULT_ICON_OFFSET * 2,
+            );
+        }
+    }
+
+    private drawJobCounter() {
+        const nodeProgress = this.state.meta.nodeProgress;
+        if (!nodeProgress || !nodeProgress.total) return;
+
+        const total = nodeProgress.total + (nodeProgress.aborted || 0);
+
+        this.drawCounter(total);
+    }
+}
+
+function iconSizes(meta: QueriesBlockMeta) {
+    return {
+        iconWidth: meta.icon?.width ?? ICON_SIZE,
+        iconHeight: meta.icon?.height ?? ICON_SIZE,
+    };
+}
