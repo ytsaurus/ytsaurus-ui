dec87ee7c0c66688636609abdca1ec56d2f011ca
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-06 16:34:00 +0300

    chore(Navigation/Flow): add StreamCanvas renderer [YTFRONT-3978]

--- packages/ui/src/ui/components/YTGraph/utils/iconToBase.ts
+++ packages/ui/src/ui/components/YTGraph/utils/iconToBase.ts
@@ -1,6 +1,7 @@
 import {renderToString} from 'react-dom/server';
+import {GRAPH_COLORS} from '../constants';
 
-export const iconToBase = (icon: React.ReactElement, color: string) => {
+export const iconToBase = (icon: React.ReactElement, color: string = GRAPH_COLORS.icon) => {
     // @ts-ignore
     const iconString = renderToString(icon).replaceAll('currentColor', color);
     return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(iconString);
