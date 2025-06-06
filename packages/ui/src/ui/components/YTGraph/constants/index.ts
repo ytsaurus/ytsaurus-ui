14e4d617b59b947b65b830692d9691d6b4e86734
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-06 15:50:31 +0300

    chore(YTGraph): use delayed initialization for GRAPH_COLORS [YTFRONT-3978]

--- packages/ui/src/ui/components/YTGraph/constants/index.ts
+++ packages/ui/src/ui/components/YTGraph/constants/index.ts
@@ -1,23 +1,33 @@
+import {makeObjectWithDelayedInit} from '../../../utils/delayed-init';
 import {getCssColor} from '../../../utils/get-css-color';
 
-export const GRAPH_COLORS = {
-    text: getCssColor('--yql-graph-color-text-label'),
-    secondary: getCssColor('--g-color-text-secondary'),
-    border: getCssColor('--yql-graph-color-edge'),
-    selectedBorder: getCssColor('--yql-graph-color-edge-highlight'),
-    progressBorder: getCssColor('--info-color'),
-    background: getCssColor('--g-color-base-generic-ultralight'),
-    selectedBackground: getCssColor('--hover-background'),
-    progressColor: 'rgba(59, 201, 53, 0.15)',
-    aborted: getCssColor('--default-background'),
-    failed: getCssColor('--g-color-base-danger-heavy'),
-    jobBlockBackground: 'rgba(88, 106, 122, 1)',
-    jobBlockColor: getCssColor('--g-color-text-brand'),
+const GRAPH_COLORS_ = {
+    text: () => getCssColor('--yql-graph-color-text-label'),
+    secondary: () => getCssColor('--g-color-text-secondary'),
+    border: () => getCssColor('--yql-graph-color-edge'),
+    selectedBorder: () => getCssColor('--yql-graph-color-edge-highlight'),
+    progressBorder: () => getCssColor('--info-color'),
+    background: () => getCssColor('--g-color-base-generic-ultralight'),
+    selectedBackground: () => getCssColor('--hover-background'),
+    progressColor: () => 'rgba(59, 201, 53, 0.15)',
+    aborted: () => getCssColor('--default-background'),
+    failed: () => getCssColor('--g-color-base-danger-heavy'),
+    jobBlockBackground: () => 'rgba(88, 106, 122, 1)',
+    jobBlockColor: () => getCssColor('--g-color-text-brand'),
 };
 
-export const STATE_COLOR_MAP = {
-    completed: getCssColor('--success-color'),
-    pending: getCssColor('--info-color'),
-    aborted: getCssColor('--yellow-color'),
-    failed: getCssColor('--danger-color'),
+const STATE_COLOR_MAP_ = {
+    completed: () => getCssColor('--success-color'),
+    pending: () => getCssColor('--info-color'),
+    aborted: () => getCssColor('--yellow-color'),
+    failed: () => getCssColor('--danger-color'),
 };
+
+export const GRAPH_COLORS = makeObjectWithDelayedInit(GRAPH_COLORS_, {
+    skipUndefined: true,
+    defaultValue: '',
+});
+export const STATE_COLOR_MAP = makeObjectWithDelayedInit(STATE_COLOR_MAP_, {
+    skipUndefined: true,
+    defaultValue: '',
+});
