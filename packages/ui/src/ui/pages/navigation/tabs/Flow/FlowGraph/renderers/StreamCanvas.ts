0a2789eaeefec63aafb35e52a6ecdbdb0bfa4b3d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-09 19:18:27 +0300

    chore(Navigation/Flow): add sink_streams/timer_streams [YTFRONT-3978]

--- packages/ui/src/ui/pages/navigation/tabs/Flow/FlowGraph/renderers/StreamCanvas.ts
+++ packages/ui/src/ui/pages/navigation/tabs/Flow/FlowGraph/renderers/StreamCanvas.ts
@@ -1,7 +1,3 @@
-import React from 'react';
-
-import FileCodeIcon from '@gravity-ui/icons/svgs/file-code.svg';
-
 import format from '../../../../../../common/hammer/format';
 
 import {YTGrapCanvasBlock} from '../../../../../../components/YTGraph';
@@ -17,23 +13,31 @@ export class StreamCanvasBlock extends YTGrapCanvasBlock<FlowGraphBlockItem<'str
 
     renderHeader() {
         const w = 24;
-        this.drawInnerIcon({src: <FileCodeIcon />, xPos: PADDING, yPos: PADDING, w, h: w});
 
-        const yPos = PADDING;
+        this.drawInnerIcon({
+            src: this.state.icon,
+            xPos: PADDING,
+            yPos: PADDING,
+            w,
+            h: w,
+        });
+
+        const yPos = PADDING + 4;
         const xPos = PADDING + w + 2;
 
         this.drawInnerText({
             yPos,
             xPos,
             text: this.state.name,
-            fontSize: 'header2',
+            fontSize: 'header',
             padding: PADDING,
+            oneLine: true,
         });
 
         this.drawInnerText({
-            yPos: yPos + 30,
+            yPos: yPos + 25,
             xPos,
-            text: format.Bytes(this.state.meta.bytes_per_second) + '/s',
+            text: format.Bytes(this.state.meta.bytes_per_second, {digits: 0}) + '/s',
             align: 'right',
             fontSize: 'header',
         });
