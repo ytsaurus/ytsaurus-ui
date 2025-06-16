5139e58a229adce167362f17402666fac6f35c5f
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-16 17:54:49 +0300

    feat(Navigation/Flow): add Graph-view [YTFRONT-3978]
    add groupping by groupId

--- packages/ui/src/ui/components/YTGraph/config.tsx
+++ packages/ui/src/ui/components/YTGraph/config.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ELK, {ElkExtendedEdge} from 'elkjs';
+import ELK, {ElkExtendedEdge, ElkNode} from 'elkjs';
 import {
     CanvasBlock,
     ECameraScaleLevel,
@@ -16,7 +16,7 @@ import {Component} from '@gravity-ui/graph/build/lib';
 
 import {getCssColor} from '../../utils/get-css-color';
 import {useMemoizedIfEqual} from '../../hooks/use-updater';
-import {YTGraphData} from './YTGraph';
+import {YTGraphBlock, YTGraphData} from './YTGraph';
 
 export const getGraphColors = (): RecursivePartial<TGraphColors> => {
     return {
@@ -81,71 +81,69 @@ export function useConfig<T extends TBlock>(
     return {...config, scale, setScale};
 }
 
-const getElkConfig = (
-    children: {id: string; level?: number; width?: number; height?: number}[],
-    edges: ElkExtendedEdge[],
-    defaultSize: {height: number; width: number} = {
-        width: DEFAULT_BLOCK_SIZE,
-        height: DEFAULT_BLOCK_SIZE,
-    },
-) => {
+const LAYOUT_OPTIONS = {
+    'elk.algorithm': 'layered',
+    'elk.direction': 'RIGHT',
+    'spacing.nodeNode': '100', // Node horizontal spacing
+    'spacing.nodeNodeBetweenLayers': '50', // Layer spacing
+    'spacing.edgeNode': '25', // Nodes and edges spacing
+    'spacing.edgeEdge': '25', // Edges spacing
+    'spacing.edgeEdgeBetweenLayers': '35', // Distance between edges in layers
+    'spacing.edgeNodeBetweenLayers': '25', // Distance between nodes and edges in layers
+    'nodePlacement.strategy': 'TIGHT_TREE', // Node placement strategy
+    'elk.layered.nodePlacement.bk.fixedAlignment': 'CENTER', // Node alignment
+    'elk.layered.crossingMinimization.strategy': 'LAYER_PER_LAYER_SWEEP',
+    'elk.layered.thoroughness': '200', // Level of detail
+    'elk.portConstraints': 'FREE',
+};
+
+function toElkNodes(children: Array<ElkNode & {level?: number}> = []) {
+    return children.map((item): ElkNode => {
+        return {
+            id: item.id,
+            ...(item.width !== undefined ? {width: item.width} : {}),
+            ...(item.height !== undefined ? {height: item.height} : {}),
+            layoutOptions: {
+                ...LAYOUT_OPTIONS,
+                'elk.direction': 'DOWN',
+                ...(item.level === 1 ? {'elk.layered.layering.layerConstraint': '1'} : {}),
+            },
+            ...(item.children?.length! > 0 ? {children: toElkNodes(item.children)} : {}),
+        };
+    });
+}
+
+const getElkConfig = (children: Array<ElkNode & {level?: number}>, edges: ElkExtendedEdge[]) => {
     return {
         id: 'root',
-        children: children.map((node) => ({
-            id: node.id,
-            width: node.width ?? defaultSize.width,
-            height: node.height ?? defaultSize.height,
-            ...(node.level === 1
-                ? {
-                      layoutOptions: {
-                          'elk.layered.layering.layerConstraint': '1',
-                      },
-                  }
-                : {}),
-        })),
+        children: toElkNodes(children),
         edges,
-        layoutOptions: {
-            'elk.algorithm': 'layered',
-            'elk.direction': 'RIGHT',
-            'spacing.nodeNode': String(DEFAULT_BLOCK_SIZE), // Node horizontal spacing
-            'spacing.nodeNodeBetweenLayers': Math.round(defaultSize.height / 2).toString(), // Layer spacing
-            'spacing.edgeNode': '25', // Nodes and edges spacing
-            'spacing.edgeEdge': '25', // Edges spacing
-            'spacing.edgeEdgeBetweenLayers': '35', // Distance between edges in layers
-            'spacing.edgeNodeBetweenLayers': '25', // Distance between nodes and edges in layers
-            'nodePlacement.strategy': 'TIGHT_TREE', // Node placement strategy
-            'elk.layered.nodePlacement.bk.fixedAlignment': 'CENTER', // Node alignment
-            'elk.layered.crossingMinimization.strategy': 'LAYER_PER_LAYER_SWEEP',
-            'elk.layered.thoroughness': '200', // Level of detail
-            'elk.portConstraints': 'FREE',
-        },
+        layoutOptions: LAYOUT_OPTIONS,
     };
 };
 
-export function useElkLayout<B extends TBlock, C extends TConnection>({
+export function useElkLayout<B extends YTGraphBlock<string, {}>, C extends TConnection>({
     blocks,
     connections,
-}: YTGraphData<B, C>): {data: YTGraphData<B, C>; isLoading: boolean} {
-    const elkConfig = React.useMemo(() => {
-        const children = blocks.map(({id, width, height}) => ({
-            id: String(id),
-            width,
-            height,
-        }));
+}: YTGraphData<B, C>): {
+    data: YTGraphData<B, C>;
+    isLoading: boolean;
+} {
+    const elkParams = React.useMemo(() => {
         const edges = connections.map((item) => ({
             id: connectionId(item),
             sources: [item.sourceBlockId as string],
             targets: [item.targetBlockId as string],
         }));
 
-        return getElkConfig(children, edges);
+        return {elkConfig: getElkConfig(blocks, edges)};
     }, [blocks, connections]);
 
     const elk = React.useMemo(() => {
         return new ELK();
     }, []);
 
-    const [positions] = useMemoizedIfEqual(useElk(elkConfig, elk));
+    const [positions] = useMemoizedIfEqual(useElk(elkParams.elkConfig, elk));
 
     const res = React.useMemo(() => {
         const {result, isLoading} = positions;
@@ -158,14 +156,14 @@ export function useElkLayout<B extends TBlock, C extends TConnection>({
             isLoading,
             data: {
                 blocks: blocks.map((item) => {
-                    return {...item, ...result?.blocks?.[item.id]};
+                    return {...item, ...positions.result.blocks[item.id]} as B;
                 }),
                 connections: connections.map((item) => {
                     return {...item, ...result?.edges?.[connectionId(item)]};
                 }),
             },
         };
-    }, [blocks, connections, positions]);
+    }, [blocks, positions]);
     return res;
 }
 
