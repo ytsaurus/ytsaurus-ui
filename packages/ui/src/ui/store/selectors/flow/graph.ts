6512f9d766a9ee61f8b1aa72ea79a2419658050f
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-20 17:05:13 +0300

    chore(Navigation/Flow/Graph): display request error [YTFRONT-3978]

--- packages/ui/src/ui/store/selectors/flow/graph.ts
+++ packages/ui/src/ui/store/selectors/flow/graph.ts
@@ -1,3 +1,4 @@
 import {RootState} from '../../../store/reducers';
 
 export const getFlowGraphData = (state: RootState) => state.flow.graph.data;
+export const getFlowGraphError = (state: RootState) => state.flow.graph.error;
