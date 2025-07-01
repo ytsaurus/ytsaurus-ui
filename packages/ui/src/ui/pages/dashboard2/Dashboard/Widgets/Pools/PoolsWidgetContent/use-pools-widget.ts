654b22fec8b337b31bc088a464b5b8ee2039f162
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-07-01 22:43:03 +0300

    feat(Dashboard2/PoolsWidget): add favourite/custom list setting [YTFRONT-3400]

--- packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Pools/PoolsWidgetContent/use-pools-widget.ts
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Pools/PoolsWidgetContent/use-pools-widget.ts
@@ -1,18 +1,28 @@
 import {PluginWidgetProps} from '@gravity-ui/dashkit';
+import {useSelector} from 'react-redux';
 
 import map_ from 'lodash/map';
 import zipObject_ from 'lodash/zipObject';
 import includes_ from 'lodash/includes';
 
+import {RootState} from '../../../../../../store/reducers';
 import {PoolQueryParams} from '../../../../../../store/api/dashboard2/pools/pools';
 import {usePoolsQuery} from '../../../../../../store/api/dashboard2/pools';
+import {getFavouritePools} from '../../../../../../store/selectors/favourites';
+import {getPoolsTypeFilter} from '../../../../../../store/selectors/dashboard2/pools';
 
 const resources = ['cpu', 'memory', 'operations', 'gpu'];
 
 export function usePoolsWidget(props: PluginWidgetProps) {
     const userPools = props.data?.pools as PoolQueryParams[];
     const resourcesColumns = props.data?.columns as string[];
-    const {data, isLoading, isFetching, error} = usePoolsQuery({queries: userPools});
+    const favouritePools = useSelector(getFavouritePools);
+    const type = useSelector((state: RootState) => getPoolsTypeFilter(state, props.id));
+    const {data, isLoading, isFetching, error} = usePoolsQuery({
+        type,
+        favouriteList: favouritePools,
+        customList: userPools,
+    });
 
     const visibleColumns = zipObject_(
         resources,
