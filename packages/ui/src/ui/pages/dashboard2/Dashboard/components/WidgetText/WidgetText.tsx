a45db78e4cd1c790ebaaf83280e717c7330ad018
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-07-02 17:01:33 +0300

    chore(Dashboard2/Services): fix ellipsis text

--- packages/ui/src/ui/pages/dashboard2/Dashboard/components/WidgetText/WidgetText.tsx
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/components/WidgetText/WidgetText.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Text, TextProps} from '@gravity-ui/uikit';
+import {Flex, Text, TextProps} from '@gravity-ui/uikit';
 
 type Props = Omit<TextProps, 'ellipsis' | 'whiteSpace'> & {children: React.ReactNode};
 
 export function WidgetText(props: Props) {
     return (
-        <Text {...props} ellipsis whiteSpace={'nowrap'}>
-            {props.children}
-        </Text>
+        <Flex width={'100%'} overflow={'hidden'}>
+            <Text {...props} ellipsis whiteSpace={'nowrap'}>
+                {props.children}
+            </Text>
+        </Flex>
     );
 }
