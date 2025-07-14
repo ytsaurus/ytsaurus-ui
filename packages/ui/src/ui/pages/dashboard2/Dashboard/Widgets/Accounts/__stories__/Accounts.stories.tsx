ed493c4ddc2d266ecb1b01584c4c16d1b0e0cdec
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-07-14 13:25:08 +0300

    chore: init storybook [YTFRONT-3400]

---
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Accounts/__stories__/Accounts.stories.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+
+// @ts-expect-error
+import yt from '@ytsaurus/javascript-wrapper/lib/yt';
+
+import {defaultDashboardItems} from '../../../../../../constants/dashboard2';
+
+import {WidgetBase} from '../../../components/WidgetBase/WidgetBase';
+import {baseWidgetProps} from '../../../utils/mocks';
+
+import {AccountsWidgetControls} from '../AccountsWidgetControls/AccountsWidgetControls';
+import {AccountsWidgetContent} from '../AccountsWidgetContent/AccountsWidgetContent';
+import {AccountsWidgetHeader} from '../AccountsWidgetHeader/AccountsWidgetHeader';
+import {
+    accountsHandler,
+    accountsHandlerEmpty,
+    accountsHandlerError,
+    accountsHandlerWithLoading,
+} from '../__tests__/mocks';
+
+yt.setup.setGlobalOption('proxy', 'test-cluster.yt.my-domain.com');
+
+const meta = {
+    title: 'Dashboard/Accounts Widget',
+    decorators: [
+        (Story: any) => (
+            <div style={{padding: '20px', height: '1000px'}}>
+                <Story />
+            </div>
+        ),
+    ],
+};
+
+const extenedAccountsProps = {
+    ...baseWidgetProps,
+    data: {
+        ...defaultDashboardItems.accounts.data,
+        columns: [
+            {name: 'Nodes'},
+            {name: 'Chunks'},
+            {name: 'super_mega_wide_medium_that_cannot_fit_in'},
+        ],
+        name: baseWidgetProps.data.name,
+    },
+};
+
+const BaseComponent = () => (
+    <div style={{height: 255, width: 750}}>
+        <WidgetBase
+            {...extenedAccountsProps}
+            controls={<AccountsWidgetControls {...extenedAccountsProps} />}
+            content={<AccountsWidgetContent {...extenedAccountsProps} />}
+            header={<AccountsWidgetHeader {...extenedAccountsProps} />}
+        />
+    </div>
+);
+
+export default meta;
+
+export const Default = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [accountsHandler],
+        },
+    },
+};
+
+export const Loading = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [accountsHandlerWithLoading],
+        },
+    },
+};
+
+export const Empty = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [accountsHandlerEmpty],
+        },
+    },
+};
+
+export const Error = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [accountsHandlerError],
+        },
+    },
+};
---
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Pools/__stories__/Pools.stories.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+
+// @ts-expect-error
+import yt from '@ytsaurus/javascript-wrapper/lib/yt';
+
+import {defaultDashboardItems} from '../../../../../../constants/dashboard2';
+
+import {WidgetBase} from '../../../components/WidgetBase/WidgetBase';
+import {baseWidgetProps} from '../../../utils/mocks';
+
+import {PoolsWidgetControls} from '../PoolsWidgetControls/PoolsWidgetControls';
+import {PoolsWidgetContent} from '../PoolsWidgetContent/PoolsWidgetContent';
+import {PoolsWidgetHeader} from '../PoolsWidgetHeader/PoolsWidgetHeader';
+import {
+    poolsHandler,
+    poolsHandlerEmpty,
+    poolsHandlerError,
+    poolsHandlerWithLoading,
+} from '../__tests__/mocks';
+
+yt.setup.setGlobalOption('proxy', 'test-cluster.yt.my-domain.com');
+
+const meta = {
+    title: 'Dashboard/Pools Widget',
+    decorators: [
+        (Story: any) => (
+            <div style={{padding: '20px', height: '1000px'}}>
+                <Story />
+            </div>
+        ),
+    ],
+};
+
+const extendPoolsProps = {
+    ...baseWidgetProps,
+    data: {
+        ...defaultDashboardItems.pools.data,
+        name: baseWidgetProps.data.name,
+    },
+};
+
+const BaseComponent = () => (
+    <div style={{height: 260, width: 560}}>
+        <WidgetBase
+            controls={<PoolsWidgetControls {...extendPoolsProps} />}
+            content={<PoolsWidgetContent {...extendPoolsProps} />}
+            header={<PoolsWidgetHeader {...extendPoolsProps} />}
+            {...extendPoolsProps}
+        />
+    </div>
+);
+
+export default meta;
+
+export const Default = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [poolsHandler],
+        },
+    },
+};
+
+export const Loading = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [poolsHandlerWithLoading],
+        },
+    },
+};
+
+export const Empty = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [poolsHandlerEmpty],
+        },
+    },
+};
+
+export const Error = {
+    render: BaseComponent,
+    parameters: {
+        msw: {
+            handlers: [poolsHandlerError],
+        },
+    },
+};
