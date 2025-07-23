c77bdebbd67f854f13a96afcbb40b63a913dea67
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-07-23 14:05:36 +0300

    chore: use YTApiIdType instead of YTApiId

--- packages/ui/src/ui/store/api/yt/index.ts
+++ packages/ui/src/ui/store/api/yt/index.ts
@@ -19,15 +19,15 @@ export const ytApi = rootApi.injectEndpoints({
     endpoints: (build) => ({
         fetchBatch: build.query<BatchApiResults, BatchApiArgs>({
             queryFn: executeBatchV3,
-            providesTags: (_result, _error, arg) => [String(arg.id)],
+            providesTags: (_result, _error, arg) => [arg.id],
         }),
         updateBatch: build.mutation<BatchApiResults, BatchApiArgs>({
             queryFn: executeBatchV3,
-            invalidatesTags: (_result, _error, arg) => [String(arg.id)],
+            invalidatesTags: (_result, _error, arg) => [arg.id],
         }),
         listQueries: build.query({
             queryFn: listQueries,
-            providesTags: (_result, _error, _arg) => [String(YTApiId.listQueries)],
+            providesTags: (_result, _error, _arg) => [YTApiId.listQueries],
         }),
         flowExecute: build.query({
             queryFn: flowExecute,
