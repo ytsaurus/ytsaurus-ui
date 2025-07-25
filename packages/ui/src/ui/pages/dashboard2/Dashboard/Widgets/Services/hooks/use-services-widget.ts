983cd3a586863b86be369c0ccf76211fff558cf8
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-07-25 10:38:11 +0300

    chore: refactor services widget [YTFRONT-3400]

--- packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Services/hooks/use-services-widget.ts
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Services/hooks/use-services-widget.ts
@@ -6,29 +6,21 @@ import type {ServicesItem} from '../../../../../../store/api/dashboard2/services
 
 import {RootState} from '../../../../../../store/reducers';
 import {getCluster} from '../../../../../../store/selectors/global';
-import {getFavouriteBundles, getFavouriteChyt} from '../../../../../../store/selectors/favourites';
 import {getServicesTypeFilter} from '../../../../../../store/selectors/dashboard2/services';
 
 export function useServicesWidget(props: PluginWidgetProps) {
     const customItems = props.data?.services as ServicesItem[];
 
     const cluster = useSelector(getCluster);
-    const favouriteCliques = useSelector(getFavouriteChyt);
-    const favouriteBundles = useSelector(getFavouriteBundles);
 
     const type = useSelector((state: RootState) => getServicesTypeFilter(state, props.id));
 
-    let items = [];
-    if (type === 'favourite') {
-        items = [
-            ...favouriteCliques.map((clique) => ({service: 'chyt', item: clique.path})),
-            ...favouriteBundles.map((bundle) => ({service: 'bundle', item: bundle.path})),
-        ];
-    } else {
-        items = customItems;
-    }
-
-    const {data, isLoading, isFetching, error} = useServicesQuery({id: props.id, cluster, items});
+    const {data, isLoading, isFetching, error} = useServicesQuery({
+        type,
+        id: props.id,
+        cluster,
+        customItems,
+    });
 
     return {data, isLoading: isLoading || isFetching, error};
 }
