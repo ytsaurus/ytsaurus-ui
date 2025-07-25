3d9856200f8dc8cb944fe62b7d92f0d12f59bbd4
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-07-25 20:48:07 +0300

    chore: use lazy loading for @gravity-ui/chartkit [YTFRONT-4662]

--- packages/ui/src/ui/store/api/index.ts
+++ packages/ui/src/ui/store/api/index.ts
@@ -1,7 +1,7 @@
 import {BaseQueryFn, createApi} from '@reduxjs/toolkit/query/react';
 import {YTApiIdType} from '../../../shared/constants/yt-api-id';
 
-const tagTypes = [] as Array<YTApiIdType>;
+const tagTypes = [] as Array<YTApiIdType | `${YTApiIdType}_${string}`>;
 
 const baseQuery: BaseQueryFn = () => {
     return {data: undefined};
