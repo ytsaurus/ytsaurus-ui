1ccd8b8d57233c9e2792a56c5f1b7438770f0098
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-07-30 18:21:27 +0300

    chore: refactor api endpoints & add listOperationEvents

--- packages/ui/src/ui/store/api/yt/types.ts
+++ packages/ui/src/ui/store/api/yt/types.ts
@@ -8,6 +8,23 @@ import {
     SubscriptionOptions,
 } from '@reduxjs/toolkit/query';
 
+import {YTError} from '../../../types';
+import {ApiMethodParams} from '../../../rum/rum-wrap-api';
+
+// YT API Utility Types
+
+export type YTEndpointApiArgs<CommandParameters> = ClusterOrSetup<CommandParameters> &
+    Omit<ApiMethodParams<CommandParameters>, 'setup'>;
+
+export type ClusterOrSetup<CommandParameters> =
+    // cluster or setup param should be required for the case of selection
+    {cluster: string} | Required<Pick<ApiMethodParams<CommandParameters>, 'setup'>>;
+
+export type OverrideDataType<T extends {data?: unknown}, Data> = Omit<T, 'data' | 'error'> & {
+    data?: Data;
+    error?: YTError;
+};
+
 /**
  * MOTIVATION:
  * We need to redefine some types from RTK Query internally since they're not exported,
