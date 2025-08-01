5edf3de918f68a7a7cacf6f5b9513c38d88ca4b7
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-08-02 00:08:45 +0300

    chore: provide better types for operations widget [YTFRONT-3400]

--- packages/ui/src/ui/pages/dashboard2/Dashboard/components/WidgetSettings/SettingsValues.ts
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/components/WidgetSettings/SettingsValues.ts
@@ -1,16 +1,15 @@
 import {ConfigItemData} from '@gravity-ui/dashkit';
 import type {AccountsWidgetData} from '../../Widgets/Accounts/types';
-import {OperationsSettingsValues} from '../../Widgets/Operations/settings';
 import {PoolsSettingsValues} from '../../Widgets/Pools/settings';
 import {QueriesSettingsValues} from '../../Widgets/Queries/settings';
 import {ServicesSettingsValues} from '../../Widgets/Services/settings';
 import type {NavigationWidgetData} from '../../Widgets/Navigation/types';
-import {AccountsWidgetData} from '../../Widgets/Accounts/types';
+import type {OperationsWidgetData} from '../../Widgets/Operations/types';
 
 export type SettingsValues =
     | AccountsWidgetData
     | NavigationWidgetData
-    | OperationsSettingsValues
+    | OperationsWidgetData
     | PoolsSettingsValues
     | QueriesSettingsValues
     | ServicesSettingsValues
