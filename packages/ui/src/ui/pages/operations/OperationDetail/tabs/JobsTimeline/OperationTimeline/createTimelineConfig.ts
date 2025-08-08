a748f439604d32bae2200748583d032efb7868c7
simbiozizv <simbiozizv@yandex-team.ru>
2025-08-08 18:36:24 +0300

    feat(Operation/Timeline): show incarnations [YTFRONT-4980]

---
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/JobsTimeline/OperationTimeline/createTimelineConfig.ts
@@ -0,0 +1,44 @@
+import {JobLineRenderer} from '../../../../../../components/TimelineBlock/renderer/JobLineRenderer';
+import {ROW_HEIGHT} from '../constants';
+import {
+    MarkerDeselectionMode,
+    TimeLineConfig,
+    TimelineEvent,
+    TimelineMarker,
+} from '@gravity-ui/timeline';
+
+export const createTimelineConfig = <
+    TEvent extends TimelineEvent,
+    TMarker extends TimelineMarker,
+>(): TimeLineConfig<TEvent, TMarker> => ({
+    settings: {
+        start: 0,
+        end: 0,
+        axes: [],
+        events: [],
+        markers: [],
+        selectedEventIds: [],
+        markerDeselectionMode: MarkerDeselectionMode.ON_MARKER_CLICK_ONLY,
+        clickEventsCollectionFilter: (candidates) => {
+            return candidates
+                .filter((event) => event.renderer instanceof JobLineRenderer)
+                .splice(0, 1);
+        },
+        clickMarkerCollectionFilter: (candidates) => {
+            const group = candidates.find((candidate) => 'group' in candidate);
+            if (group) return [group];
+
+            return candidates.splice(0, 1);
+        },
+    },
+    viewConfiguration: {
+        axes: {
+            trackHeight: ROW_HEIGHT,
+            lineHeight: 12,
+        },
+        markers: {
+            collapseMinDistance: 10,
+        },
+        hideRuler: true,
+    },
+});
