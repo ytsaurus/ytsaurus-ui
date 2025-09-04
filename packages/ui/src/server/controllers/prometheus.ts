91154f3cd48e47d614acbfb8b8fee4a9f877da21
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-09-04 18:37:14 +0300

    chore(PrometheusDashboard): use single request per chart [YTFRONT-4388]

--- packages/ui/src/server/controllers/prometheus.ts
+++ packages/ui/src/server/controllers/prometheus.ts
@@ -2,15 +2,10 @@ import axios from 'axios';
 import type {Request, Response} from 'express';
 
 import {parseXYFromPanelId, replaceExprParams} from '../../shared/prometheus/utils';
-import {QueryRangePostData} from '../../shared/prometheus/types';
+import {QueryRangeData, QueryRangePostData} from '../../shared/prometheus/types';
 
-import {
-    ErrorWithCode,
-    UNEXPECTED_PIPE_AXIOS_RESPONSE,
-    pipeAxiosResponse,
-    sendAndLogError,
-} from '../utils';
 import {getPreloadedPrometheusDashboard} from '../components/prometheus-dashboards';
+import {ErrorWithCode, sendAndLogError} from '../utils';
 
 export async function prometheusQueryRange(req: Request, res: Response) {
     const BASE_URL = req.ctx.config.prometheusBaseUrl;
@@ -24,8 +19,7 @@ export async function prometheusQueryRange(req: Request, res: Response) {
             throw new ErrorWithCode(400, 'Failed to parse x,y from id query parameter', {id});
         }
 
-        const {dashboardType, start, end, step, targetIndex, params} =
-            req.body as QueryRangePostData;
+        const {dashboardType, start, end, step, params} = req.body as QueryRangePostData;
 
         const {panels} = await getPreloadedPrometheusDashboard({
             cluster,
@@ -35,31 +29,31 @@ export async function prometheusQueryRange(req: Request, res: Response) {
         const panel = panels.find((item) => {
             return item.gridPos.x === x && item.gridPos.y === y;
         });
-        if (!panel || panel.type !== 'timeseries' || !panel.targets[targetIndex]) {
+        if (!panel || panel.type !== 'timeseries') {
             throw new ErrorWithCode(400, `Failed to find a corresponding panel`, {
                 cluster,
                 dashboardType,
                 x,
                 y,
-                targetIndex,
             });
         }
 
         const {targets} = panel;
 
-        const query = await replaceExprParams(targets[targetIndex].expr, params, step);
+        const results = await Promise.all(
+            targets.map(({expr}) => {
+                const query = replaceExprParams(expr, params, step);
+                return axios
+                    .get<QueryRangeData>(`${BASE_URL}/api/v1/query_range?`, {
+                        params: {query, start, end, step},
+                    })
+                    .then(async (response) => {
+                        return response.data;
+                    });
+            }),
+        );
 
-        await axios
-            .get(`${BASE_URL}/api/v1/query_range?`, {
-                params: {query, start, end, step},
-                responseType: 'stream',
-            })
-            .then(async (response) => {
-                const pipedSize = await pipeAxiosResponse(req.ctx, res, response);
-                if (!pipedSize) {
-                    throw new Error(UNEXPECTED_PIPE_AXIOS_RESPONSE);
-                }
-            });
+        res.send({results});
     } catch (e: any) {
         sendAndLogError(req.ctx, res, null, e);
     }
