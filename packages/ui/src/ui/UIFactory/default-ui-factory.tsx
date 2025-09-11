a71788231475628606a07e9353e43147905d3f5a
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-09-11 11:40:28 +0300

    chore: refactor uifactory for incarnations [YTFRONT-5119]

--- packages/ui/src/ui/UIFactory/default-ui-factory.tsx
+++ packages/ui/src/ui/UIFactory/default-ui-factory.tsx
@@ -13,6 +13,7 @@ import {YT} from '../config/yt-config';
 import {DefaultSubjectLinkLazy} from '../components/SubjectLink/lazy';
 import type {SubjectCardProps} from '../components/SubjectLink/SubjectLink';
 import {QUERY_RESULT_CHART_TAB} from '../pages/query-tracker/QueryResultsVisualization';
+import {Incarnations} from '../pages/operations/OperationDetail/tabs/incarnations/Incarnations';
 
 import {UIFactory} from './index';
 
@@ -324,5 +325,8 @@ export const defaultUIFactory: UIFactory = {
     },
     getAnalyticsService() {
         return [];
+    },
+    renderIncarnationsTab() {
+        return <Incarnations />;
     },
 };
