dc9b5fb6160b5155cd9c7e8418c2892c29714e14
Kanstantsin Autushka <avtushko2004@gmail.com>
2025-09-22 12:39:10 +0300

    chore(Dashboard2): make medium miss on current cluster more clear [YTFRONT-5154]

--- packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Accounts/AccountsWidgetHeader/AccountsWidgetHeader.tsx
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Accounts/AccountsWidgetHeader/AccountsWidgetHeader.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import {useSelector} from 'react-redux';
 
 import {WidgetHeader} from '../../../../../../pages/dashboard2/Dashboard/components/WidgetHeader/WidgetHeader';
+import {getMediumList} from '../../../../../../store/selectors/thor';
+import {YTError} from '../../../../../../types';
 
 import {useAccountsWidget} from '../hooks/use-accounts-widget';
 import type {AccountsWidgetProps} from '../types';
@@ -9,7 +12,35 @@ import i18n from '../i18n';
 
 export function AccountsWidgetHeader(props: AccountsWidgetProps) {
     const name = props?.data?.name;
-    const {accounts, isLoading} = useAccountsWidget(props);
+    const {accounts, isLoading, userColumns} = useAccountsWidget(props);
+    const mediumList = useSelector(getMediumList);
+
+    const error = useMemo((): YTError | undefined => {
+        if (!Array.isArray(userColumns)) {
+            return undefined;
+        }
+
+        const invalidMediums: string[] = [];
+        userColumns.forEach((col) => {
+            // Extract the column name if it's a ColumnSortByInfo object, otherwise use as string
+            const columnName = typeof col === 'string' ? col : col.name || String(col);
+            if (!['Nodes', 'Chunks', ...mediumList].includes(columnName)) {
+                invalidMediums.push(columnName);
+            }
+        });
+
+        if (invalidMediums.length === 0) {
+            return undefined;
+        }
+
+        return {
+            message: 'Some mediums do not exist on current cluster',
+            inner_errors: invalidMediums.map((medium) => ({
+                message: `Medium ${medium} does not exist on current cluster`,
+            })),
+        };
+    }, [userColumns, mediumList]);
+
     return (
         <WidgetHeader
             title={name || i18n('title')}
@@ -17,6 +48,7 @@ export function AccountsWidgetHeader(props: AccountsWidgetProps) {
             page={'ACCOUNTS'}
             isLoading={isLoading}
             id={props.id}
+            error={error}
         />
     );
 }
