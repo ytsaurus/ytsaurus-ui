59a4a7b349ade4f391c11bbfbbfcafa52a61e7ab
Kanstantsin Autushka <autushka@qavm-f9b691f5.qemu>
2025-09-24 11:30:17 +0300

    feat(Dashboard2/Navigation): add path navigation [YTFRONT-5216]

--- packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Navigation/NavigationWidgetContent/NavigationWidgetContent.tsx
+++ packages/ui/src/ui/pages/dashboard2/Dashboard/Widgets/Navigation/NavigationWidgetContent/NavigationWidgetContent.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {FocusEvent} from 'react';
+import {useSelector} from 'react-redux';
+import {useHistory} from 'react-router';
+import {Flex} from '@gravity-ui/uikit';
 
 import {WidgetSkeleton} from '../../../../../../pages/dashboard2/Dashboard/components/WidgetSkeleton/WidgetSkeleton';
+import PathEditor from '../../../../../../containers/PathEditor/PathEditor';
 
 import {useNavigationWidget} from '../hooks/use-navigation-widget';
 import type {NavigationWidgetProps} from '../types';
 
 import {NavigationWidgetContentBase} from './NavigationWidgetContentBase';
+import {getCluster} from '../../../../../../store/selectors/global';
 
 export function NavigationWidgetContent(props: NavigationWidgetProps) {
     const {type, items, isLoading, error} = useNavigationWidget(props);
 
     return (
-        <>
+        <Flex width={'100%'} direction={'column'} gap={2}>
+            <DashboardPathEditor />
             {isLoading ? (
                 <WidgetSkeleton itemHeight={30} />
             ) : (
                 <NavigationWidgetContentBase pathsType={type} items={items || []} error={error} />
             )}
-        </>
+        </Flex>
     );
+}
+
+function DashboardPathEditor() {
+    const history = useHistory();
+    const cluster = useSelector(getCluster);
+
+    const handleApply = (path: string) => {
+        const normalizedPath = path.endsWith('/') ? path.slice(0, -1) : path;
+
+        const navigationUrl = `/${cluster}/navigation?path=${normalizedPath}`;
+        history.push(navigationUrl);
+    };
+
+    const handleFocus = React.useCallback((event: FocusEvent<HTMLInputElement>) => {
+        event.target?.select();
+    }, []);
+
+    return <PathEditor autoFocus defaultPath={''} onApply={handleApply} onFocus={handleFocus} />;
 }
