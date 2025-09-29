52378f1bc7f09c4591faefc92d1e00fb688d649d
simbiozizv <simbiozizv@yandex-team.ru>
2025-09-29 17:56:17 +0300

    fix(Queries/Chart): date in axis [YTFRONT-5105]

--- packages/ui/src/ui/pages/query-tracker/QueryResultsVisualization/preparers/getPointData.ts
+++ packages/ui/src/ui/pages/query-tracker/QueryResultsVisualization/preparers/getPointData.ts
@@ -1,25 +1,19 @@
 import {Result} from '../../module/query_result/types';
+import {DateTime64Types, NumberTypes} from '../../module/queryChart/constants/yqlTypes';
 // @ts-ignore
 import {timestamp} from '@gravity-ui/unipika/lib/plugins/yql-timestamp';
 
-export const NumberTypes = [
-    'yql.interval',
-    'yql.int32',
-    'yql.int64',
-    'yql.uint8',
-    'yql.uint32',
-    'yql.uint64',
-    'yql.float',
-    'yql.double',
-    'yql.decimal',
-];
-
-export const DateTimeTypes = ['yql.datetime', 'yql.date', 'yql.timestamp'];
-export const DateTime64Types = ['yql.datetime64', 'yql.date64', 'yql.timestamp64'];
-
 export const getPointValue = (value: Result) => {
     if (NumberTypes.includes(value.$type)) return Number(value.$value);
-    if (DateTimeTypes.includes(value.$type)) return Number(value.$value) * 1000;
+
+    if (value.$type === 'yql.date') {
+        const millisecondsInDay = 24 * 60 * 60 * 1000;
+        return Number(value.$value) * millisecondsInDay;
+    }
+
+    if (value.$type === 'yql.datetime' || value.$type === 'yql.timestamp') {
+        return Number(value.$value) * 1000;
+    }
 
     if (DateTime64Types.includes(value.$type)) {
         return timestamp(value);
