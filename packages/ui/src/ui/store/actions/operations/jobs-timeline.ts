57e5fa16e2d2dfabc6fbf065ac48a99c72319e32
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-10-02 14:12:04 +0300

    chore(PrometheusDashboard): dashboard should be recreated when type is changed [YTFRONT-4388]

--- packages/ui/src/ui/store/actions/operations/jobs-timeline.ts
+++ packages/ui/src/ui/store/actions/operations/jobs-timeline.ts
@@ -1,5 +1,3 @@
-import {getBatchError} from '../../../../shared/utils/error';
-
 import {YTApiId, ytApiV3Id} from '../../../rum/rum-wrap-api';
 import {ThunkAction} from 'redux-thunk';
 import {RootState} from '../../reducers';
