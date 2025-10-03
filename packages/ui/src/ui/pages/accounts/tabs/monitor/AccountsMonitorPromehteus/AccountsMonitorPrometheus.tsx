655779b5a7bd37d0243115276cd650a3d02347db
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-10-03 20:16:32 +0300

    chore(PrometheusDashboard): use common prometheusDashboardParams for accounts, bundles, chyt [YTFRONT-4388]

--- packages/ui/src/ui/pages/accounts/tabs/monitor/AccountsMonitorPromehteus/AccountsMonitorPrometheus.tsx
+++ packages/ui/src/ui/pages/accounts/tabs/monitor/AccountsMonitorPromehteus/AccountsMonitorPrometheus.tsx
@@ -11,6 +11,7 @@ import Select from '../../../../../components/Select/Select';
 import {Toolbar} from '../../../../../components/WithStickyToolbar/Toolbar/Toolbar';
 import {getAccountsMapByName} from '../../../../../store/selectors/accounts/accounts-ts';
 import {getMediumList} from '../../../../../store/selectors/thor';
+import {usePrometheusDashboardParams} from '../../../../../store/reducers/prometheusDashboard/prometheusDahsboard';
 
 import i18n from '../i18n';
 
@@ -19,11 +20,66 @@ type LeftRightMedium = {
     right_medium?: string;
 };
 
+const ACCOUNTS_DASHBOARD_TYPE = 'master-accounts';
+
 export function AccountsMonitorPrometheus({cluster, account}: {cluster: string; account: string}) {
+    const {accountData, options, params, selection, setSelection} = useAccountMonitoringParams({
+        cluster,
+        account,
+    });
+
+    return !accountData ? null : (
+        <>
+            <Toolbar
+                marginTopSkip
+                itemsToWrap={[
+                    {
+                        node: (
+                            <Select
+                                value={
+                                    selection?.left_medium !== undefined
+                                        ? [selection.left_medium]
+                                        : []
+                                }
+                                items={options}
+                                label={i18n('left-medium') + ':'}
+                                onUpdate={([left_medium]) => {
+                                    setSelection({left_medium});
+                                }}
+                            />
+                        ),
+                        width: 300,
+                    },
+                    {
+                        node: (
+                            <Select
+                                value={
+                                    selection?.right_medium !== undefined
+                                        ? [selection.right_medium]
+                                        : []
+                                }
+                                items={options}
+                                label={i18n('right-medium') + ':'}
+                                onUpdate={([right_medium]) => {
+                                    setSelection({right_medium});
+                                }}
+                            />
+                        ),
+                        width: 300,
+                    },
+                ]}
+            />
+            <PrometheusDashboardLazy type={ACCOUNTS_DASHBOARD_TYPE} params={params} />
+        </>
+    );
+}
+
+function useAccountMonitoringParams({cluster, account}: {cluster: string; account: string}) {
     const mediumList: Array<string> = useSelector(getMediumList);
     const accountData = useSelector(getAccountsMapByName)[account];
 
-    const [selection, setSelection] = React.useState<LeftRightMedium | undefined>();
+    const {params: selection, setParams: setSelection} =
+        usePrometheusDashboardParams<LeftRightMedium>(ACCOUNTS_DASHBOARD_TYPE);
 
     React.useEffect(() => {
         if (!accountData) {
@@ -43,12 +99,24 @@ export function AccountsMonitorPrometheus({cluster, account}: {cluster: string; 
             'limit',
         ).filter(Boolean);
 
-        const [left_medium, right_medium] = uniq_([
+        let {left_medium, right_medium} = selection;
+        if (-1 === mediumList.indexOf(left_medium!)) {
+            left_medium = undefined;
+        }
+
+        if (-1 === mediumList.indexOf(right_medium!)) {
+            right_medium = undefined;
+        }
+
+        const [left, right] = uniq_([
             ...mediumsDescBySize.map(({medium}) => medium),
             ...mediumList,
         ]);
-        setSelection({left_medium, right_medium});
-    }, [accountData, mediumList]);
+        setSelection({
+            left_medium: left_medium ?? left,
+            right_medium: right_medium ?? right ?? left,
+        });
+    }, [accountData, selection, mediumList, setSelection]);
 
     const params = React.useMemo(() => {
         return !cluster || !account
@@ -64,52 +132,5 @@ export function AccountsMonitorPrometheus({cluster, account}: {cluster: string; 
         return {value, text: value};
     });
 
-    return !accountData ? null : (
-        <>
-            <Toolbar
-                marginTopSkip
-                itemsToWrap={[
-                    {
-                        node: (
-                            <Select
-                                value={
-                                    selection?.left_medium !== undefined
-                                        ? [selection.left_medium]
-                                        : []
-                                }
-                                items={options}
-                                label={i18n('left-medium') + ':'}
-                                onUpdate={([left_medium]) => {
-                                    setSelection((state) => {
-                                        return {...state, left_medium};
-                                    });
-                                }}
-                            />
-                        ),
-                        width: 300,
-                    },
-                    {
-                        node: (
-                            <Select
-                                value={
-                                    selection?.right_medium !== undefined
-                                        ? [selection.right_medium]
-                                        : []
-                                }
-                                items={options}
-                                label={i18n('right-medium') + ':'}
-                                onUpdate={([right_medium]) => {
-                                    setSelection((state) => {
-                                        return {...state, right_medium};
-                                    });
-                                }}
-                            />
-                        ),
-                        width: 300,
-                    },
-                ]}
-            />
-            <PrometheusDashboardLazy type="master-accounts" params={params} />
-        </>
-    );
+    return {options, params, selection, setSelection, accountData};
 }
