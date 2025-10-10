94df880a0e116720b7ecaab2c4c76e26af39aa3f
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-10-10 19:19:48 +0300

    chore(Scheduling): move PoolTags [YTFRONT-5134]

--- packages/ui/src/ui/pages/scheduling/Content/tabs/Overview/SchedulingTable/SchedulingTable.tsx
+++ packages/ui/src/ui/pages/scheduling/Content/tabs/Overview/SchedulingTable/SchedulingTable.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import cn from 'bem-cn-lite';
 
 import type {ColumnDef} from '@gravity-ui/table/tanstack';
 
 import ColumnHeader from '../../../../../../components/ColumnHeader/ColumnHeader';
 import {DataTableGravity, useTable} from '../../../../../../components/DataTableGravity';
-import {setExpandedPools} from '../../../../../../store/actions/scheduling/expanded-pools';
-import {
-    getSchedulingOverviewTableItems,
-    getCurrentTreeExpandedPools,
-} from '../../../../../../store/selectors/scheduling/scheduling';
+import {getSchedulingOverviewTableItems} from '../../../../../../store/selectors/scheduling/scheduling';
 import {getSchedulingOperationsLoadingStatus} from '../../../../../../store/selectors/scheduling/expanded-pools';
 
 import './SchedulingTable.scss';
 
 import i18n from './i18n';
+import {NameCell} from './NameCell';
 
 const block = cn('yt-scheduling-table');
 
@@ -33,44 +30,19 @@ export function SchedulingTable() {
 export function useSchedulingTableColumns() {
     const loading = useSelector(getSchedulingOperationsLoadingStatus);
 
-    const dispatch = useDispatch();
-    const expandedPools = useSelector(getCurrentTreeExpandedPools);
-
-    const handlePoolExpand = React.useCallback(
-        (poolName: string, value: boolean) => {
-            dispatch(setExpandedPools({[poolName]: value}));
-        },
-        [dispatch],
-    );
-
     const columns: Array<ColumnDef<RowData>> = [
         {
             id: 'name',
             header: () => <ColumnHeader column={i18n('pool-operation')} loading={loading} />,
             cell: ({row}) => {
-                const {
-                    name,
-                    type,
-                    level = 0,
-                    child_pool_count = 0,
-                    pool_operation_count = 0,
-                } = row.original;
-                const allowExpand = child_pool_count || pool_operation_count;
-                const expanded = type === 'pool' ? expandedPools?.get(name) : undefined;
-                return (
-                    <div
-                        style={{paddingLeft: level * 40}}
-                        onClick={() => handlePoolExpand(name, !expanded)}
-                    >
-                        {allowExpand ? (expanded ? '- ' : '+ ') : null}
-                        {name}
-                    </div>
-                );
+                return <NameCell row={row.original} />;
             },
         },
         {
             id: 'actions',
-            cell: () => {},
+            cell: () => {
+                return '...';
+            },
         },
     ];
     return columns;
