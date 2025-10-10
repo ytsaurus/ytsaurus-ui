8eeec7b737d17bd287fa4628442f439f15792cc4
Kanstantsin Autushka <kostya.avtushko@yandex.by>
2025-10-10 15:13:02 +0300

    chore: update

--- packages/ui/src/ui/store/thunkDispatch.ts
+++ packages/ui/src/ui/store/thunkDispatch.ts
@@ -1,8 +1,8 @@
-import {AnyAction} from 'redux';
+import {UnknownAction} from 'redux';
 import {ThunkDispatch} from 'redux-thunk';
 import {RootState} from './reducers';
 import {useDispatch} from 'react-redux';
 
-export type AppThunkDispatch<T extends AnyAction> = ThunkDispatch<RootState, any, T>;
+export type AppThunkDispatch<T extends UnknownAction> = ThunkDispatch<RootState, any, T>;
 
-export const useThunkDispatch = () => useDispatch<AppThunkDispatch<AnyAction>>();
+export const useThunkDispatch = () => useDispatch<AppThunkDispatch<UnknownAction>>();
