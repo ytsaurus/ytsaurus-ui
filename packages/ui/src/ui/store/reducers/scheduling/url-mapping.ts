e0f271cfb40bc996fe327747c444b1140259a6a1
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-10-14 19:46:09 +0300

    chore(Scheduling): add url-mapping for content mode [YTFRONT-5134]

--- packages/ui/src/ui/store/reducers/scheduling/url-mapping.ts
+++ packages/ui/src/ui/store/reducers/scheduling/url-mapping.ts
@@ -1,15 +1,9 @@
 import {initialState as schedulingInitialState} from './scheduling';
 import {initialState as tableSortState} from '../../../store/reducers/tables';
 
-import {
-    SCHEDULING_POOL_CHILDREN_TABLE_ID,
-    SCHEDULING_POOL_TREE_TABLE_ID,
-} from '../../../constants/scheduling';
+import {SCHEDULING_POOL_TREE_TABLE_ID} from '../../../constants/scheduling';
 import {parseSortState} from '../../../utils';
-import {produce} from 'immer';
-import {updateIfChanged} from '../../../utils/utils';
-import {RootState} from '../../../store/reducers';
-import {aclFiltersParams, getAclFiltersPreparedState} from '../acl/url-mapping';
+import {aclFiltersParams} from '../acl/url-mapping';
 import {
     makeObjectParseSerialize,
     parseSerializeNumber,
@@ -27,13 +21,6 @@ export const schedulingParams = {
     },
 };
 
-export function getSchedulingPreparedState(state: RootState, {query}: {query: RootState}) {
-    return produce(state, (draft) => {
-        updateIfChanged(draft.scheduling.scheduling, 'pool', query.scheduling.scheduling.pool);
-        updateIfChanged(draft.scheduling.scheduling, 'tree', query.scheduling.scheduling.tree);
-    });
-}
-
 export const schedulingOverviewParams = {
     ...schedulingParams,
     sortState: {
@@ -41,6 +28,10 @@ export const schedulingOverviewParams = {
         initialState: {...tableSortState[SCHEDULING_POOL_TREE_TABLE_ID]},
         options: {parse: parseSortState},
         type: 'object',
+    },
+    contentMode: {
+        stateKey: 'scheduling.scheduling.contentMode',
+        initialState: schedulingInitialState.contentMode,
     },
     abc: {
         stateKey: 'scheduling.scheduling.abcServiceFilter',
@@ -57,45 +48,6 @@ export const schedulingOverviewParams = {
     },
 };
 
-export function getSchedulingOverviewPreparedState(state: RootState, {query}: {query: RootState}) {
-    state = getSchedulingPreparedState(state, {query});
-    return produce(state, (draft) => {
-        updateIfChanged(
-            draft.scheduling.scheduling,
-            'abcServiceFilter',
-            query.scheduling.scheduling.abcServiceFilter,
-        );
-        updateIfChanged(
-            draft.tables,
-            SCHEDULING_POOL_TREE_TABLE_ID,
-            query.tables[SCHEDULING_POOL_TREE_TABLE_ID],
-        );
-        updateIfChanged(
-            draft.scheduling.scheduling,
-            'operationRefId',
-            query.scheduling.scheduling.operationRefId,
-        );
-    });
-}
-
-export const schedulingDetailsParams = {
-    ...schedulingParams,
-    filter: {
-        stateKey: 'scheduling.scheduling.poolChildrenFilter',
-        initialState: schedulingInitialState.poolChildrenFilter,
-    },
-    contentMode: {
-        stateKey: 'scheduling.scheduling.contentMode',
-        initialState: schedulingInitialState.contentMode,
-    },
-    sortState: {
-        stateKey: `tables.${SCHEDULING_POOL_CHILDREN_TABLE_ID}`,
-        initialState: {...tableSortState[SCHEDULING_POOL_CHILDREN_TABLE_ID]},
-        options: {parse: parseSortState},
-        type: 'object',
-    },
-};
-
 export const schedulingMonitorParams = {
     ...schedulingParams,
 };
@@ -104,33 +56,3 @@ export const schedulingAclParams = {
     ...schedulingParams,
     ...aclFiltersParams,
 };
-
-export function getSchedulingDetailsPreparedState(state: RootState, {query}: {query: RootState}) {
-    state = getSchedulingPreparedState(state, {query});
-    return produce(state, (draft) => {
-        updateIfChanged(
-            draft.scheduling.scheduling,
-            'poolChildrenFilter',
-            query.scheduling.scheduling.poolChildrenFilter,
-        );
-        updateIfChanged(
-            draft.scheduling.scheduling,
-            'contentMode',
-            query.scheduling.scheduling.contentMode,
-        );
-        updateIfChanged(
-            draft.tables,
-            SCHEDULING_POOL_CHILDREN_TABLE_ID,
-            query.tables[SCHEDULING_POOL_CHILDREN_TABLE_ID],
-        );
-    });
-}
-
-export function getSchedulingMonitorPreparedState(state: RootState, location: {query: RootState}) {
-    return getSchedulingPreparedState(state, location);
-}
-
-export function getSchedulingAclPreparedState(prevState: RootState, {query}: {query: RootState}) {
-    const state = getAclFiltersPreparedState(prevState, {query});
-    return getSchedulingPreparedState(state, {query});
-}
