a45cd9a84d74ef2eeabab949603a1fa2059d5ad0
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-11-01 18:42:35 +0300

    chore(Scheduling/Meta): do not display 'Integral guarantees' for pools without the guarantee [YTFRONT-5134]

--- packages/ui/src/ui/pages/scheduling/Content/tabs/Overview/SchedulingMeta.tsx
+++ packages/ui/src/ui/pages/scheduling/Content/tabs/Overview/SchedulingMeta.tsx
@@ -1,13 +1,13 @@
+import cn from 'bem-cn-lite';
 import React from 'react';
 import {useSelector} from 'react-redux';
-import cn from 'bem-cn-lite';
 
 import {Progress} from '@gravity-ui/uikit';
 
 import format from '../../../../../common/hammer/format';
 
 import CollapsibleSection from '../../../../../components/CollapsibleSection/CollapsibleSection';
-import MetaTable, {MetaTableProps} from '../../../../../components/MetaTable/MetaTable';
+import MetaTable, {MetaTableItem} from '../../../../../components/MetaTable/MetaTable';
 import {ROOT_POOL_NAME} from '../../../../../constants/scheduling';
 import {getCurrentPool} from '../../../../../store/selectors/scheduling/scheduling';
 import {
@@ -36,12 +36,15 @@ export function SchedulingMeta() {
             maxRunningOperationCount,
             resources,
             integralType,
+            flowCPU = 0,
+            flowGPU = 0,
             weight,
         } = pool ?? {};
         const {cpu, gpu, user_memory} = resources ?? {};
         const hasStrong = cpu?.min! > 0 || gpu?.min! > 0 || user_memory?.min! > 0;
 
-        const hasIntegralType = integralType && integralType !== 'none';
+        const hasIntegralType =
+            (integralType && integralType !== 'none') || flowCPU > 0 || flowGPU > 0;
 
         const guaranteeType = [
             hasStrong && 'Strong',
@@ -53,42 +56,46 @@ export function SchedulingMeta() {
         const burstUnit = formatUnits(guarantees.burst);
         const flowUnit = formatUnits(guarantees.flow);
 
-        const items: MetaTableProps['items'] = [
-            [
-                {key: i18n('mode'), value: format.ReadableField(mode)},
-                {key: i18n('weight'), value: format.Number(weight)},
-                {
-                    key: i18n('operations'),
-                    value: renderProgress(operationCount, maxOperationCount),
-                },
-                {
-                    key: i18n('operations-running'),
-                    value: renderProgress(runningOperationCount, maxRunningOperationCount),
-                },
-            ],
-            [
-                {
-                    key: 'GPU',
-                    value: renderProgress(gpu?.usage, gpu?.min),
-                    visible: Boolean(gpu?.usage || gpu?.min),
-                },
-                {key: 'CPU', value: renderProgress(cpu?.usage, cpu?.min)},
-                {
-                    key: 'RAM',
-                    value: renderProgress(user_memory?.usage, user_memory?.min, 'Bytes'),
-                },
-            ],
-            [
+        const res = {
+            items: [
+                [
+                    {key: i18n('mode'), value: format.ReadableField(mode)},
+                    {key: i18n('weight'), value: format.Number(weight)},
+                    {
+                        key: i18n('operations'),
+                        value: renderProgress(operationCount, maxOperationCount),
+                    },
+                    {
+                        key: i18n('operations-running'),
+                        value: renderProgress(runningOperationCount, maxRunningOperationCount),
+                    },
+                ],
+                [
+                    {
+                        key: 'GPU',
+                        value: renderProgress(gpu?.usage, gpu?.min),
+                        visible: Boolean(gpu?.usage || gpu?.min),
+                    },
+                    {key: 'CPU', value: renderProgress(cpu?.usage, cpu?.min)},
+                    {
+                        key: 'RAM',
+                        value: renderProgress(user_memory?.usage, user_memory?.min, 'Bytes'),
+                    },
+                ],
+            ] as Array<Array<MetaTableItem>>,
+            subTitles: [i18n('general'), i18n('strong-guarantees')],
+        };
+
+        if (hasIntegralType) {
+            res.items.push([
                 {key: i18n('guarantee-type'), value: guaranteeType},
                 {key: i18n('integral-guarantee'), value: '??? ОТКУДА БРАТЬ ЗНАЧЕНИЯ ???'},
                 {key: i18n('burst-unit'), value: burstUnit, visible: burstUnit.length > 0},
                 {key: i18n('flow-unit'), value: flowUnit, visible: flowUnit.length > 0},
-            ],
-        ];
-        return {
-            items,
-            subTitles: [i18n('general'), i18n('strong-guarantees'), i18n('integral-guarantees')],
-        };
+            ]);
+            res.subTitles.push(i18n('integral-guarantees'));
+        }
+        return res;
     }, [pool, guarantees]);
 
     if (!pool?.name || pool?.name === ROOT_POOL_NAME) {
