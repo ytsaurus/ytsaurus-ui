6a6252aba28b7b63d6a525a81c0936f25120d388
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-11-06 13:24:36 +0300

    chore(Scheduling): bring back highlighted operation feature [YTFRONT-4914,YTFRONT-5134]

--- packages/ui/src/ui/components/DataTableGravity/DataTableGravity.tsx
+++ packages/ui/src/ui/components/DataTableGravity/DataTableGravity.tsx
@@ -16,9 +16,14 @@ export function DataTableGravity<TData, TScrollElement extends Element | Window>
     className,
     virtualized,
     rowHeight,
+    scrollToIndex,
+    rowClassName,
+    isHighlightedRow,
     ...props
 }: TableProps<TData, TScrollElement> & {
     style?: React.CSSProperties;
+    scrollToIndex?: number;
+    isHighlightedRow?: (row?: TData) => boolean;
 } & VirtuallizerProps) {
     const containerRef = React.useRef<HTMLTableSectionElement>(null);
 
@@ -31,9 +36,38 @@ export function DataTableGravity<TData, TScrollElement extends Element | Window>
 
     const virtualizerProps = virtualized ? {rowVirtualizer} : ({} as {});
 
+    React.useEffect(() => {
+        let timerId: ReturnType<typeof setTimeout>;
+        if (scrollToIndex !== undefined && scrollToIndex >= 0) {
+            timerId = setTimeout(
+                () => rowVirtualizer.scrollToIndex(scrollToIndex, {align: 'center'}),
+                500,
+            );
+        }
+        return () => clearTimeout(timerId);
+    }, [rowVirtualizer, scrollToIndex]);
+
+    const rowClassNameFn = React.useCallback<Exclude<typeof rowClassName, undefined | string>>(
+        (row) => {
+            const highlighted = isHighlightedRow ? isHighlightedRow(row?.original) : undefined;
+            return block(
+                'row',
+                {highlighted},
+                typeof rowClassName === 'string' ? rowClassName : rowClassName?.(row),
+            );
+        },
+        [rowClassName, isHighlightedRow],
+    );
+
     return (
         <div className={block({virtualized}, className)} ref={containerRef}>
-            <Table className={block('table')} stickyHeader {...virtualizerProps} {...props} />
+            <Table
+                className={block('table')}
+                stickyHeader
+                rowClassName={rowClassNameFn}
+                {...virtualizerProps}
+                {...props}
+            />
         </div>
     );
 }
