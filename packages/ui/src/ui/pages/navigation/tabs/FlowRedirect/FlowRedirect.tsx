ff4fa048342df84b26ad29b3651f5783a150f976
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-12-11 14:04:05 +0300

    chore(Navigation/Flow): move files to pages/flow [YTFRONT-5241]

---
+++ packages/ui/src/ui/pages/navigation/tabs/FlowRedirect/FlowRedirect.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {useSelector} from '../../../../store/redux-hooks';
+import {getPath} from '../../../../store/selectors/navigation';
+import {makeFlowLink} from '../../../../utils/app-url';
+import {getAppBrowserHistory} from '../../../../store/window-store';
+
+export function FlowRedirect() {
+    const path = useSelector(getPath);
+
+    React.useMemo(() => {
+        const url = makeFlowLink({path});
+        getAppBrowserHistory().replace(url);
+    }, [path]);
+
+    return null;
+}
--- packages/ui/src/ui/store/selectors/flow/filters.ts
+++ packages/ui/src/ui/store/selectors/flow/filters.ts
@@ -1,3 +1,5 @@
 import {RootState} from '../../../store/reducers';
 
 export const getFlowViewMode = (state: RootState) => state.flow.filters.flowViewMode;
+
+export const getPipelinePath = (state: RootState) => state.flow.filters.pipelinePath;
