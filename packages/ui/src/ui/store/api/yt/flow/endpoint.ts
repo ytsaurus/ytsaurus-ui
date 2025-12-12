e69d957e6183a65e51815bdaed1f5180c070f608
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-12-12 20:48:55 +0300

    chore(Flow): rework Computations table [YTFRONT-5241]

--- packages/ui/src/ui/store/api/yt/flow/endpoint.ts
+++ packages/ui/src/ui/store/api/yt/flow/endpoint.ts
@@ -1,22 +1,19 @@
+import {FlowExecuteCommand, FlowExecuteTypes} from '../../../../../shared/yt-types';
 import {YTApiId, ytApiV4Id} from '../../../../rum/rum-wrap-api';
 import {YTError} from '../../../../types';
 import {YTEndpointApiArgs} from '../types';
-import {
-    FlowExecuteCommand,
-    FlowExecuteCommandBody,
-    FlowExecuteData,
-    FlowExecuteParams,
-} from '../../../../../shared/yt-types';
 
-export type FlowApiArgs<T extends FlowExecuteCommand> = YTEndpointApiArgs<FlowExecuteParams<T>> &
-    FlowExecuteCommandBody<T>;
+export type FlowApiArgs<T extends FlowExecuteCommand> = YTEndpointApiArgs<
+    FlowExecuteTypes[T]['ParamsType']
+> &
+    FlowExecuteTypes[T]['BodyType'];
 
 export async function flowExecute<T extends FlowExecuteCommand>({...args}: FlowApiArgs<T>) {
     try {
         const data = (await ytApiV4Id.flowExecute(YTApiId.flowExecuteDescribePipeline, {
             ...args,
             data: args.body ?? {},
-        })) as FlowExecuteData[T];
+        })) as FlowExecuteTypes[T]['ResponseType'];
 
         return {data};
     } catch (error: unknown) {
