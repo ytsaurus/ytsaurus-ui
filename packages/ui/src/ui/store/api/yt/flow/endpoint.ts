d31c51ecb09ea11d8472dea4a2cf100d714bb024
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-12-19 13:28:14 +0300

    chore(Flow/Parition): add partition page [YTFRONT-5241]

--- packages/ui/src/ui/store/api/yt/flow/endpoint.ts
+++ packages/ui/src/ui/store/api/yt/flow/endpoint.ts
@@ -10,10 +10,13 @@ export type FlowApiArgs<T extends FlowExecuteCommand> = YTEndpointApiArgs<
 
 export async function flowExecute<T extends FlowExecuteCommand>({...args}: FlowApiArgs<T>) {
     try {
-        const data = (await ytApiV4Id.flowExecute(YTApiId.flowExecuteDescribePipeline, {
-            ...args,
-            data: args.body ?? {},
-        })) as FlowExecuteTypes[T]['ResponseType'];
+        const data = (await ytApiV4Id.flowExecute(
+            `${YTApiId.flowExecute}_${args.parameters.flow_command}`,
+            {
+                ...args,
+                data: args.body ?? {},
+            },
+        )) as FlowExecuteTypes[T]['ResponseType'];
 
         return {data};
     } catch (error: unknown) {
