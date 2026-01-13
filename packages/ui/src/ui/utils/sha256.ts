66931e0020475040c41f5a9a782f803e042f3d42
simbiozizv <simbiozizv@yandex-team.ru>
2026-01-13 12:21:06 +0300

    chore(Utils): error if crypto not supported [YTFRONT-5474]

--- packages/ui/src/ui/utils/sha256.ts
+++ packages/ui/src/ui/utils/sha256.ts
@@ -1,9 +1,17 @@
+export const isCryptoSubtleAvailable = () => {
+    return Boolean(crypto?.subtle?.digest);
+};
+
 export async function sha256(str: string) {
+    if (!isCryptoSubtleAvailable()) {
+        throw new Error(
+            'crypto.subtle is not available. ' +
+                'This usually happens when using HTTP instead of HTTPS. ' +
+                'Use isCryptoSubtleAvailable() to check before calling sha256().',
+        );
+    }
+
     const buf = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(str));
 
     return [...new Uint8Array(buf)].map((x) => ('00' + x.toString(16)).slice(-2)).join('');
 }
-
-export const isCryptoSubtleAvailable = () => {
-    return Boolean(crypto?.subtle?.digest);
-};
