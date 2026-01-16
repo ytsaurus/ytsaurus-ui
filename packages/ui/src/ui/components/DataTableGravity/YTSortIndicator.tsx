332acbc24d39171d2dd3c8e36fbda71c5e9418bf
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-01-16 16:44:16 +0300

    chore(Flow): use YT-specific icons for sort-indicators [YTFRONT-5241]

---
+++ packages/ui/src/ui/components/DataTableGravity/YTSortIndicator.tsx
@@ -0,0 +1,30 @@
+import {CaretDown, CaretUp, CaretsExpandVertical} from '@gravity-ui/icons';
+import {SortIndicatorProps} from '@gravity-ui/table';
+import {Icon} from '@gravity-ui/uikit';
+import React from 'react';
+import format from '../../common/hammer/format';
+import {Tooltip} from '../../components/Tooltip/Tooltip';
+
+function getIcon(order?: 'asc' | 'desc' | boolean) {
+    switch (order) {
+        case 'asc':
+            return CaretUp;
+        case 'desc':
+            return CaretDown;
+        default:
+            return CaretsExpandVertical;
+    }
+}
+
+export function YTSortIndicator<TData, TValue>({header}: SortIndicatorProps<TData, TValue>) {
+    const order = header.column.getIsSorted();
+
+    const allowSort = header.column.getCanSort();
+    const sortIcon = allowSort ? getIcon(order) : null;
+
+    return sortIcon ? (
+        <Tooltip content={order ? format.ReadableField(order) : null}>
+            <Icon data={sortIcon} size={16} />
+        </Tooltip>
+    ) : null;
+}
