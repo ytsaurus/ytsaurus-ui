71b502eb5d9cf0827af969b8b4513e230185e190
simbiozizv <simbiozizv@yandex-team.ru>
2026-03-12 15:56:13 +0300

    feat(Queries): multiple full result [YTFRONT-5615]

---
+++ packages/ui/src/ui/pages/query-tracker/QueryResultsView/QueryFullResultList/QueryFullResultRow.tsx
@@ -0,0 +1,68 @@
+import React, {FC} from 'react';
+import block from 'bem-cn-lite';
+import {Button, Flex, Icon, Link, Text} from '@gravity-ui/uikit';
+import ArrowUpRightFromSquareIcon from '@gravity-ui/icons/svgs/arrow-up-right-from-square.svg';
+import {makeNavigationLink} from '../../../../utils/app-url';
+import './QueryFullResultRow.scss';
+import TextIndentIcon from '@gravity-ui/icons/svgs/text-indent.svg';
+import {makePathByQueryEngine} from '../../Navigation/helpers/makePathByQueryEngine';
+import {insertTextWhereCursor} from '../../Navigation/helpers/insertTextWhereCursor';
+import {useMonaco} from '../../hooks/useMonaco';
+import {QueryEngine} from '../../../../../shared/constants/engines';
+import i18n from './i18n';
+
+type Props = {
+    id: number;
+    engine: QueryEngine;
+    cluster: string;
+    path: string;
+    isExist: boolean;
+};
+
+const b = block('yt-query-full-result-row');
+
+export const QueryFullResultRow: FC<Props> = ({id, cluster, engine, path, isExist}) => {
+    const {getEditor} = useMonaco();
+    const navigationLink = makeNavigationLink({cluster, path: `//${path}`});
+
+    const handlePastePath = () => {
+        if (!cluster) return;
+        const editor = getEditor('queryEditor');
+        const pathString = makePathByQueryEngine({
+            cluster,
+            path: `//${path}`,
+            engine,
+        });
+        insertTextWhereCursor(pathString, editor);
+    };
+
+    return (
+        <div className={b()}>
+            <Text variant="subheader-1">{i18n('title_full-result', {id: String(id)})}</Text>
+            {!isExist && <span>{i18n('alert_table-removed-from-cluster')}</span>}
+            {isExist ? (
+                <Link target="_blank" href={navigationLink}>
+                    {cluster}.`{path}`
+                </Link>
+            ) : (
+                <Text>
+                    {cluster}.`{path}`
+                </Text>
+            )}
+            <Flex alignItems="center" gap={1}>
+                <Button
+                    view="flat"
+                    disabled={!isExist}
+                    onClick={() => {
+                        window.open(navigationLink, '_blank');
+                    }}
+                >
+                    <Icon data={ArrowUpRightFromSquareIcon} size={16} />
+                </Button>
+                <Button view="flat" onClick={handlePastePath}>
+                    <Icon data={TextIndentIcon} size={16} />
+                </Button>
+            </Flex>
+        </div>
+    );
+};
