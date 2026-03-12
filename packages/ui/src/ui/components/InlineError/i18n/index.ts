654779bba9c57e5f39ca5d22c3377b282ceae928
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-03-12 21:42:43 +0300

    chore(InlineError): add i18n [YTFRONT-5079]

---
+++ packages/ui/src/ui/components/InlineError/i18n/index.ts
@@ -0,0 +1,8 @@
+import {addI18Keysets} from '../../../utils/i18n';
+
+import en from './en.json';
+import ru from './ru.json';
+
+const COMPONENT = 'yt:inline-error';
+
+export default addI18Keysets(COMPONENT, {en, ru});
