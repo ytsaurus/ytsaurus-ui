e93e73262a2a5188f48b7a71b6997e5f3dafa8f7
simbiozizv <simbiozizv@yandex-team.ru>
2026-03-17 16:18:53 +0300

    chore(Queries): use navigation module [YTFRONT-5601]

--- packages/ui/src/ui/pages/query-tracker/Navigation/NavigationTable/PreviewTab/i18n/index.ts
+++
@@ -1,6 +0,0 @@
-import {addI18Keysets} from '../../../../../../i18n';
-
-import en from './en.json';
-import ru from './ru.json';
-
-export default addI18Keysets('yt:query-tracker.navigation-table', {en, ru});
