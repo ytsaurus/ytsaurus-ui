8d459fd7523855163bc18eb723c7cff29e8f6da1
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-03-30 12:39:18 +0300

    chore(Flow): remove unnecessary store/*/flow/layout files [YTFRONT-5653]

--- packages/ui/src/ui/store/reducers/flow/index.ts
+++ packages/ui/src/ui/store/reducers/flow/index.ts
@@ -3,12 +3,10 @@ import {combineReducers} from 'redux';
 import {filters} from './filters';
 import {dynamicSpec, staticSpec} from './specs';
 import {status} from './status';
-import {layout} from './layout';
 
 export const flow = combineReducers({
     filters,
     dynamicSpec,
     staticSpec,
     status,
-    layout,
 });
