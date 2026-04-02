7d96450ab93b7184ce8f9f4fbcee50c3d3e41663
xexys <xexys@yandex-team.ru>
2026-04-02 14:34:26 +0300

    chore: getChytSpecletError -> selectChytSpecletError [YTFRONT-5653]

--- packages/ui/src/ui/store/selectors/chyt/speclet.ts
+++ packages/ui/src/ui/store/selectors/chyt/speclet.ts
@@ -3,4 +3,4 @@ import {RootState} from '../../reducers';
 export const selectChytSpecletLoaded = (state: RootState) => state.chyt.speclet.loaded;
 export const selectChytSpecletData = (state: RootState) => state.chyt.speclet.data;
 export const selectChytSpecletDataAlias = (state: RootState) => state.chyt.speclet.dataAlias;
-export const getChytSpecletError = (state: RootState) => state.chyt.speclet.error;
+export const selectChytSpecletError = (state: RootState) => state.chyt.speclet.error;
