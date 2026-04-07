6b5ce290fe2381f565fa3069de5d0e9c6063b9c8
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-04-07 17:46:24 +0300

    chore(InlineError): add i18n [YTFRONT-5069]

--- packages/ui/src/ui/components/InlineError/InlineError.tsx
+++ packages/ui/src/ui/components/InlineError/InlineError.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {YTError} from '../../../@types/types';
 import {ClickableText} from '../../components/ClickableText/ClickableText';
 import {showErrorPopup} from '../../utils/utils';
+import i18n from './i18n';
 
 type InlineErrorProps = {
     error?: YTError;
@@ -10,8 +11,10 @@ type InlineErrorProps = {
 export function InlineError({error}: InlineErrorProps) {
     return !error ? null : (
         <span className={'yt-error-inline'}>
-            {error.message || 'An error occured.'} &nbsp;
-            <ClickableText onClick={() => showErrorPopup(error)}>Details</ClickableText>
+            {error.message || i18n('error_default-message')} &nbsp;
+            <ClickableText onClick={() => showErrorPopup(error)}>
+                {i18n('action_details')}
+            </ClickableText>
         </span>
     );
 }
