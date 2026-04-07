0ff6457d0779c09050c917443d16649fd2c7ed21
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-04-07 17:48:43 +0300

    chore(Label): add i18n [YTFRONT-5069]

--- packages/components/src/components/Label/LabelOnOff.tsx
+++ packages/components/src/components/Label/LabelOnOff.tsx
@@ -1,11 +1,12 @@
 import {Label} from './Label';
 import {format} from '../../utils/hammer';
+import i18n from './i18n';
 
 export function LabelOnOff({value, className}: {value?: boolean; className?: string}) {
     if (value === undefined) {
         return format.NO_VALUE;
     }
     const theme = value ? 'success' : 'danger';
-    const text = value ? 'On' : 'Off';
+    const text = value ? i18n('value_on') : i18n('value_off');
     return <Label theme={theme} text={text} className={className} />;
 }
