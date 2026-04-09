9d49795dc1649ae597ca853b7573e779fcc0fe5f
xexys <xexys@yandex-team.ru>
2026-04-09 14:41:48 +0300

    chore: add RoutedLink container [YTFRONT-5711]

---
+++ packages/ui/src/ui/containers/RoutedLink/RoutedLink.tsx
@@ -0,0 +1,32 @@
+import {Link, LinkProps} from '@gravity-ui/uikit';
+import React, {forwardRef} from 'react';
+import {Link as RouterLink} from 'react-router-dom';
+import {makeRoutedURL} from '../../store/window-store';
+import {useRoutedClickHandler} from '../../hooks/useRoutedClickHandler';
+
+type LinkAdapterProps = LinkProps & {
+    navigate: () => void;
+};
+
+const LinkAdapter = forwardRef<HTMLAnchorElement, LinkAdapterProps>(function LinkAdapter(
+    {navigate, onClick, ...props},
+    ref,
+) {
+    const handleClick = useRoutedClickHandler({navigate, onClick});
+
+    return <Link {...props} onClick={handleClick} ref={ref} />;
+});
+
+export type RoutedLinkProps = LinkProps & {
+    disablePreserveLocation?: boolean;
+};
+
+export const RoutedLink = forwardRef<HTMLAnchorElement, RoutedLinkProps>(function RoutedLink(
+    {disablePreserveLocation, ...linkProps},
+    ref,
+) {
+    const {href} = linkProps;
+    const to = disablePreserveLocation ? href : () => makeRoutedURL(href);
+
+    return <RouterLink {...linkProps} component={LinkAdapter} to={to} ref={ref} />;
+});
