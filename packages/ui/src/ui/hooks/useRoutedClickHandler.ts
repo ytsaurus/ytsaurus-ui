05d774ee346601eaeb148cf949213270f894435e
xexys <xexys@yandex-team.ru>
2026-04-09 14:38:34 +0300

    chore: add useRoutedClickHandler hook [YTFRONT-5711]

---
+++ packages/ui/src/ui/hooks/useRoutedClickHandler.ts
@@ -0,0 +1,26 @@
+import {MouseEvent, MouseEventHandler, useCallback} from 'react';
+
+const isModifiedEvent = (event: MouseEvent): boolean => {
+    return event.metaKey || event.altKey || event.ctrlKey || event.shiftKey;
+};
+
+type Params = {
+    navigate: () => void;
+    onClick: MouseEventHandler | undefined;
+};
+
+export const useRoutedClickHandler = ({navigate, onClick}: Params): MouseEventHandler => {
+    return useCallback(
+        (event) => {
+            onClick?.(event);
+
+            if (event.defaultPrevented || isModifiedEvent(event)) {
+                return;
+            }
+
+            event.preventDefault();
+            navigate();
+        },
+        [navigate, onClick],
+    );
+};
