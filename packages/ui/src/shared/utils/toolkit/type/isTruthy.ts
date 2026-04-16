c2bdc568a3c0ec320d514854429e5080440df786
xexys <xexys@yandex-team.ru>
2026-04-16 11:37:03 +0300

    chore: isTruthy types fix [YTFRONT-5266]

--- packages/ui/src/shared/utils/toolkit/type/isTruthy.ts
+++ packages/ui/src/shared/utils/toolkit/type/isTruthy.ts
@@ -1,5 +1,5 @@
 type Falsy = false | '' | 0 | null | undefined;
 
-export const isTruthy = <T>(value: T | Falsy): value is T => {
+export const isTruthy = <T>(value: T): value is Exclude<T, Falsy> => {
     return Boolean(value);
 };
