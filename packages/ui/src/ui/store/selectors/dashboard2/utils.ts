7a18ff39a02cf20a9281d2b6657f9f73e4469184
xexys <xexys@yandex-team.ru>
2026-04-17 17:53:23 +0300

    chore: fix dashboard2/utils [YTFRONT-5653]

--- packages/ui/src/ui/store/selectors/dashboard2/utils.ts
+++ packages/ui/src/ui/store/selectors/dashboard2/utils.ts
@@ -2,12 +2,16 @@ import {createSelector} from '@reduxjs/toolkit';
 
 import find_ from 'lodash/find';
 
-import {selectDashboardConfig} from '../../../store/selectors/dashboard2/dashboard';
+import {selectDashboardConfig} from './dashboard';
 
 export function createWidgetDataFieldSelector<T>(field: string, fallback?: T) {
-    return createSelector([selectDashboardConfig, (_, widgetId) => widgetId], (config, widgetId) => {
-        const items = config.items;
-        const widget = find_(items, (item) => item.id === widgetId);
-        return (widget?.data?.[field] || fallback) as T;
-    });
+    return createSelector(
+        [selectDashboardConfig, (_, widgetId) => widgetId],
+        (config, widgetId) => {
+            const items = config.items;
+            const widget = find_(items, (item) => item.id === widgetId);
+
+            return (widget?.data?.[field] || fallback) as T;
+        },
+    );
 }
