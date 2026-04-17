8c0a7931274c3e0071b6c671d053f65478db44ef
xexys <xexys@yandex-team.ru>
2026-04-17 15:57:25 +0300

    chore: add operation logName param to url utils [YTFRONT-5734]

--- packages/ui/src/ui/utils/app-url/makeOperationLogsUrl.ts
+++ packages/ui/src/ui/utils/app-url/makeOperationLogsUrl.ts
@@ -4,8 +4,17 @@ import {YT} from '../../config/yt-config';
 type Params = {
     cluster?: string;
     operationId: string;
+    logName?: string;
 };
 
-export const makeOperationLogsUrl = ({cluster, operationId}: Params): string => {
-    return `/${cluster || YT.cluster}/${Page.OPERATIONS}/${operationId}/logs`;
+export const makeOperationLogsUrl = ({cluster, operationId, logName}: Params): string => {
+    const pathname = `/${cluster || YT.cluster}/${Page.OPERATIONS}/${operationId}/logs`;
+
+    const searchParams = new URLSearchParams();
+
+    if (logName) {
+        searchParams.set('logName', logName);
+    }
+
+    return searchParams.size > 0 ? `${pathname}?${searchParams}` : pathname;
 };
