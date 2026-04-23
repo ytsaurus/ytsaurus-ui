873129c45810e7369bb6b03785cf958e9aca8d55
lugertr <nikitavishnev@bk.ru>
2026-04-23 18:11:23 +0300

    fix(Navigation/tablets): correcting the sorting and display of fields [YTFRONT-5611]

--- packages/ui/src/ui/store/selectors/navigation/tabs/tablets.js
+++ packages/ui/src/ui/store/selectors/navigation/tabs/tablets.js
@@ -18,52 +18,45 @@ export const getTabletsSortState = (state) => state.tables[NAVIGATION_TABLETS_TA
 const getTabletsFilter = (state) => state.navigation.tabs.tablets.tabletsFilter;
 export const getActiveHistogram = (state) => state.navigation.tabs.tablets.histogramType;
 
-const selectSortedReplicationLagTimes = createSelector(
-    [getRawReplicationLagTimes, getTabletsSortState],
-    (rawTablets, sortState) => hammer.utils.sort(rawTablets, sortState, tableItems),
-);
+const selectMergedRawTablets = createSelector(
+    [getRawTablets, getRawReplicationLagTimes],
+    (rawTablets, rawReplicationLagTimes) => {
+        if (!rawReplicationLagTimes || rawReplicationLagTimes.length === 0) {
+            return rawTablets;
+        }
 
-const selectFilteredReplicationLagTimes = createSelector(
-    [selectSortedReplicationLagTimes, getTabletsFilter],
-    (sortedTablets, tabletsFilter) =>
-        hammer.filter.filter({
-            data: sortedTablets,
-            input: tabletsFilter,
-            factors: [
-                function (item) {
-                    return tableItems['replication_lag_time']?.get?.(item);
-                },
-                function (item) {
-                    return tableItems['replication_mode']?.get?.(item);
-                },
-            ],
-        }),
-);
-
-const selectPreparedReplicationLagTimes = createSelector(
-    [selectFilteredReplicationLagTimes],
-    (items) => {
-        const res = prepareDataForColumns(items);
-        return res;
-    },
-);
-
-const selectReplicationLagTimesMap = createSelector(
-    [selectPreparedReplicationLagTimes],
-    (replicationLagTimes) => {
-        const map = new Map();
-        replicationLagTimes.forEach((item) => {
-            map.set(item.tablet_id, {
-                replication_lag_time: item.replication_lag_time,
-                replication_mode: item.replication_mode,
+        const replicationByTabletId = new Map();
+        rawReplicationLagTimes.forEach((item) => {
+            const tabletId = tableItems['tablet_id'].get(item);
+            if (!tabletId) return;
+            replicationByTabletId.set(tabletId, {
+                replication_lag_time: tableItems['replication_lag_time'].get(item),
+                replication_mode: tableItems['replication_mode'].get(item),
             });
         });
-        return map;
+
+        if (replicationByTabletId.size === 0) {
+            return rawTablets;
+        }
+
+        return rawTablets.map((tablet) => {
+            const tabletId = tableItems['tablet_id'].get(tablet);
+            const replicationFields = replicationByTabletId.get(tabletId);
+            if (!replicationFields) return tablet;
+
+            if (tablet && tablet.$value !== undefined) {
+                return {
+                    ...tablet,
+                    $value: {...tablet.$value, ...replicationFields},
+                };
+            }
+            return {...tablet, ...replicationFields};
+        });
     },
 );
 
 const selectSortedTablets = createSelector(
-    [getRawTablets, getTabletsSortState],
+    [selectMergedRawTablets, getTabletsSortState],
     (rawTablets, sortState) => hammer.utils.sort(rawTablets, sortState, tableItems),
 );
 
@@ -86,6 +79,12 @@ const selectFilteredTablets = createSelector(
                 function (item) {
                     return tableItems['cell_leader_address'].get(item);
                 },
+                function (item) {
+                    return tableItems['replication_lag_time']?.get?.(item);
+                },
+                function (item) {
+                    return tableItems['replication_mode']?.get?.(item);
+                },
             ],
         }),
 );
@@ -96,36 +95,22 @@ export const selectPreparedDataForColumns = createSelector([selectFilteredTablet
     return res;
 });
 
-const selectMergedPreparedTablets = createSelector(
-    [selectPreparedDataForColumns, selectReplicationLagTimesMap],
-    (tablets, replicationLagMap) => {
-        return tablets.map((tablet) => {
-            const replicationData = replicationLagMap.get(tablet.tablet_id);
-            if (replicationData) {
-                return {
-                    ...tablet,
-                    replication_lag_time: replicationData.replication_lag_time,
-                    replication_mode: replicationData.replication_mode,
-                };
-            }
-            return tablet;
-        });
-    },
-);
-
 export const selectIsReplicationDataExist = createSelector(
-    [selectMergedPreparedTablets],
-    (tablets) => {
-        if (!tablets || tablets.length === 0) {
+    [getRawReplicationLagTimes],
+    (rawReplicationLagTimes) => {
+        if (!rawReplicationLagTimes || rawReplicationLagTimes.length === 0) {
             return false;
         }
-        return tablets.some(
-            (tablet) => tablet && (tablet.replication_lag_time || Boolean(tablet.replication_mode)),
+        return rawReplicationLagTimes.some(
+            (item) =>
+                item &&
+                (tableItems['replication_lag_time']?.get?.(item) ||
+                    Boolean(tableItems['replication_mode']?.get?.(item))),
         );
     },
 );
 
-export const selectTablets = createSelector(selectMergedPreparedTablets, (filteredTablets) => {
+export const selectTablets = createSelector(selectPreparedDataForColumns, (filteredTablets) => {
     const aggregation = prepareAggregation(filteredTablets);
     return [aggregation, ...filteredTablets];
 });
