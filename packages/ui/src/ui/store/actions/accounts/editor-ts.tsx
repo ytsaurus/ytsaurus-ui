ea6003541805b0e5a9e9af5056daf2c04cbc7bb4
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-05-12 17:41:07 +0300

    chore(Accounts/Editor): move some functions to store/actions/accounts [YTFRONT-5762]

--- packages/ui/src/ui/store/actions/accounts/editor-ts.tsx
+++ packages/ui/src/ui/store/actions/accounts/editor-ts.tsx
@@ -1,15 +1,24 @@
 import update_ from 'lodash/update';
-
-import {RootState} from '../../../store/reducers';
-import {setAccountLimit} from '../../../utils/accounts/editor';
-import {ThunkAction} from 'redux-thunk';
-import {FIX_MY_TYPE} from '../../../types';
-
+import {type ThunkAction} from 'redux-thunk';
+import {type RootState} from '../../../store/reducers';
+import {type FIX_MY_TYPE} from '../../../types';
+import {
+    createAccount,
+    createAccountHome,
+    setAccountAbc,
+    setAccountLimit,
+} from '../../../utils/accounts/editor';
 // @ts-ignore
 import yt from '@ytsaurus/javascript-wrapper/lib/yt';
 import {RESOURCES_LIMITS_PREFIX} from '../../../constants/accounts';
-import {accountsIncreaseEditCounter, loadEditedAccount} from './accounts';
+import {ROOT_ACCOUNT_NAME} from '../../../constants/accounts/accounts';
+import {IdmObjectType} from '../../../constants/acl';
+import {selectCluster} from '../../../store/selectors/global';
+import {updateAcl} from '../../../utils/acl/acl-api';
+import {type ResponsibleType} from '../../../utils/acl/acl-types';
 import {wrapApiPromiseByToaster} from '../../../utils/utils';
+import {accountsIncreaseEditCounter, loadEditedAccount} from './accounts';
+import i18n from './i18n';
 
 export interface AccountQuotaParams {
     limit: number;
@@ -53,10 +62,53 @@ export function setAccountQuota(params: AccountQuotaParams): EditorAction {
         const toasterName = params.account + '_' + params.resourcePath;
         return wrapApiPromiseByToaster(setAccountQuotaImpl(params), {
             toasterName,
-            successContent: 'The quota is updated',
+            successContent: i18n('alert_quota-updated'),
         }).then(() => {
             dispatch(loadEditedAccount(params.account));
             dispatch(accountsIncreaseEditCounter());
+        });
+    };
+}
+
+function setResponsibleUsers(
+    users: Array<ResponsibleType>,
+    accountName: string,
+    inheritAcl: boolean,
+): EditorAction {
+    return (_dispatch, getState) => {
+        const path = accountName;
+        const cluster = selectCluster(getState());
+
+        return updateAcl(cluster, path, {
+            idmKind: IdmObjectType.ACCOUNT,
+            responsible: users,
+            inheritAcl,
+        });
+    };
+}
+
+export interface NewAccountInfo {
+    abcService?: {slug: string; id: number};
+    account: string;
+    parentAccount: string;
+    responsibles: Array<ResponsibleType>;
+    createHome: boolean;
+}
+
+export function createAccountFromInfo(newAccountInfo: NewAccountInfo): EditorAction {
+    return (dispatch) => {
+        const {abcService, account, parentAccount, responsibles, createHome} = newAccountInfo;
+
+        return createAccount(account, parentAccount).then(() => {
+            const {id, slug} = abcService || {};
+
+            return Promise.all([
+                setAccountAbc(account, id, slug).catch(() => {}),
+                createHome ? createAccountHome(account).catch(() => {}) : Promise.resolve(),
+                dispatch(
+                    setResponsibleUsers(responsibles, account, parentAccount !== ROOT_ACCOUNT_NAME),
+                ).catch(() => {}),
+            ]);
         });
     };
 }
