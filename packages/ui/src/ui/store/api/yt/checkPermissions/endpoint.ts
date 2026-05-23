b5f9f41104387fd944c1543d1611ab65ccc364a9
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-05-24 00:03:22 +0300

    chore(store/api/yt): add checkPermissions endpoint [YTFRONT-5782]

---
+++ packages/ui/src/ui/store/api/yt/checkPermissions/endpoint.ts
@@ -0,0 +1,18 @@
+import {YTError} from '../../../../../@types/types';
+import {type YTApiIdType} from '../../../../../shared/constants/yt-api-id';
+import {type CheckPermissionsParams} from '../../../../../shared/yt-types';
+import {ytApiV3Id} from '../../../../rum/rum-wrap-api';
+import {type YTEndpointApiArgs} from '../types';
+
+export type CheckPermissionParams = YTEndpointApiArgs<CheckPermissionsParams> & {
+    id: YTApiIdType;
+};
+
+export async function checkPermission({id, ...rest}: CheckPermissionParams) {
+    try {
+        const response: {action: 'allow' | 'deny'} = await ytApiV3Id.checkPermission(id, rest);
+        return {data: response};
+    } catch (error) {
+        return {error: error as YTError};
+    }
+}
---
+++ packages/ui/src/ui/store/api/yt/checkPermissions/index.ts
@@ -0,0 +1,37 @@
+import {DEFAULT_UPDATER_TIMEOUT} from '../../../../hooks/use-updater';
+import {useSelector} from '../../../../store/redux-hooks';
+import {selectCluster} from '../../../../store/selectors/global/cluster';
+import {getUseAutoRefresh} from '../../../../store/selectors/settings/settings-ts';
+import {OverrideDataType} from '../types';
+import {ytApi} from '../ytApi';
+import {type CheckPermissionParams, checkPermission} from './endpoint';
+
+const checkPermissionApi = ytApi.injectEndpoints({
+    endpoints: (build) => ({
+        checkPermissions: build.query({
+            queryFn: checkPermission,
+            providesTags: (_result, _error, params) => [params.id],
+        }),
+    }),
+});
+
+export function useCheckPermissionQuery(params: CheckPermissionParams) {
+    const useAutoRefresh = useSelector(getUseAutoRefresh);
+    const cluster = useSelector(selectCluster);
+
+    const options = {
+        pollingInterval: useAutoRefresh ? DEFAULT_UPDATER_TIMEOUT : undefined,
+        skipPollingIfUnfocused: true,
+    };
+
+    const customParams =
+        'setup' in params
+            ? params
+            : {
+                  ...params,
+                  cluster,
+              };
+
+    const res = checkPermissionApi.useCheckPermissionsQuery(customParams, options);
+    return res as OverrideDataType<typeof res, (typeof res)['data']>;
+}
