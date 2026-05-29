ef59e4f84df2b718e60ee6fd87d0bd4e696615a1
simbiozizv <simbiozizv@yandex-team.ru>
2026-05-29 16:03:19 +0300

    chore(System): remove unused selector [YTFRONT-5653]

--- packages/ui/src/ui/store/selectors/system/index.ts
+++
@@ -1,6 +0,0 @@
-import {type RootState} from '../../reducers';
-
-export const isSystemResourcesLoaded = (state: RootState) => {
-    const {loaded, nodeAttrsLoaded} = state.system.resources;
-    return loaded && nodeAttrsLoaded;
-};
