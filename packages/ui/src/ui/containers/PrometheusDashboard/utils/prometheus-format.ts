44bfa7662e9e0faeb739dd307d3ebd2d3942588d
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-06-10 14:54:37 +0300

    chore(PrometheusDashboard/Chart): format axis labels [YTFRONT-5831]

--- packages/ui/src/ui/containers/PrometheusDashboard/utils/prometheus-format.ts
+++ packages/ui/src/ui/containers/PrometheusDashboard/utils/prometheus-format.ts
@@ -1,7 +1,11 @@
 import {type PrometheusDashboardUnitType} from '../../../../shared/prometheus/types';
 import format from '../../../common/hammer/format';
 
-export function getPrometheusFormatter(unit: PrometheusDashboardUnitType | unknown) {
+export type ValueFormatterFn = (v: number | string | null) => string;
+
+export function getPrometheusFormatter(
+    unit: PrometheusDashboardUnitType | string | undefined,
+): ValueFormatterFn {
     switch (unit) {
         case 'bytes':
         case 'decbytes':
@@ -10,16 +14,16 @@ export function getPrometheusFormatter(unit: PrometheusDashboardUnitType | unkno
         case 'gbytes':
             return formatGBytes;
         case 'Bps':
-            return (v: number) => format.Bytes(v) + '/s';
+            return (v: number | string | null) => format.Bytes(v) + '/s';
         case 'iops':
-            return (v: number) => format.NumberSmart(v) + 'io/s';
+            return (v: number | string | null) => format.NumberSmart(v) + 'io/s';
         default:
             return format.NumberSmart;
     }
 }
 
-function formatGBytes(value: number | string | undefined) {
+function formatGBytes(value: number | string | null) {
     const NAMES = [' GiB', ' TiB', ' PiB', ' EiB'];
 
-    return format.FormatWithSuffixes({value, NAMES, DIVIDER: 1024});
+    return format.FormatWithSuffixes({value, NAMES, DIVIDER: 1024, digits: 2});
 }
