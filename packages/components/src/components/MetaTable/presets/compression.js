708f47b28166918c809eac7f04fcefb3e94bce94
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-06-19 20:15:23 +0300

    chore(@ytsaurus/components): add i18n for MetaTable presets [YTFRONT-5069]

--- packages/components/src/components/MetaTable/presets/compression.js
+++ packages/components/src/components/MetaTable/presets/compression.js
@@ -1,4 +1,5 @@
 import {format, ypath} from '../../../utils';
+import i18n from './i18n';
 
 export const compression = (attributes) => {
     const [compressionRatio, compressionCodec] = ypath.getValues(attributes, [
@@ -9,10 +10,12 @@ export const compression = (attributes) => {
     return [
         {
             key: 'compression_ratio',
+            label: i18n('field_compression-ratio'),
             value: format['Number'](compressionRatio, {digits: 5}),
         },
         {
             key: 'compression_codec',
+            label: i18n('field_compression-codec'),
             value: compressionCodec,
         },
     ];
