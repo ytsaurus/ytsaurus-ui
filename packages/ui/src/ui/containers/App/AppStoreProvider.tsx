f1c321962182e93c816124c0170a7f4587b6e054
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-06-24 15:38:51 +0300

    fix(App/Toaster): better background color for toasts [YTFRONT-5856]

--- packages/ui/src/ui/containers/App/AppStoreProvider.tsx
+++ packages/ui/src/ui/containers/App/AppStoreProvider.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import {ThemeProvider} from '@gravity-ui/uikit';
-
 import {Provider} from 'react-redux';
+import cn from 'bem-cn-lite';
 import {useSelector} from '../../store/redux-hooks';
 import ErrorBoundary from '../../containers/ErrorBoundary/ErrorBoundary';
 import {getWindowStore} from '../../store/window-store';
 import {selectTheme} from '../../store/selectors/global';
+import './AppStoreProvider.scss';
+
+const block = cn('yt-app-store-provider');
 
 export function AppStoreProvider({children}: {children: React.ReactNode}) {
     return (
@@ -20,5 +22,9 @@ export function AppStoreProvider({children}: {children: React.ReactNode}) {
 
 export function AppThemeProvider({children}: {children: React.ReactNode}) {
     const theme = useSelector(selectTheme);
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+    return (
+        <ThemeProvider theme={theme} rootClassName={block()}>
+            {children}
+        </ThemeProvider>
+    );
 }
