da32ad526bb6dd94758c04a56beed9a5aa4bbbec
simbiozizv <simbiozizv@yandex-team.ru>
2026-07-09 11:59:35 +0300

    chore(UI): rename tablet selectors [YTFRONT-5653]

--- packages/ui/src/ui/store/selectors/tablet/stores.js
+++ packages/ui/src/ui/store/selectors/tablet/stores.js
@@ -3,9 +3,10 @@ import {createSelector} from 'reselect';
 import {storesTableItems} from '../../../utils/tablet/table';
 import {TABLET_PARTITION_STORES_TABLE_ID} from '../../../constants/tablet';
 
-const getRawStores = (state) => state.tablet.stores.stores;
-const getSortState = (state) => state.tables[TABLET_PARTITION_STORES_TABLE_ID];
+const selectRawStores = (state) => state.tablet.stores.stores;
+const selectSortState = (state) => state.tables[TABLET_PARTITION_STORES_TABLE_ID];
 
-export const getStores = createSelector([getRawStores, getSortState], (stores, sortState) =>
-    hammer.utils.sort(stores, sortState, storesTableItems),
+export const selectStores = createSelector(
+    [selectRawStores, selectSortState],
+    (stores, sortState) => hammer.utils.sort(stores, sortState, storesTableItems),
 );
