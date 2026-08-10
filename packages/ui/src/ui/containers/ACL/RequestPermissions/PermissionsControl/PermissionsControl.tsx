a8c158c69bb1b69575e236c9872a78ef7976d789
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-08-10 17:34:20 +0300

    feat(ACL): hide 'Full read' under 'Additional permissions' [YTFRONT-5871]

--- packages/ui/src/ui/containers/ACL/RequestPermissions/PermissionsControl/PermissionsControl.tsx
+++ packages/ui/src/ui/containers/ACL/RequestPermissions/PermissionsControl/PermissionsControl.tsx
@@ -1,14 +1,19 @@
-import {Checkbox} from '@gravity-ui/uikit';
+import {Alert, Checkbox, Disclosure} from '@gravity-ui/uikit';
 import cn from 'bem-cn-lite';
 import indexOf_ from 'lodash/indexOf';
 import isEmpty_ from 'lodash/isEmpty';
+import isEqual_ from 'lodash/isEqual';
 import map_ from 'lodash/map';
 import React, {Component} from 'react';
 import i18nPermissionValues from '../../../../containers/ACL/i18n-permission-values';
 import {type YTPermissionTypeUI} from '../../../../utils/acl/acl-api';
+import i18n from './i18n';
 import './PermissionsControl.scss';
+import {YTText} from '@ytsaurus/components';
 
 const block = cn('acl-permissions-control');
+
+const ADDITIONAL_PERMISSIONS = ['full_read'];
 
 interface Props {
     value: Record<string, Array<YTPermissionTypeUI>>;
@@ -54,10 +59,9 @@ export default class PermissionsControl extends Component<Props, State> {
     renderPermissionCheckbox(
         permissionName: string,
         permissionsToSet: Array<YTPermissionTypeUI>,
-        index: number,
+        isDisabled: boolean,
     ) {
-        const {value, disabled, disabledChoices} = this.props;
-        const itemDisabled = disabled || indexOf_(disabledChoices, index) !== -1;
+        const {value} = this.props;
 
         const content = permissionsToSet
             .map((item) => {
@@ -71,29 +75,66 @@ export default class PermissionsControl extends Component<Props, State> {
                 key={permissionName}
                 content={content}
                 onChange={() => this.handleCheckboxChange(permissionName, permissionsToSet)}
-                disabled={itemDisabled}
+                disabled={isDisabled}
                 className={block('item')}
             />
         );
     }
 
-    renderChoices(choices?: Required<Props>['choices']) {
-        return map_(choices, (item, index) => {
-            const name = PermissionsControl.getChoiceName(item);
-            return (
-                <React.Fragment key={index}>
-                    {this.renderPermissionCheckbox(name, item, index)}
-                </React.Fragment>
-            );
-        });
-    }
-
     render() {
-        const {choices, error} = this.props;
+        const {choices, disabled, disabledChoices, error} = this.props;
+
+        const mainChoices: Array<{item: Array<YTPermissionTypeUI>; isDisabled: boolean}> = [];
+        const additionalChoices: Array<{item: Array<YTPermissionTypeUI>; isDisabled: boolean}> = [];
+
+        (choices ?? []).forEach((item, index) => {
+            const isDisabled = disabled || indexOf_(disabledChoices, index) !== -1;
+            if (isEqual_(ADDITIONAL_PERMISSIONS, item)) {
+                additionalChoices.push({item, isDisabled});
+            } else {
+                mainChoices.push({item, isDisabled});
+            }
+        });
+
+        const hasAdditional = additionalChoices.length > 0;
 
         return (
             <div className={block()}>
-                {this.renderChoices(choices)}
+                {map_(mainChoices, ({item, isDisabled}, index) => {
+                    const name = PermissionsControl.getChoiceName(item);
+                    return (
+                        <React.Fragment key={index}>
+                            {this.renderPermissionCheckbox(name, item, isDisabled)}
+                        </React.Fragment>
+                    );
+                })}
+                {hasAdditional && (
+                    <Disclosure
+                        className={block('additional')}
+                        defaultExpanded={false}
+                        arrowPosition="right"
+                        summary={i18n('title_additional-permissions')}
+                    >
+                        <div className={block('additional-content')}>
+                            {map_(additionalChoices, ({item, isDisabled}, index) => {
+                                const name = PermissionsControl.getChoiceName(item);
+                                return (
+                                    <React.Fragment key={index}>
+                                        {this.renderPermissionCheckbox(name, item, isDisabled)}
+                                    </React.Fragment>
+                                );
+                            })}
+                            <Alert
+                                theme="info"
+                                message={
+                                    <YTText color="secondary">
+                                        {i18n('context_full-read-note')}
+                                    </YTText>
+                                }
+                            />
+                        </div>
+                    </Disclosure>
+                )}
                 {error && <div className={block('error-message')}>{error}</div>}
             </div>
         );
