740ee861be638cf43b0dfcb4d5f02f838181c4a6
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-08-14 00:22:06 +0300

    chore(Flow/Graph): render grey anchors [YTFRONT-5792]

--- packages/ui/src/ui/pages/flow/Flow/FlowGraph/renderers/ComputationAnchor.ts
+++ packages/ui/src/ui/pages/flow/Flow/FlowGraph/renderers/ComputationAnchor.ts
@@ -1,11 +1,7 @@
 import {Anchor, ECameraScaleLevel} from '@gravity-ui/graph';
 import {GRAPH_COLORS} from '../../../../../components/YTGraph/constants';
 import {type FlowComputationRuntimeType} from '../../types';
-import {
-    COMPUTATION_ANCHOR_SIZE,
-    getStreamsSummmaryByAnchorType,
-    hasVisibleStreamsSummaryDetails,
-} from '../utils/utils';
+import {getStreamsSummmaryByAnchorType, hasVisibleStreamsSummaryDetails} from '../utils/utils';
 
 export class ComputationAnchor extends Anchor {
     getStreamsSummmaryDetails() {
@@ -17,7 +13,8 @@ export class ComputationAnchor extends Anchor {
     }
 
     render() {
-        if (this.context.camera.getCameraBlockScaleLevel() === ECameraScaleLevel.Detailed) {
+        const cameraLevel = this.context.camera.getCameraBlockScaleLevel();
+        if (cameraLevel === ECameraScaleLevel.Detailed) {
             return;
         }
 
@@ -29,17 +26,20 @@ export class ComputationAnchor extends Anchor {
         const {drained, backpressureDetected} = summary;
         const {ctx} = this.context;
 
+        ctx.fillStyle = GRAPH_COLORS.genericMediumBackground;
+
         if (drained) {
             ctx.fillStyle = GRAPH_COLORS.infoLine;
         } else if (backpressureDetected) {
             ctx.fillStyle = GRAPH_COLORS.warningLine;
-        } else {
-            return;
         }
+
+        const isMinimalistic = cameraLevel === ECameraScaleLevel.Minimalistic;
+        const radius = isMinimalistic ? (this.state.size / 4) * 3 : this.state.size / 2;
 
         const {x, y} = this.getPosition();
         ctx.beginPath();
-        ctx.arc(x, y, COMPUTATION_ANCHOR_SIZE / 2, 0, 2 * Math.PI);
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.fill();
         ctx.closePath();
     }
