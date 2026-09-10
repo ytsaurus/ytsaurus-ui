697065f6933ff6a421ccb0996b9f3be25bc17001
xexys <xexys@yandex-team.ru>
2026-09-10 17:50:42 +0300

    refactor: split RestoreObjectModal component [YTFRONT-6022]

--- packages/ui/src/ui/pages/navigation/Navigation/PathEditorModal/RestoreObjectModal/RestoreObjectModal.js
+++ packages/ui/src/ui/pages/navigation/Navigation/PathEditorModal/RestoreObjectModal/RestoreObjectModal.js
@@ -1,79 +1,9 @@
-import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-
-import PathEditorModal from '../PathEditorModal';
-
-import i18n from './i18n';
-
-import {CLOSE_RESTORE_POPUP} from '../../../../../constants/navigation/modals/restore-object';
 import {closeEditingPopup} from '../../../../../store/actions/navigation/modals/path-editing-popup';
 import {restoreObject} from '../../../../../store/actions/navigation/modals/restore-object';
 import {updateView} from '../../../../../store/actions/navigation';
 
-class RestoreObjectModal extends Component {
-    static propTypes = {
-        // from connect
-        error: PropTypes.shape({
-            code: PropTypes.number,
-            message: PropTypes.string,
-        }).isRequired,
-        errorMessage: PropTypes.string.isRequired,
-        restoredPath: PropTypes.string.isRequired,
-        objectPath: PropTypes.string.isRequired,
-        restoring: PropTypes.bool.isRequired,
-        showError: PropTypes.bool.isRequired,
-        popupVisible: PropTypes.bool.isRequired,
-
-        updateView: PropTypes.func.isRequired,
-        restoreObject: PropTypes.func.isRequired,
-        closeEditingPopup: PropTypes.func.isRequired,
-    };
-
-    handleConfirmButtonClick = () => {
-        const {restoreObject, objectPath, restoredPath, updateView} = this.props;
-
-        restoreObject(objectPath, restoredPath, updateView);
-    };
-
-    handleCancelButtonClick = () => this.props.closeEditingPopup(CLOSE_RESTORE_POPUP);
-
-    handleApply = (newPath) => {
-        const {restoreObject, objectPath, showError, restoring, updateView} = this.props;
-        const disabled = restoring || showError;
-
-        if (!disabled) {
-            restoreObject(objectPath, newPath, updateView);
-        }
-    };
-
-    render() {
-        const {popupVisible, restoring, restoredPath, showError, errorMessage, error} = this.props;
-
-        const modalTitle = i18n('title_restore');
-        const title = i18n('title_object-already-exists');
-        const description = i18n('context_restore-description');
-        const placeholder = i18n('action_enter-restored-path');
-
-        return (
-            <PathEditorModal
-                title={title}
-                description={description}
-                placeholder={placeholder}
-                modalTitle={modalTitle}
-                visible={popupVisible}
-                inProcess={restoring}
-                editingPath={restoredPath}
-                showError={showError}
-                errorMessage={errorMessage}
-                error={error}
-                onConfirmButtonClick={this.handleConfirmButtonClick}
-                onCancelButtonClick={this.handleCancelButtonClick}
-                onApply={this.handleApply}
-            />
-        );
-    }
-}
+import {RestoreObjectModalBase} from './RestoreObjectModalBase';
 
 const mapStateToProps = ({navigation}) => {
     const {restoredPath, objectPath, popupVisible, showError, restoring, errorMessage, error} =
@@ -96,4 +26,6 @@ const mapDispatchToProps = {
     closeEditingPopup,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(RestoreObjectModal);
+const RestoreObjectModal = connect(mapStateToProps, mapDispatchToProps)(RestoreObjectModalBase);
+
+export default RestoreObjectModal;
