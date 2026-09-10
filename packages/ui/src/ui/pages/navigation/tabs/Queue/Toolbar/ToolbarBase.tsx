adac73e323079b9877548cef3917f01871f0610c
xexys <xexys@yandex-team.ru>
2026-09-10 19:09:10 +0300

    refactor: split QueueToolbar component [YTFRONT-6022]

--- packages/ui/src/ui/pages/navigation/tabs/Queue/Toolbar/ToolbarBase.tsx
+++ packages/ui/src/ui/pages/navigation/tabs/Queue/Toolbar/ToolbarBase.tsx
@@ -1,15 +1,10 @@
 import React, {type ComponentType} from 'react';
-import {type ConnectedProps, connect} from 'react-redux';
 import cn from 'bem-cn-lite';
 
 import RadioButton from '../../../../../components/RadioButton/RadioButton';
 import {QUEUE_MODE} from '../../../../../constants/navigation/tabs/queue';
-import {changeQueueMode} from '../../../../../store/actions/navigation/tabs/queue/filters';
-import {type RootState} from '../../../../../store/reducers';
-import {selectQueueMode} from '../../../../../store/selectors/navigation/tabs/queue';
 
 import i18n from './i18n';
-import './Toolbar.scss';
 
 const block = cn('queue-toolbar');
 
@@ -44,7 +39,7 @@ const tabItems: React.ComponentProps<typeof RadioButton>['items'] = [
     },
 ];
 
-const Toolbar: React.VFC<Props> = ({extras: Extras, queueMode, changeQueueMode}) => {
+export const ToolbarBase: React.VFC<Props> = ({extras: Extras, queueMode, changeQueueMode}) => {
     return (
         <div className={block()}>
             <RadioButton value={queueMode} onChange={changeQueueMode} items={tabItems} />
@@ -52,18 +47,7 @@ const Toolbar: React.VFC<Props> = ({extras: Extras, queueMode, changeQueueMode})
         </div>
     );
 };
-
-function mapStateToProps(state: RootState) {
-    return {
-        queueMode: selectQueueMode(state),
-    };
-}
-
-const mapDispatchToProps = {
-    changeQueueMode,
+type PropsFromRedux = {
+    queueMode: QUEUE_MODE;
+    changeQueueMode: (evt: React.ChangeEvent<HTMLInputElement>) => void;
 };
-
-const connector = connect(mapStateToProps, mapDispatchToProps);
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-export default connector(Toolbar);
