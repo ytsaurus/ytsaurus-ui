a3d2fc2aa25e50245d5bbd209bfdff22e29c31e3
xexys <xexys@yandex-team.ru>
2026-09-10 19:11:12 +0300

    refactor: split Statistics component [YTFRONT-6022]

--- packages/ui/src/ui/pages/operations/OperationDetail/tabs/statistics/Statistics.tsx
+++ packages/ui/src/ui/pages/operations/OperationDetail/tabs/statistics/Statistics.tsx
@@ -1,24 +1,6 @@
-import React, {Component} from 'react';
-import {type ConnectedProps, connect} from 'react-redux';
+import React from 'react';
+import {connect} from 'react-redux';
 import {useSelector} from '../../../../../store/redux-hooks';
-import cn from 'bem-cn-lite';
-
-import format from '../../../../../common/hammer/format';
-
-import ErrorBoundary from '../../../../../containers/ErrorBoundary/ErrorBoundary';
-import ElementsTable from '../../../../../components/ElementsTable/ElementsTable';
-import RadioButton from '../../../../../components/RadioButton/RadioButton';
-import HelpLink from '../../../../../components/HelpLink/HelpLink';
-import Button from '../../../../../components/Button/Button';
-import Filter from '../../../../../components/Filter/Filter';
-import Icon from '../../../../../components/Icon/Icon';
-import {UseResizeEventForTable} from '../../../../../components/UseResizeEventForTable/UseResizeEventForTable';
-
-import {
-    AGGREGATOR_RADIO_ITEMS,
-    DEBOUNCE_DELAY,
-    TREE_STATE,
-} from '../../../../../constants/operations/statistics';
 import {
     changeAggregation,
     changeFilterText,
@@ -26,8 +8,6 @@ import {
     changePoolTreeFilter,
     setTreeState,
 } from '../../../../../store/actions/operations/statistics';
-import {statisticsTableProps} from '../../../../../utils/operations/tabs/statistics/statisticsTableProps';
-import {makeRadioButtonProps} from '../../../../../utils';
 
 import {type RootState} from '../../../../../store/reducers';
 import {selectOperationDetailsLoadingStatus} from '../../../../../store/selectors/operations/operation';
@@ -40,226 +20,9 @@ import {RumMeasureTypes} from '../../../../../rum/rum-measure-types';
 import {useRumMeasureStop} from '../../../../../rum/RumUiContext';
 import {isFinalLoadingStatus} from '../../../../../utils/utils';
 import {useAppRumMeasureStart} from '../../../../../rum/rum-app-measures';
-import {docsUrl} from '../../../../../config';
-import UIFactory from '../../../../../UIFactory';
-import {WaitForFont} from '../../../../../containers/WaitForFont/WaitForFont';
-
-import {OperationStatisticName, OperationStatisticValue} from './OperationStatisticName';
-import i18n from './i18n';
 import './Statistics.scss';
 
-const statisticsBlock = cn('operation-statistics');
-const toolbarBlock = cn('elements-toolbar');
-
-type ItemType = ReturnType<typeof selectOperationStatisticsFiltered>[0];
-
-interface ItemState {
-    collapsed?: boolean;
-    empty?: boolean;
-}
-
-type Props = {className?: string} & ConnectedProps<typeof connector>;
-
-export class Statistics extends Component<Props> {
-    override componentWillUnmount() {
-        this.expandTable();
-    }
-
-    // eslint-disable-next-line react/sort-comp
-    get template() {
-        const {setTreeState, activeAggregation = 'avg'} = this.props;
-
-        return {
-            metric(
-                item: ItemType,
-                _columnName: 'metric',
-                toggleItemState: (...args: Array<unknown>) => void,
-                itemState: ItemState,
-            ) {
-                const OFFSET = 40;
-                const offsetStyle = {paddingLeft: item.level * OFFSET};
-
-                if (item.isLeafNode) {
-                    return (
-                        <span className={statisticsBlock('metric')} style={offsetStyle}>
-                            <Icon className={statisticsBlock('metric-icon')} awesome="chart-line" />
-                            <OperationStatisticName name={item.name} title={item.title} />
-                        </span>
-                    );
-                } else {
-                    const togglerIconName =
-                        itemState.collapsed || itemState.empty ? 'angle-down' : 'angle-up';
-                    const itemIconName =
-                        itemState.collapsed || itemState.empty ? 'folder' : 'folder-open';
-
-                    const toggleItemAndTreeState = (...rest: Array<unknown>) => {
-                        if (!itemState.empty) {
-                            toggleItemState(...rest);
-                            setTreeState('mixed');
-                        }
-                    };
-
-                    return (
-                        <span
-                            className={statisticsBlock('group')}
-                            style={offsetStyle}
-                            onClick={toggleItemAndTreeState}
-                        >
-                            <Icon
-                                className={statisticsBlock('group-icon-toggler')}
-                                awesome={togglerIconName}
-                            />
-                            <Icon
-                                className={statisticsBlock('group-icon')}
-                                awesome={itemIconName}
-                            />
-                            <span>{item.title}</span>
-                        </span>
-                    );
-                }
-            },
-            __default__(item: ItemType, columnName: keyof Required<typeof item>['data']) {
-                if (item.isLeafNode) {
-                    const metric = item.data?.[columnName];
-
-                    let value;
-                    let settings;
-
-                    if (activeAggregation === 'avg') {
-                        value = metric && metric.count && metric.sum / metric.count;
-
-                        if (value! < 1) {
-                            settings = {
-                                significantDigits: 6,
-                            };
-                        }
-                    } else {
-                        value = metric && metric[activeAggregation];
-                    }
-
-                    if (activeAggregation === 'count') {
-                        return format['Number'](value, settings);
-                    }
-
-                    return (
-                        <OperationStatisticValue
-                            value={value!}
-                            settings={settings}
-                            name={item.name}
-                        />
-                    );
-                }
-                return null;
-            },
-        };
-    }
-
-    collapseTable = () => this.props.setTreeState(TREE_STATE.COLLAPSED);
-    expandTable = () => this.props.setTreeState(TREE_STATE.EXPANDED);
-
-    renderToolbar() {
-        const {filterText, activeAggregation, jobTypes, poolTrees, activeJobType, activePoolTree} =
-            this.props;
-
-        return (
-            <div className={toolbarBlock(null, statisticsBlock('toolbar'))}>
-                <div className={toolbarBlock('container')}>
-                    <div className={toolbarBlock('component', statisticsBlock('filter'))}>
-                        <Filter
-                            size="m"
-                            value={filterText}
-                            debounce={DEBOUNCE_DELAY}
-                            onChange={this.props.changeFilterText}
-                        />
-                    </div>
-
-                    {poolTrees.length > 1 && (
-                        <div className={toolbarBlock('component')}>
-                            <RadioButton
-                                size="m"
-                                value={activePoolTree ?? ''}
-                                onUpdate={this.props.changePoolTreeFilter}
-                                items={makeRadioButtonProps(poolTrees, '')}
-                                name="operation-statistics-pool-tree"
-                            />
-                        </div>
-                    )}
-
-                    {jobTypes.length > 1 && (
-                        <div className={toolbarBlock('component')}>
-                            <RadioButton
-                                size="m"
-                                value={activeJobType ?? ''}
-                                onUpdate={this.props.changeJobType}
-                                items={makeRadioButtonProps(jobTypes, '')}
-                                name="operation-statistics-job-type"
-                            />
-                        </div>
-                    )}
-
-                    <div className={toolbarBlock('component')}>
-                        <RadioButton
-                            size="m"
-                            value={activeAggregation}
-                            onChange={this.props.changeAggregation}
-                            items={AGGREGATOR_RADIO_ITEMS}
-                            name="operation-statistics-aggregator"
-                        />
-                    </div>
-
-                    <div className={toolbarBlock('component', statisticsBlock('expand-collapse'))}>
-                        <span className={statisticsBlock('expand-metrics')}>
-                            <Button
-                                size="m"
-                                title={i18n('action_expand-all')}
-                                onClick={this.expandTable}
-                            >
-                                <Icon awesome="arrow-to-bottom" />
-                            </Button>
-                        </span>
-
-                        <span className={statisticsBlock('collapse-metrics')}>
-                            <Button
-                                size="m"
-                                title={i18n('action_collapse-all')}
-                                onClick={this.collapseTable}
-                            >
-                                <Icon awesome="arrow-to-top" />
-                            </Button>
-                        </span>
-                    </div>
-                    {docsUrl(
-                        <div className={toolbarBlock('component', statisticsBlock('help'))}>
-                            <HelpLink url={UIFactory.docsUrls['problems:jobstatistics']} />
-                        </div>,
-                    )}
-                </div>
-            </div>
-        );
-    }
-
-    override render() {
-        const {className, treeState, items} = this.props;
-
-        return (
-            <ErrorBoundary>
-                <div className={statisticsBlock(null, className)}>
-                    {this.renderToolbar()}
-                    <WaitForFont>
-                        <UseResizeEventForTable length={items.length} />
-                        <ElementsTable
-                            {...statisticsTableProps}
-                            templates={this.template}
-                            css={statisticsBlock()}
-                            treeState={treeState}
-                            items={items}
-                        />
-                    </WaitForFont>
-                </div>
-            </ErrorBoundary>
-        );
-    }
-}
+import {StatisticsBase} from './StatisticsBase';
 
 const mapStateToProps = (state: RootState) => {
     const {treeState, activeAggregation} = state.operations.statistics;
@@ -285,9 +48,7 @@ const mapDispatchToProps = {
     changePoolTreeFilter,
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
-
-const StatisticsConnected = connector(Statistics);
+const StatisticsConnected = connect(mapStateToProps, mapDispatchToProps)(StatisticsBase);
 
 export default function SpecificationWithRum(props: {className: string}) {
     const loadState = useSelector(selectOperationDetailsLoadingStatus);
