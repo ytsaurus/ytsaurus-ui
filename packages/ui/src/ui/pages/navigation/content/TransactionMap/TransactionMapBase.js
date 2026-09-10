b21328954283488fad7fa7d213d4d0428dd4d637
xexys <xexys@yandex-team.ru>
2026-09-10 17:51:18 +0300

    refactor: split TransactionMap component [YTFRONT-6022]

---
+++ packages/ui/src/ui/pages/navigation/content/TransactionMap/TransactionMapBase.js
@@ -0,0 +1,160 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import hammer from '../../../../common/hammer';
+import cn from 'bem-cn-lite';
+
+import ClickableAttributesButton from '../../../../components/AttributesButton/ClickableAttributesButton';
+import LoadDataHandler from '../../../../containers/LoadDataHandler/LoadDataHandler';
+import {FormattedText, FormattedTextOrLink} from '../../../../components/formatters';
+import ElementsTable from '../../../../components/ElementsTable/ElementsTable';
+import Filter from '../../../../components/Filter/Filter';
+import Icon from '../../../../components/Icon/Icon';
+import {StickyContainer} from '../../../../components/StickyContainer/StickyContainer';
+import {Toolbar} from '../../../../components/WithStickyToolbar/Toolbar/Toolbar';
+
+import {NAVIGATION_TRANSACTION_MAP_TABLE_ID} from '../../../../constants/navigation/content/transaction-map';
+import {tableItems} from '../../../../utils/navigation/content/transaction-map/table';
+import {getIconNameForType} from '../../../../utils/navigation/path-editor';
+import {itemNavigationAllowed} from '../../../../pages/navigation/Navigation/ContentViewer/helpers';
+
+import i18n from './i18n';
+
+const block = cn('navigation-transaction-map');
+
+export class TransactionMapBase extends Component {
+    static propTypes = {
+        // from connect
+        loading: PropTypes.bool.isRequired,
+        loaded: PropTypes.bool.isRequired,
+        error: PropTypes.bool.isRequired,
+        errorData: PropTypes.object.isRequired,
+
+        path: PropTypes.string.isRequired,
+        filter: PropTypes.string.isRequired,
+        transactions: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+        transaction: PropTypes.string,
+
+        changeFilter: PropTypes.func.isRequired,
+        loadTransactions: PropTypes.func.isRequired,
+    };
+
+    static renderIcon(item) {
+        const iconName = getIconNameForType(item.type);
+
+        return <Icon awesome={iconName} />;
+    }
+
+    static renderId(item) {
+        const isLink = itemNavigationAllowed(item);
+
+        return <FormattedTextOrLink state={item.pathState} asLink={isLink} text={item.id} />;
+    }
+
+    static renderOwner(item) {
+        return <FormattedText text={item.owner} />;
+    }
+
+    static renderStartTime(item) {
+        return hammer.format['DateTime'](item.started, {format: 'full'});
+    }
+
+    static renderTitle(item) {
+        return <FormattedText text={item.title} />;
+    }
+
+    static renderActions(item) {
+        return (
+            <ClickableAttributesButton
+                title={item.id}
+                path={`//sys/transactions/${item.id}`}
+                withTooltip
+            />
+        );
+    }
+
+    componentDidMount() {
+        this.props.loadTransactions();
+    }
+
+    componentDidUpdate(prevProps) {
+        const {path, transaction, loadTransactions} = this.props;
+
+        if (prevProps.path !== path || prevProps.transaction !== transaction) {
+            loadTransactions();
+        }
+    }
+
+    get tableSettings() {
+        return {
+            css: block(),
+            theme: 'light',
+            cssHover: true,
+            striped: false,
+            tableId: NAVIGATION_TRANSACTION_MAP_TABLE_ID,
+            columns: {
+                items: tableItems,
+                sets: {
+                    default: {
+                        items: ['icon', 'id', 'title', 'owner', 'start_time', 'actions'],
+                    },
+                },
+                mode: 'default',
+            },
+            templates: {
+                icon: TransactionMapBase.renderIcon,
+                id: TransactionMapBase.renderId,
+                owner: TransactionMapBase.renderOwner,
+                start_time: TransactionMapBase.renderStartTime,
+                title: TransactionMapBase.renderTitle,
+                actions: TransactionMapBase.renderActions,
+            },
+            computeKey(item) {
+                return item.id;
+            },
+        };
+    }
+
+    handleFilterChange = (value) => this.props.changeFilter(value);
+
+    render() {
+        const {filter, loading, transactions} = this.props;
+
+        return (
+            <div className={block()}>
+                <StickyContainer>
+                    {({stickyTopClassName}) => (
+                        <React.Fragment>
+                            <Toolbar
+                                className={stickyTopClassName}
+                                itemsToWrap={[
+                                    {
+                                        node: (
+                                            <Filter
+                                                hasClear
+                                                size="m"
+                                                value={filter}
+                                                debounce={300}
+                                                className={block('filter')}
+                                                onChange={this.handleFilterChange}
+                                                placeholder={i18n(
+                                                    'placeholder_filter-transactions',
+                                                )}
+                                            />
+                                        ),
+                                    },
+                                ]}
+                            />
+                            <LoadDataHandler {...this.props}>
+                                <ElementsTable
+                                    {...this.tableSettings}
+                                    items={transactions}
+                                    isLoading={loading}
+                                />
+                            </LoadDataHandler>
+                        </React.Fragment>
+                    )}
+                </StickyContainer>
+            </div>
+        );
+    }
+}
