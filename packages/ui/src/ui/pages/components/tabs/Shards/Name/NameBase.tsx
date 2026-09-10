8f63708530bf029db586a0b7b06a2dfc0f7fbd61
xexys <xexys@yandex-team.ru>
2026-09-10 19:05:16 +0300

    refactor: split ShardName component [YTFRONT-6022]

--- packages/ui/src/ui/pages/components/tabs/Shards/Name/NameBase.tsx
+++ packages/ui/src/ui/pages/components/tabs/Shards/Name/NameBase.tsx
@@ -1,22 +1,15 @@
 import React, {Fragment, useState} from 'react';
-import {type ResolveThunks, connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import {TextInput} from '@gravity-ui/uikit';
-import Button from '../../../../components/Button/Button';
-import Modal from '../../../../components/Modal/Modal';
-import {YTErrorBlock} from '../../../../containers/Block/Block';
-import Icon from '../../../../components/Icon/Icon';
-import i18n from './i18n';
+import Button from '../../../../../components/Button/Button';
+import Modal from '../../../../../components/Modal/Modal';
+import {YTErrorBlock} from '../../../../../containers/Block/Block';
+import {type FIX_MY_TYPE} from '../../../../../types';
+import Icon from '../../../../../components/Icon/Icon';
+import i18n from '../i18n';
 
-import {
-    closeNameEditor,
-    openNameEditor,
-    setShardName,
-} from '../../../../store/actions/components/shards';
-import {type RootState} from '../../../../store/reducers';
-
-Name.propTypes = {
+NameBase.propTypes = {
     // from parent
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
@@ -42,13 +35,24 @@ type OwnProps = {
     className: string;
 };
 
-type StateProps = ReturnType<typeof mapStateToProps>;
+type StateProps = {
+    loading: boolean;
+    loaded: boolean;
+    error: boolean;
+    errorData: FIX_MY_TYPE;
+    visible: boolean;
+    editId: string;
+};
 
-type DispatchProps = ResolveThunks<typeof mapDispatchToProps>;
+type DispatchProps = {
+    setShardName: (id: string, name: string) => void;
+    openNameEditor: (id: string) => void;
+    closeNameEditor: () => void;
+};
 
 type NameProps = OwnProps & StateProps & DispatchProps;
 
-function Name({
+export function NameBase({
     name: initialName,
     id,
     editId,
@@ -102,25 +106,3 @@ function Name({
         </div>
     );
 }
-
-const mapStateToProps = (state: RootState) => {
-    const {nameId, nameVisible, nameLoading, nameLoaded, nameError, nameErrorData} =
-        state.components.shards;
-
-    return {
-        loading: nameLoading,
-        loaded: nameLoaded,
-        error: nameError,
-        errorData: nameErrorData,
-        visible: nameVisible,
-        editId: nameId,
-    };
-};
-
-const mapDispatchToProps = {
-    setShardName,
-    openNameEditor,
-    closeNameEditor,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Name);
