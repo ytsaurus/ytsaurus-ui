65cab144397d0c292243a2883a4566b64e44d290
xexys <xexys@yandex-team.ru>
2026-09-10 19:05:52 +0300

    refactor: split CreateDirectoryModal component [YTFRONT-6022]

--- packages/ui/src/ui/pages/navigation/Navigation/PathEditorModal/CreateDirectoryModal/CreateDirectoryModal.tsx
+++ packages/ui/src/ui/pages/navigation/Navigation/PathEditorModal/CreateDirectoryModal/CreateDirectoryModal.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
-import {type ConnectedProps, connect} from 'react-redux';
-
-import PathEditorModal from '../PathEditorModal';
-
-import i18n from './i18n';
-
-import {CLOSE_CREATE_DIRECTORY_POPUP} from '../../../../../constants/navigation/modals/create-directory';
+import {connect} from 'react-redux';
 import {
     abortRequests,
     clearCreateDirectoryError,
@@ -14,76 +7,8 @@ import {
 import {closeEditingPopup} from '../../../../../store/actions/navigation/modals/path-editing-popup';
 import {updateView} from '../../../../../store/actions/navigation';
 import {type RootState} from '../../../../../store/reducers';
-import {Checkbox} from '@gravity-ui/uikit';
 
-type State = {
-    recursive: boolean;
-};
-
-type ReduxProps = ConnectedProps<typeof connector>;
-
-class CreateDirectoryModal extends React.Component<ReduxProps> {
-    override state: State = {
-        recursive: false,
-    };
-
-    override render() {
-        const {popupVisible, creating, creatingPath, showError, errorMessage, error} = this.props;
-
-        const modalTitle = i18n('title_create-directory');
-        const title = i18n('title_enter-name');
-        const description = i18n('context_new-object-path');
-        const placeholder = i18n('context_enter-directory-path');
-
-        return (
-            <PathEditorModal
-                title={title}
-                description={description}
-                placeholder={placeholder}
-                modalTitle={modalTitle}
-                editingPath={creatingPath}
-                errorMessage={errorMessage}
-                error={error}
-                visible={popupVisible}
-                showError={showError}
-                inProcess={creating}
-                onConfirmButtonClick={this.handleConfirmButtonClick}
-                onCancelButtonClick={this.handleCancelButtonClick}
-                onApply={this.handleApply}
-                options={
-                    <Checkbox checked={this.state.recursive} onUpdate={this.onRecursiveUpdate}>
-                        {i18n('action_make-parent-dirs')}
-                    </Checkbox>
-                }
-            />
-        );
-    }
-
-    onRecursiveUpdate = (recursive: boolean) => {
-        this.setState({recursive});
-        this.props.clearCreateDirectoryError?.();
-    };
-
-    handleConfirmButtonClick = () => {
-        const {creatingPath, createDirectory, updateView} = this.props;
-
-        createDirectory({path: creatingPath, recursive: this.state.recursive}, updateView);
-    };
-
-    handleCancelButtonClick = () => {
-        this.props.closeEditingPopup(CLOSE_CREATE_DIRECTORY_POPUP);
-        this.props.abortRequests();
-    };
-
-    handleApply = (newPath: string) => {
-        const {creating, showError, createDirectory, updateView} = this.props;
-        const disabled = creating || showError;
-
-        if (!disabled) {
-            createDirectory({path: newPath, recursive: this.state.recursive}, updateView);
-        }
-    };
-}
+import {CreateDirectoryModalBase} from './CreateDirectoryModalBase';
 
 const mapStateToProps = (state: RootState) => {
     const {creatingPath, popupVisible, showError, creating, errorMessage, error} =
@@ -107,6 +32,6 @@ const mapDispatchToProps = {
     clearCreateDirectoryError,
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
+const CreateDirectoryModal = connect(mapStateToProps, mapDispatchToProps)(CreateDirectoryModalBase);
 
-export default connector(CreateDirectoryModal);
+export default CreateDirectoryModal;
