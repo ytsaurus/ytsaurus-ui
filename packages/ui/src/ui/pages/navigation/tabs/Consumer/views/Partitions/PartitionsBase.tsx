a22c2f5c1bc561595f6af8a1178beee89f5a0719
xexys <xexys@yandex-team.ru>
2026-09-10 19:09:10 +0300

    refactor: split ConsumerPartitions component [YTFRONT-6022]

--- packages/ui/src/ui/pages/navigation/tabs/Consumer/views/Partitions/PartitionsBase.tsx
+++ packages/ui/src/ui/pages/navigation/tabs/Consumer/views/Partitions/PartitionsBase.tsx
@@ -1,85 +1,21 @@
 import React, {useEffect} from 'react';
-import {type ConnectedProps, connect} from 'react-redux';
 import {useSelector} from '../../../../../../store/redux-hooks';
 import cn from 'bem-cn-lite';
-import {createSelector} from 'reselect';
 import {type Column, type Settings} from '@gravity-ui/react-data-table';
-
-import format from '../../../../../../common/hammer/format';
 import {DataTableYT} from '../../../../../../components/DataTableYT';
 import {YTErrorBlock} from '../../../../../../containers/Block/Block';
+import {type SelectedPartition} from '../../../../../../store/selectors/navigation/tabs/consumer';
+import {type YTError} from '../../../../../../types';
 import {NoContent} from '../../../../../../components/NoContent';
-import {CONSUMER_RATE_MODE} from '../../../../../../constants/navigation/tabs/consumer';
-import {
-    datetime,
-    error,
-    multimeter,
-    number,
-} from '../../../../../../pages/navigation/tabs/Queue/utils/column-builder';
-import {loadConsumerPartitions} from '../../../../../../store/actions/navigation/tabs/consumer/partitions';
-import {type RootState} from '../../../../../../store/reducers';
-import {type TPerformanceCounters} from '../../../../../../store/reducers/navigation/tabs/queue/types';
-import {
-    type SelectedPartition,
-    selectConsumerPartitionsColumns,
-    selectConsumerRateMode,
-    selectConsumerTimeWindow,
-    selectPartitions,
-    selectPartitionsError,
-    selectPartitionsLoaded,
-    selectPartitionsLoading,
-    selectTargetQueue,
-} from '../../../../../../store/selectors/navigation/tabs/consumer';
+import {selectTargetQueue} from '../../../../../../store/selectors/navigation/tabs/consumer';
 
 import i18n from './i18n';
 
-import './Partitions.scss';
-
-const block = cn('consumer-partitions');
-
-const readRateName: Record<CONSUMER_RATE_MODE, string> = {
-    get [CONSUMER_RATE_MODE.ROWS]() {
-        return i18n('field_read-rate');
-    },
-    get [CONSUMER_RATE_MODE.DATA_WEIGHT]() {
-        return i18n('field_read-rate');
-    },
-};
-const readRateGetter: Record<CONSUMER_RATE_MODE, (row: SelectedPartition) => TPerformanceCounters> =
-    {
-        [CONSUMER_RATE_MODE.ROWS]: (x) => x.read_row_count_rate,
-        [CONSUMER_RATE_MODE.DATA_WEIGHT]: (x) => x.read_data_weight_rate,
-    };
-
-const getColumns = createSelector(
-    [selectConsumerRateMode, selectConsumerTimeWindow, selectConsumerPartitionsColumns],
-    (rateMode, timeWindow, columns): Array<Column<SelectedPartition>> => {
-        return columns
-            .filter((column) => column.checked)
-            .map(({name, caption}) => {
-                if (name === 'error') {
-                    return error<SelectedPartition>(caption, (x) => x[name], block('error'));
-                } else if (name === 'read_rate') {
-                    return multimeter<SelectedPartition>(
-                        readRateName[rateMode],
-                        readRateGetter[rateMode],
-                        timeWindow,
-                        rateMode === CONSUMER_RATE_MODE.ROWS
-                            ? format.RowsPerSecond
-                            : format.BytesPerSecond,
-                    );
-                } else if (name === 'next_row_commit_time') {
-                    return datetime<SelectedPartition>(caption, (x) => x[name]);
-                } else {
-                    return number<SelectedPartition>(caption, (x) => x[name]);
-                }
-            });
-    },
-);
+export const block = cn('consumer-partitions');
 
 const settings: Settings = {displayIndices: false};
 
-const Partitions: React.VFC<PropsFromRedux> = ({
+export const PartitionsBase: React.VFC<PropsFromRedux> = ({
     loadConsumerPartitions,
     columns,
     partitions,
@@ -120,22 +56,11 @@ const Partitions: React.VFC<PropsFromRedux> = ({
         />
     );
 };
-
-function mapStateToProps(state: RootState) {
-    return {
-        columns: getColumns(state),
-        partitions: selectPartitions(state),
-        partitionsError: selectPartitionsError(state),
-        partitionsLoading: selectPartitionsLoading(state),
-        partitionsLoaded: selectPartitionsLoaded(state),
-    };
-}
-
-const mapDispatchToProps = {
-    loadConsumerPartitions,
+type PropsFromRedux = {
+    columns: Column<SelectedPartition>[];
+    partitions: SelectedPartition[];
+    partitionsError: YTError | null;
+    partitionsLoading: boolean;
+    partitionsLoaded: boolean;
+    loadConsumerPartitions: (queue: string) => void;
 };
-
-const connector = connect(mapStateToProps, mapDispatchToProps);
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-export default connector(Partitions);
