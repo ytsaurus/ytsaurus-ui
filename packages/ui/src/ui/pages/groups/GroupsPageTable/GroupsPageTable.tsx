16a37cb7dc04847db6bc6b93e1fccc28f1322d58
xexys <xexys@yandex-team.ru>
2026-09-10 19:05:52 +0300

    refactor: split GroupsPageTable component [YTFRONT-6022]

--- packages/ui/src/ui/pages/groups/GroupsPageTable/GroupsPageTable.tsx
+++ packages/ui/src/ui/pages/groups/GroupsPageTable/GroupsPageTable.tsx
@@ -1,25 +1,9 @@
-import React from 'react';
-import cn from 'bem-cn-lite';
-import {type ConnectedProps, connect} from 'react-redux';
-import DataTable from '@gravity-ui/react-data-table';
+import {connect} from 'react-redux';
 
 import {openAttributesModal} from '../../../store/actions/modals/attributes-modal';
-import ColumnHeader from '../../../components/ColumnHeader/ColumnHeader';
-import CommaSeparatedListWithRestCounter from '../../../components/CommaSeparateListWithRestCounter/CommaSeparateListWithRestCounter';
-import {SubjectCard} from '../../../components/SubjectLink/SubjectLink';
-
-import ErrorBoundary from '../../../containers/ErrorBoundary/ErrorBoundary';
-import ExpandIcon from '../../../components/ExpandIcon/ExpandIcon';
-import {Tooltip} from '@ytsaurus/components';
-import {DataTableYT} from '../../../components/DataTableYT';
-
-import LoadDataHandler from '../../../containers/LoadDataHandler/LoadDataHandler';
 
 import {fetchGroups, setGroupsPageSorting, toggleGroupExpand} from '../../../store/actions/groups';
-import {STICKY_TOOLBAR_BOTTOM} from '../../../components/WithStickyToolbar/WithStickyToolbar';
-import GroupEditorDialog from '../../../pages/groups/GroupEditorDialog/GroupEditorDialog';
 import {
-    type GroupsTreeNode,
     selectGroupEditorVisible,
     selectGroupsFlattenTree,
     selectGroupsSort,
@@ -28,212 +12,8 @@ import {
 
 import './GroupsPageTable.scss';
 import {type RootState} from '../../../store/reducers';
-import {type OrderType} from '../../../utils/sort-helpers';
-import {GroupActions} from '../GroupActions/GroupActions';
-import i18n from './i18n';
 
-const block = cn('groups-page-table');
-
-const TABLE_SETTINGS = {
-    displayIndices: false,
-    stickyHead: DataTable.MOVING,
-    stickyTop: STICKY_TOOLBAR_BOTTOM,
-    syncHeadOnResize: true,
-    dynamicRender: true,
-} as const;
-
-const COLUMN_NAMES: Record<string, string> = {
-    get name() {
-        return i18n('field_name');
-    },
-    get externalSystem() {
-        return i18n('field_external-system');
-    },
-    get members() {
-        return i18n('field_members');
-    },
-    get size() {
-        return i18n('field_size');
-    },
-    get responsibles() {
-        return i18n('field_responsible-users');
-    },
-    actions: '',
-};
-
-interface GroupsPageTableProps extends ConnectedProps<typeof connector> {
-    className?: string;
-}
-
-class GroupsPageTable extends React.Component<GroupsPageTableProps> {
-    override componentDidMount() {
-        const {fetchGroups} = this.props;
-        fetchGroups();
-    }
-
-    renderColumnHeader(col: string, sortable: boolean) {
-        const {
-            sort: {column, order},
-        } = this.props;
-        const isSorted = col === column;
-
-        return (
-            <ColumnHeader
-                className={block('header-cell', {col, sortable}, 'data-table__head-cell')}
-                column={col}
-                title={COLUMN_NAMES[col]}
-                order={isSorted ? order : ''}
-                onSort={this.onColumnSort}
-            />
-        );
-    }
-
-    onColumnSort = (colName: string, nextOrder: OrderType) => {
-        const {setGroupsPageSorting} = this.props;
-        setGroupsPageSorting(colName, nextOrder);
-    };
-
-    renderNameCell(col: string, {row}: {row: GroupsTreeNode}) {
-        const {name, shift, hasChildren, expanded} = row;
-        return (
-            <div
-                className={block('content', {
-                    col,
-                    shift: String(shift),
-                    level: String(Math.min(10, shift!)),
-                })}
-            >
-                <ExpandIcon
-                    data={name}
-                    visible={hasChildren}
-                    onClick={this.toggleExpand}
-                    expanded={expanded}
-                />
-                <Tooltip content={name}>
-                    <span className={block('group-name')}>{name}</span>
-                </Tooltip>
-            </div>
-        );
-    }
-
-    toggleExpand = (groupName?: string, isExpanded?: boolean) => {
-        const {toggleGroupExpand} = this.props;
-        toggleGroupExpand(groupName!, !isExpanded);
-    };
-
-    renderExternalSystemCell(col: string, {row}: {row: GroupsTreeNode}) {
-        const {externalSystem} = row;
-
-        return (
-            <div className={block('content', {col})}>
-                <div className={block(col)}>{externalSystem}</div>
-            </div>
-        );
-    }
-
-    renderUsersCell(col: 'members', {row}: {row: GroupsTreeNode}) {
-        const {[col]: value} = row;
-        return (
-            <div className={block('content', {col})}>
-                <CommaSeparatedListWithRestCounter
-                    className={block('groups')}
-                    items={value}
-                    itemRenderer={(item: string) => {
-                        if (item.startsWith('idm-group:')) {
-                            return item;
-                        } else {
-                            return <SubjectCard name={item} />;
-                        }
-                    }}
-                />
-            </div>
-        );
-    }
-
-    renderSizeCell(col: string, {row}: {row: GroupsTreeNode}) {
-        const {members} = row;
-        return (
-            <div className={block('content', {col})}>
-                <span>{members!.length}</span>
-            </div>
-        );
-    }
-
-    renderActionsCell(col: string, {row}: {row: GroupsTreeNode}) {
-        const {name: groupname} = row;
-
-        return <GroupActions className={block('content', {col})} groupname={groupname} />;
-    }
-
-    renderTable() {
-        const {loaded, loading, groups} = this.props;
-
-        const columnProps = (name: string, disableSorting = false) => {
-            return {
-                name,
-                align: DataTable.LEFT,
-                sortable: false,
-                className: block('td', {col: name.toLowerCase()}),
-                header: this.renderColumnHeader(name, !disableSorting),
-            } as const;
-        };
-
-        const DISABLED_SORTING = true;
-
-        const columns = [
-            {
-                ...columnProps('name'),
-                render: this.renderNameCell.bind(this, 'name'),
-            },
-            {
-                ...columnProps('externalSystem', DISABLED_SORTING),
-                render: this.renderExternalSystemCell.bind(this, 'externalSystem'),
-            },
-            {
-                ...columnProps('members', DISABLED_SORTING),
-                render: this.renderUsersCell.bind(this, 'members'),
-            },
-            {
-                ...columnProps('size'),
-                render: this.renderSizeCell.bind(this, 'size'),
-            },
-            {
-                ...columnProps('actions'),
-                header: '',
-                render: this.renderActionsCell.bind(this, 'actions'),
-            },
-        ];
-
-        return (
-            <DataTableYT
-                loaded={loaded}
-                loading={loading}
-                data={groups}
-                columns={columns}
-                settings={TABLE_SETTINGS}
-                theme={'yt-internal'}
-            />
-        );
-    }
-
-    override render() {
-        const {className, error, showEditor} = this.props;
-        return (
-            <ErrorBoundary>
-                <div className={block(null, className)}>
-                    <LoadDataHandler
-                        loaded={true}
-                        error={Boolean(error)}
-                        errorData={error || undefined}
-                    >
-                        {this.renderTable()}
-                    </LoadDataHandler>
-                </div>
-                {showEditor && <GroupEditorDialog />}
-            </ErrorBoundary>
-        );
-    }
-}
+import {GroupsPageTableBase} from './GroupsPageTableBase';
 
 const mapStateToProps = (state: RootState) => {
     const {loaded, loading, error} = selectGroupsTableDataState(state);
@@ -259,6 +39,6 @@ const mapDispatchToProps = {
     openAttributesModal,
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
+const GroupsPageTable = connect(mapStateToProps, mapDispatchToProps)(GroupsPageTableBase);
 
-export default connector(GroupsPageTable);
+export default GroupsPageTable;
