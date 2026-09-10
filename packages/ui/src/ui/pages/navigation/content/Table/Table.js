d15c3bda32a6ca511b7df93f54d4c8376e391f1a
xexys <xexys@yandex-team.ru>
2026-09-10 17:50:43 +0300

    refactor: split Table component [YTFRONT-6022]

--- packages/ui/src/ui/pages/navigation/content/Table/Table.js
+++ packages/ui/src/ui/pages/navigation/content/Table/Table.js
@@ -1,24 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {useSelector} from '../../../../store/redux-hooks';
-import PropTypes from 'prop-types';
 import {compose} from 'redux';
-import cn from 'bem-cn-lite';
 
 import {useDisableMaxContentWidth} from '../../../../containers/MaxContentWidth';
-import DataTableWrapper from '../../../../pages/navigation/content/Table/DataTableWrapper/DataTableWrapper';
-import TableOverview from '../../../../pages/navigation/content/Table/TableOverview/TableOverview';
-import ColumnSelectorModal from '../../../../components/ColumnSelectorModal/ColumnSelectorModal';
-import OffsetSelectorModal from '../../../../pages/navigation/content/Table/OffsetSelectorModal/OffsetSelectorModal';
-import TableMeta from '../../../../pages/navigation/content/Table/TableMeta/TableMeta';
-import LoadDataHandler from '../../../../containers/LoadDataHandler/LoadDataHandler';
-import FullScreen from '../../../../components/FullScreen/FullScreen';
-import {YsonSettingsPropTypes} from '../../../../components/Yson/Yson';
-import WithStickyToolbar from '../../../../components/WithStickyToolbar/WithStickyToolbar';
-
-import {OVERVIEW_HEIGHT} from '../../../../constants/navigation/content/table';
 import {selectPath} from '../../../../store/selectors/navigation';
-import {HEADER_HEIGHT} from '../../../../constants/index';
 import {
     abortAndReset,
     closeColumnSelectorModal,
@@ -28,7 +14,6 @@ import {
 } from '../../../../store/actions/navigation/content/table/table';
 import {
     selectAllColumns,
-    selectIsYqlTypesEnabled,
     selectOffsetValue,
     selectSrcColumns,
     selectVisibleColumns,
@@ -48,165 +33,9 @@ import {useAppRumMeasureStart} from '../../../../rum/rum-app-measures';
 import {isFinalLoadingStatus} from '../../../../utils/utils';
 
 import './Table.scss';
-import TableColumnsPresetNotice from './TableOverview/TableColumnsPresetNotice';
 import {makeTableRumId} from '../../../../store/actions/navigation/content/table/table-rum-id';
-import i18n from './i18n';
 
-const block = cn('navigation-table');
-
-Table.columnsProps = PropTypes.arrayOf(
-    PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        data: PropTypes.shape({
-            caption: PropTypes.string.isRequired,
-            title: PropTypes.string.isRequired,
-        }).isRequired,
-        keyColumn: PropTypes.bool.isRequired,
-        checked: PropTypes.bool.isRequired,
-        disabled: PropTypes.bool.isRequired,
-
-        accessor: PropTypes.func,
-        label: PropTypes.string,
-    }),
-);
-
-Table.propTypes = {
-    // from connect
-    loading: PropTypes.bool.isRequired,
-    loaded: PropTypes.bool.isRequired,
-    error: PropTypes.bool.isRequired,
-    errorData: PropTypes.object.isRequired,
-
-    path: PropTypes.string.isRequired,
-    isSplit: PropTypes.bool.isRequired,
-    isDynamic: PropTypes.bool.isRequired,
-    isFullScreen: PropTypes.bool.isRequired,
-    settings: YsonSettingsPropTypes.isRequired,
-    isColumnSelectorOpen: PropTypes.bool.isRequired,
-    visibleRows: PropTypes.arrayOf(PropTypes.object).isRequired,
-
-    columns: Table.columnsProps.isRequired,
-    allColumns: Table.columnsProps.isRequired,
-    srcColumns: Table.columnsProps.isRequired,
-    keyColumns: PropTypes.arrayOf(PropTypes.string).isRequired,
-
-    offsetValue: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    yqlTypes: PropTypes.array,
-
-    updateColumns: PropTypes.func.isRequired,
-    getTableData: PropTypes.func.isRequired,
-    abortAndReset: PropTypes.func.isRequired,
-    handleScreenChanged: PropTypes.func.isRequired,
-    closeColumnSelectorModal: PropTypes.func.isRequired,
-};
-
-const renderColumnSelectorModal = (props) => {
-    const {updateColumns, allColumns, srcColumns, isColumnSelectorOpen, closeColumnSelectorModal} =
-        props;
-
-    return (
-        <ColumnSelectorModal
-            items={allColumns}
-            srcItems={srcColumns}
-            onConfirm={updateColumns}
-            isVisible={isColumnSelectorOpen}
-            onCancel={closeColumnSelectorModal}
-        />
-    );
-};
-
-const renderTable = (props) => {
-    const {
-        visibleColumns,
-        keyColumns,
-        yqlTypes,
-        settings,
-        visibleRows,
-        isDynamic,
-        isFullScreen,
-        offsetValue,
-        loading,
-        loaded,
-        isSplit,
-    } = props;
-
-    let stickyTop;
-    if (isSplit) {
-        stickyTop = 0; // TODO: add sticky for the Overview in the split mode https://github.com/captivationsoftware/react-sticky/issues/282
-    } else if (isFullScreen) {
-        stickyTop = 0;
-    } else {
-        stickyTop = HEADER_HEIGHT + OVERVIEW_HEIGHT;
-    }
-
-    const tableSettings = {
-        stickyTop,
-        stickyHead: 'moving',
-        syncHeadOnResize: true,
-        sortable: false,
-        stripedRows: true,
-        displayIndices: !isDynamic,
-    };
-
-    return (
-        <LoadDataHandler {...props} loaded={false}>
-            <DataTableWrapper
-                data={visibleRows}
-                loaded={loaded}
-                loading={loading}
-                yqlTypes={yqlTypes}
-                keyColumns={keyColumns}
-                ysonSettings={settings}
-                settings={tableSettings}
-                columns={visibleColumns}
-                isFullScreen={isFullScreen}
-                emptyDataMessage={i18n('alert_no-items-to-show')}
-                startIndex={!isDynamic ? offsetValue : undefined}
-            />
-        </LoadDataHandler>
-    );
-};
-
-function Table(props) {
-    const {path, getTableData, abortAndReset, isSplit} = props;
-    const isYqlV3Types = useSelector(selectIsYqlTypesEnabled);
-
-    useEffect(() => {
-        getTableData();
-        return abortAndReset;
-    }, [path, isYqlV3Types, abortAndReset]);
-
-    const {isFullScreen, handleScreenChanged, isDynamic} = props;
-
-    const toolbar = <TableOverview />;
-    return (
-        <div className={block()}>
-            <TableMeta />
-            <TableColumnsPresetNotice />
-            <FullScreen
-                className={block('fullscreen')}
-                enabled={isFullScreen}
-                onChange={handleScreenChanged}
-            >
-                {isSplit && !isFullScreen ? (
-                    <React.Fragment>
-                        {toolbar}
-                        {renderTable(props)}
-                    </React.Fragment>
-                ) : (
-                    <WithStickyToolbar
-                        hideToolbarShadow
-                        toolbarClassName={block('toolbar', {fullscreen: isFullScreen})}
-                        toolbar={toolbar}
-                        content={({sticky}) => renderTable({...props, sticky})}
-                    />
-                )}
-            </FullScreen>
-            {renderColumnSelectorModal(props)}
-            {isDynamic && <OffsetSelectorModal />}
-        </div>
-    );
-}
+import {TableBase} from './TableBase';
 
 const mapStateToProps = (state) => {
     const {loading, loaded, error, errorData, isColumnSelectorOpen, isFullScreen} =
@@ -255,10 +84,10 @@ const mapDispatchToProps = {
     closeColumnSelectorModal,
 };
 
-const TableConnected = compose(connect(mapStateToProps, mapDispatchToProps))(Table);
+const TableConnected = compose(connect(mapStateToProps, mapDispatchToProps))(TableBase);
 
 export default function TableWithRum() {
-    const loadState = useSelector(selectNavigationTableLoadingState);
+    const tableLoadState = useSelector(selectNavigationTableLoadingState);
     const isDynamic = useSelector(selectIsDynamic);
     const cluster = useSelector(selectCluster);
 
@@ -266,7 +95,7 @@ export default function TableWithRum() {
 
     useAppRumMeasureStart({
         type: measureId,
-        startDeps: [loadState, measureId],
+        startDeps: [tableLoadState, measureId],
         allowStart: ([loadState]) => {
             return !isFinalLoadingStatus(loadState);
         },
@@ -274,7 +103,7 @@ export default function TableWithRum() {
 
     useRumMeasureStop({
         type: measureId,
-        stopDeps: [loadState, measureId],
+        stopDeps: [tableLoadState, measureId],
         sybType: isDynamic ? 'dynamic' : 'static',
         allowStop: ([loadState]) => {
             return isFinalLoadingStatus(loadState);
