00d274d8e375e9c69db559ce5d07eac2f9720447
Maksim Efremov <ma-efremoff@yandex-team.ru>
2026-09-12 11:38:22 +0300

    test(Flow/Graph): add component coverage [YTFRONT-5792]

---
+++ packages/ui/src/ui/pages/flow/Flow/FlowGraph/__stories__/index.ts
@@ -0,0 +1,7 @@
+import {composeStories} from '@storybook/react';
+
+import * as FlowGraphStoryComponents from './FlowGraph.stories';
+
+export const FlowGraphStories = composeStories(FlowGraphStoryComponents);
+
+export default FlowGraphStories;
