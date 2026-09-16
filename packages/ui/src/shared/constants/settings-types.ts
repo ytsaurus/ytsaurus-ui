1c83b6368a48681e8edd27e91d082c4d4a5aa4d2
simbiozizv <simbiozizv@yandex-team.ru>
2026-09-16 15:05:31 +0300

    chore(Settings): add redisign setting [YTFRONT-5935]

--- packages/ui/src/shared/constants/settings-types.ts
+++ packages/ui/src/shared/constants/settings-types.ts
@@ -150,6 +150,7 @@ export type Stage = string;
 interface QueryTrackerSettings {
     'global::queryTracker::queriesListSidebarVisibilityMode': boolean;
     'global::queryTracker::history::Columns': string[];
+    'global::queryTracker::useNewQueriesView': boolean;
     'global::queryTracker::useNewGraphView': boolean;
     'global::queryTracker::graphAutoCenter': boolean;
     'global::queryTracker::suggestions': boolean;
