bdc243d8d941950030fb3cf530f38ebd7be166e7
xexys <xexys@yandex-team.ru>
2026-09-21 19:18:26 +0300

    chore: support theme-driven button props [YTFRONT-5743]

--- packages/ui/src/ui/components/Button/Button.tsx
+++ packages/ui/src/ui/components/Button/Button.tsx
@@ -5,6 +5,8 @@ import cn from 'bem-cn-lite';
 import {Hotkey, type HotkeyProps, Tooltip, type TooltipProps} from '@ytsaurus/components';
 import {Button as ButtonImpl, type ButtonProps as ButtonImplProps} from '@gravity-ui/uikit';
 
+import {type ButtonActionRole, useThemeProps} from '../ThemePropsConfigProvider';
+
 import './Button.scss';
 
 const block = cn('yt-button');
@@ -12,6 +14,7 @@ const block = cn('yt-button');
 export type ButtonCustomElementType = Exclude<React.ElementType, 'a' | 'button'> | undefined;
 
 export type ButtonProps<T extends ButtonCustomElementType = undefined> = {
+    actionRole?: ButtonActionRole;
     withTooltip?: boolean;
     tooltipProps?: TooltipProps;
     hotkey?: HotkeyProps['settings'];
@@ -20,12 +23,28 @@ export type ButtonProps<T extends ButtonCustomElementType = undefined> = {
 } & ButtonImplProps<T>;
 
 export function Button(props: ButtonProps) {
-    const {children, hotkey, tooltipProps, className, inlineMargins, withTooltip, ...buttonProps} =
-        props;
-    const buttonClassName = block({inline: inlineMargins}, className);
+    const {
+        actionRole,
+        children,
+        hotkey,
+        tooltipProps,
+        className,
+        inlineMargins,
+        view,
+        withTooltip,
+        ...buttonProps
+    } = props;
+    const buttonThemeProps = useThemeProps('button', {actionRole, view});
+
+    const buttonClassName = block(
+        {inline: inlineMargins},
+        [className, buttonThemeProps.className].filter(Boolean).join(' '),
+    );
+    const buttonView = buttonThemeProps.view ?? view;
+
     const button = (
         <>
-            <ButtonImpl className={buttonClassName} {...buttonProps}>
+            <ButtonImpl {...buttonProps} className={buttonClassName} view={buttonView}>
                 {children}
             </ButtonImpl>
             {hotkey && <Hotkey settings={hotkey} />}
